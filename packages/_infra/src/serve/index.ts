2c97c5c7ae3bd513ebf3b40a0c30907d538aa996
Blayne Chard <bchard@linz.govt.nz>
2019-10-22 14:20:00 +1300

    feat: adding ssl listener for alb

--- packages/_infra/src/serve/index.ts
+++ packages/_infra/src/serve/index.ts
@@ -2,8 +2,10 @@ import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
 import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
 import targets = require('@aws-cdk/aws-elasticloadbalancingv2-targets');
+import cert = require('@aws-cdk/aws-certificatemanager');
 
 import { LambdaXyz } from './lambda.xyz';
+import { ApplicationProtocol, SslPolicy } from '@aws-cdk/aws-elasticloadbalancingv2';
 
 /**
  * Tile serving infrastructure
@@ -17,21 +19,34 @@ export class ServeStack extends cdk.Stack {
         // TODO if/when the default VPC is set to default we can switch this to 'isDefault: true'
         // eslint-disable-next-line @typescript-eslint/camelcase
         const vpc = ec2.Vpc.fromLookup(this, 'AlbVpc', { tags: { AWS_Solutions: 'LandingZoneStackSet' } });
-        const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
-            vpc,
-            internetFacing: false,
-        });
+        const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', { vpc, internetFacing: false });
 
-        const listener = lb.addListener('Listener', {
+        // Due to the convoluted way of getting SSL certificates, we have a provisioned certificate
+        // in our account we need to look it up
+        const certArn = process.env.ALB_CERTIFICATE_ARN;
+        if (certArn == null) {
+            throw new Error('Unable to load certificate ARN: $ALB_CERTIFICATE_ARN');
+        }
+
+        const sslCert = cert.Certificate.fromCertificateArn(this, 'AlbCert', certArn);
+
+        const targetLambda = new targets.LambdaTarget(lambda.lambda);
+        const targetGroup = new elbv2.ApplicationTargetGroup(this, 'TargetGroup', {
+            targets: [targetLambda],
+        });
+        lb.addListener('HttpListener', {
             port: 80,
+            protocol: ApplicationProtocol.HTTP,
+            defaultTargetGroups: [targetGroup],
+        });
+        lb.addListener('HttpsListener', {
+            port: 443,
+            protocol: ApplicationProtocol.HTTPS,
+            sslPolicy: SslPolicy.RECOMMENDED,
+            certificateArns: [sslCert.certificateArn],
+            defaultTargetGroups: [targetGroup],
         });
 
-        listener.addTargets('XyzServe', {
-            targets: [new targets.LambdaTarget(lambda.lambda)],
-        });
-
-        new cdk.CfnOutput(this, 'LambdaXyzAlb', {
-            value: lb.loadBalancerDnsName,
-        });
+        new cdk.CfnOutput(this, 'LambdaXyzAlb', { value: lb.loadBalancerDnsName });
     }
 }
