52a452800a59ad9cc7c1164873bfa2d58a2df027
Blayne Chard <bchard@linz.govt.nz>
2020-01-28 12:07:28 +1300

    feat: adding bay of plenty urban 2018/19

--- packages/lambda-xyz/src/imagery/index.ts
+++ packages/lambda-xyz/src/imagery/index.ts
@@ -2,6 +2,7 @@ import { MosaicGeboBathy } from './gebco';
 import { MosaicSentinel } from './sentinel';
 import { MosaicWellingtonUrban2017 } from './regional/wellington';
 import { MosaicGisborneRural, MosaicGisborneUrban } from './regional/gisborne';
+import { MosaicBayOfPlentyUrban } from './regional/bay.of.plenty';
 
 export const Mosaics = [
     MosaicGeboBathy,
@@ -9,4 +10,5 @@ export const Mosaics = [
     MosaicGisborneRural,
     MosaicGisborneUrban,
     MosaicWellingtonUrban2017,
+    MosaicBayOfPlentyUrban,
 ];
