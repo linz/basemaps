a5e92883fcd904fadacdefeadcf04f0d8f918688
Joseph Ramsay <jramsay@linz.govt.nz>
2020-02-04 15:27:37 +1300

    build: remaining p1 regions

--- packages/lambda-xyz/src/imagery/index.ts
+++ packages/lambda-xyz/src/imagery/index.ts
@@ -1,12 +1,18 @@
 import './gebco';
-import './regional/bay.of.plenty';
+import './regional/auckland';
+import './regional/bayofplenty';
 import './regional/christchurch';
 import './regional/dunedin';
 import './regional/gisborne';
+import './regional/hamilton';
+import './regional/hastings';
 import './regional/invercargill';
 import './regional/napier';
 import './regional/nelson';
 import './regional/newplymouth';
+import './regional/palmerstonnorth';
+import './regional/tasman';
+import './regional/upperhutt';
 import './regional/wellington';
 import './sentinel';
 
