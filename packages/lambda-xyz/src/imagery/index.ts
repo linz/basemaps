83218a0d07947077d7a56ef029f2439a8573e0d0
Blayne Chard <bchard@linz.govt.nz>
2020-02-12 10:56:51 +1300

    build: adding remaining processed cogs

--- packages/lambda-xyz/src/imagery/index.ts
+++ packages/lambda-xyz/src/imagery/index.ts
@@ -12,7 +12,18 @@ import './regional/nelson';
 import './regional/newplymouth';
 import './regional/palmerstonnorth';
 import './regional/tasman';
-import './regional/upperhutt';
 import './regional/wellington';
-import './sentinel';
 
+import './regional/auckland_urban_2017_0-075m';
+import './regional/hastings-district_urban_2017-18_0-1m';
+import './regional/manawatu-whanganui_rural_2015-16_0-3m';
+import './regional/manawatu-whanganui_rural_2016-17_0-3m';
+import './regional/northland_rural_2014-16_0-4m';
+import './regional/otago_rural_2017-19_0-3m';
+import './regional/otago_rural_2017-19_0-3m';
+import './regional/taranaki_rural_2016-17_0-3m';
+import './regional/thames-coromandel_urban_2015_0-05m';
+import './regional/west-coast_rural_2015-16_0-3m';
+import './regional/whanganui_urban_2017-18_0-075m';
+
+import './sentinel';
