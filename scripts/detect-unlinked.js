c10e5b0b2932d29142e57a2474a94c2e7b33e111
Blayne Chard <blayne@chard.com>
2020-03-27 12:37:05 +1300

    build: determine if a package is importing another package without defining it

---
+++ scripts/detect-unlinked.js
@@ -0,0 +1,55 @@
+/**
+ * Determine if a package is using another package without referencing it inside of the package.json
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+const fs = require('fs').promises;
+const cp = require('child_process');
+
+const fileExists = async path => !!(await fs.stat(path).catch(() => false));
+
+function getAllImports(path) {
+    const allImports = new Set();
+
+    const data = cp
+        .execSync(`grep -R "'@basemaps" ${path}/src || true`)
+        .toString()
+        .split('\n');
+
+    for (const line of data) {
+        const pkgName = line
+            .split(' ')
+            .pop()
+            .replace(/[';]/g, '');
+
+        if (pkgName.length > 0) allImports.add(pkgName);
+    }
+
+    return allImports;
+}
+
+async function main() {
+    const packages = await fs.readdir('./packages');
+
+    for (const pkg of packages) {
+        const pkgPath = `./packages/${pkg}`;
+        const pkgJson = JSON.parse(await fs.readFile(`${pkgPath}/package.json`));
+
+        const hasSource = await fileExists(`${pkgPath}/src`);
+        if (!hasSource) continue;
+
+        const allDeps = Object.keys(pkgJson.dependencies || {}).concat(Object.keys(pkgJson.devDependencies || {}));
+
+        const localDeps = new Set(allDeps.filter(f => f.startsWith('@basemaps')));
+        const allImports = getAllImports(pkgPath);
+
+        for (const importedPackage of allImports) {
+            if (!localDeps.has(importedPackage)) {
+                console.error(`${pkg}: Missing import "${importedPackage}"`);
+            }
+        }
+    }
+}
+
+main().catch(console.error);
