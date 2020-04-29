fbc217a6ff04a9c2a3c67710410f71b7c03cb465
Joseph Ramsay <1757695+josephramsay@users.noreply.github.com>
2020-04-30 09:23:55 +1200

    Adding Sentinel imagery clipped to coast (#517)
    * Adding Sentinel imagery clipped to coast
    
    * fix: limited quadkeys + datalake sentinel name
    
    * fix: re-cut sentinel

--- packages/lambda-xyz/src/cli/imagery/sentinel.ts
+++ packages/lambda-xyz/src/cli/imagery/sentinel.ts
@@ -1,10 +1,10 @@
-import { MosaicCog } from './mosaic.cog';
 import { EPSG } from '@basemaps/geo';
+import { MosaicCog } from './mosaic.cog';
 
 MosaicCog.create({
-    id: '01E4PN40AEH10EH128AG28YSDM',
+    id: '01E71STQ7GGD769K6XX87F17PD',
     name: 'new_zealand_sentinel_2018-19_10m',
-    quadKeys: ['31'],
+
     projection: EPSG.Google,
 
     minZoom: 0,
@@ -12,4 +12,5 @@ MosaicCog.create({
     priority: 50,
     year: 2019,
     resolution: 10 * 1000,
+    quadKeys: ['31133', '31310', '31311'],
 });
