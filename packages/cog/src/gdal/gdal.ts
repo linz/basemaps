9d23274d0217e3dc443a53752e4c2aca1aff83fc
Joseph Ramsay <jramsay@linz.govt.nz>
2020-05-05 10:14:00 +1200

    fix: default resampling cubic to bilinear

--- packages/cog/src/gdal/gdal.ts
+++ packages/cog/src/gdal/gdal.ts
@@ -56,7 +56,7 @@ export class GdalCogBuilder {
             bbox: config.bbox,
             alignmentLevels: config.alignmentLevels ?? 1,
             compression: config.compression ?? 'webp',
-            resampling: config.resampling ?? 'cubic',
+            resampling: config.resampling ?? 'bilinear',
             blockSize: config.blockSize ?? 512,
         };
 
