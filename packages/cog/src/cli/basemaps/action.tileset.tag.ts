b61b72024ef831b343d4e4febe499f3f7e352be4
Blayne Chard <blacha@users.noreply.github.com>
2020-05-05 15:02:32 +1200

    feat(cli): invalidate cloudfront cache when updating tileset information (#554)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/cog/src/cli/basemaps/action.tileset.tag.ts
+++ packages/cog/src/cli/basemaps/action.tileset.tag.ts
@@ -6,6 +6,12 @@ import {
     CommandLineStringParameter,
 } from '@rushstack/ts-command-line';
 import { TileSetBaseAction } from './tileset.action';
+import * as AWS from 'aws-sdk';
+import { EPSG } from '@basemaps/geo';
+
+// Coudfront has to be defined in us-east-1
+const cloudFormation = new AWS.CloudFormation({ region: 'us-east-1' });
+const cloudFront = new AWS.CloudFront({ region: 'us-east-1' });
 
 export class TileSetUpdateTagAction extends TileSetBaseAction {
     private version: CommandLineIntegerParameter;
@@ -62,10 +68,45 @@ export class TileSetUpdateTagAction extends TileSetBaseAction {
 
         LogConfig.get().info({ version, tag, name, projection }, 'Tagging');
 
-        if (this.commit.value!) {
+        if (this.commit.value) {
             await Aws.tileMetadata.TileSet.tag(name, projection, tag, version);
-        } else {
+        }
+        await this.invalidateCache(name, projection, tag);
+
+        if (!this.commit.value) {
             LogConfig.get().warn('DryRun:Done');
+        }
+    }
+
+    async invalidateCache(name: string, projection: EPSG, tag: TileSetTag): Promise<void> {
+        const nameStr = tag == TileSetTag.Production ? name : `${name}@${tag}`;
+        const path = `/v1/tiles/${nameStr}/${projection}/*`;
+
+        const stackInfo = await cloudFormation.describeStacks({ StackName: 'Edge' }).promise();
+        if (stackInfo.Stacks?.[0].Outputs == null) {
+            LogConfig.get().warn('Unable to find cloud front distribution');
+            return;
+        }
+        const cloudFrontDomain = stackInfo.Stacks[0].Outputs.find((f) => f.OutputKey == 'CloudFrontDomain');
+
+        const cloudFrontDistributions = await cloudFront.listDistributions().promise();
+        const cf = cloudFrontDistributions.DistributionList?.Items?.find(
+            (f) => f.DomainName == cloudFrontDomain?.OutputValue,
+        );
+
+        if (cloudFrontDomain == null || cf == null) {
+            LogConfig.get().warn('Unable to find cloud front distribution');
+            return;
+        }
+
+        LogConfig.get().info({ path, cfId: cf.Id }, 'Invalidating');
+        if (this.commit.value) {
+            await cloudFront
+                .createInvalidation({
+                    DistributionId: cf.Id,
+                    InvalidationBatch: { Paths: { Quantity: 1, Items: [path] }, CallerReference: this.id },
+                })
+                .promise();
         }
     }
 }
