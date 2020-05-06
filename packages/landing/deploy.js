feb2716b8b5455d543952c4e27cda822a0e351a8
Blayne Chard <blacha@users.noreply.github.com>
2020-05-07 10:39:33 +1200

    build: production builds need to be tagged as production (#574)
    * build: production builds need to be tagged as production
    
    * build: dont fail to deploy if old landing page does not exist

--- packages/landing/deploy.js
+++ packages/landing/deploy.js
@@ -10,6 +10,16 @@ const HashKey = 'linz-hash';
 const s3 = new AWS.S3({ region: 'us-east-1' });
 const cf = new AWS.CloudFormation({ region: 'us-east-1' });
 
+async function getHash(Bucket, Key) {
+    try {
+        const obj = await s3.getObject({ Bucket, Key }).promise();
+        return obj.Metadata[HashKey];
+    } catch (e) {
+        console.log('FailedToFetch', { Bucket, Key }, e);
+    }
+    return null;
+}
+
 /**
  * Deploy the built s3 assets into the Edge bucket
  *
@@ -18,26 +28,23 @@ const cf = new AWS.CloudFormation({ region: 'us-east-1' });
 async function deploy() {
     // Since the bucket is generated inside of CDK lets look up the bucket name
     const stackInfo = await cf.describeStacks({ StackName: 'Edge' }).promise();
-    const bucket = stackInfo.Stacks[0].Outputs.find(f => f.OutputKey == 'CloudFrontBucket');
+    const bucket = stackInfo.Stacks[0].Outputs.find((f) => f.OutputKey == 'CloudFrontBucket');
     if (bucket == null) throw new Error('Failed to find EdgeBucket');
 
     const s3BucketName = bucket.OutputValue;
 
     const allBuckets = await s3.listBuckets().promise();
-    const s3Bucket = allBuckets.Buckets.find(f => f.Name == s3BucketName);
+    const s3Bucket = allBuckets.Buckets.find((f) => f.Name == s3BucketName);
     if (s3Bucket == null) throw new Error('Failed to locate edge bucket in current account');
 
     const files = await fs.readdir(DistDir);
     for (const fileName of files) {
         const fileData = await fs.readFile(`${DistDir}/${fileName}`);
-        const hash = crypto
-            .createHash('sha512')
-            .update(fileData)
-            .digest('base64');
+        const hash = crypto.createHash('sha512').update(fileData).digest('base64');
 
-        const obj = await s3.getObject({ Bucket: s3BucketName, Key: fileName }).promise();
+        const oldHash = await getHash(s3BucketName, fileName);
         // Only upload files if they have changed
-        if (obj.Metadata[HashKey] == hash) {
+        if (oldHash == hash) {
             console.log('Skipped', fileName, `${(fileData.byteLength / 1024).toFixed(2)}Kb`, hash);
             continue;
         }
