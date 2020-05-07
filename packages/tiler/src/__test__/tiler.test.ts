3eb267a1cfceefcdc9fa9872183a71d8da5818f7
Geoff Jacobsen <geoffjacobsen@gmail.com>
2020-05-08 10:57:01 +1200

    fix(tiler): position non square COGs correctly (#580)
    Non square COGs were being shifted up from their intended position

---
+++ packages/tiler/src/__test__/tiler.test.ts
@@ -0,0 +1,40 @@
+import { Bounds } from '@basemaps/geo';
+import { BoundingBox } from '@cogeotiff/core/build/vector';
+import * as o from 'ospec';
+import { Tiler } from '../tiler';
+
+o.spec('tiler.test', () => {
+    o('createComposition should handle non square images', () => {
+        const tiler = new Tiler(256) as any;
+
+        const img = {
+            id: 6,
+            getTileBounds(): BoundingBox {
+                return { x: 0, y: 0, width: 512, height: 387 };
+            },
+            tif: { source: { name: '313111000120111.tiff' } },
+            tileSize: { width: 512, height: 512 },
+        } as any;
+
+        const raster = {
+            tiff: new Bounds(4133696, 2623424, 256, 192),
+            intersection: new Bounds(4133696, 2623488, 192, 128),
+            tile: new Bounds(4133632, 2623488, 256, 256),
+        };
+
+        const ans = tiler.createComposition(img, 0, 0, 0.5, raster);
+        const { getBuffer, crop } = ans;
+        o(ans).deepEquals({
+            id: '313111000120111.tiff',
+            source: { x: 0, y: 0, imageId: 6 },
+            getBuffer,
+            y: 0,
+            x: 64,
+            extract: { width: 512, height: 387 },
+            resize: { width: 256, height: 194 },
+            crop,
+        });
+
+        o(crop.toJson()).deepEquals(new Bounds(0, 64, 192, 130).toJson());
+    });
+});
