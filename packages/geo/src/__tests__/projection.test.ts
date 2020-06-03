02411fb6ba1a11fdfdd9f557cc9776cefc66e5ec
Geoff Jacobsen <gjacobson@linz.govt.nz>
2020-06-03 21:06:35 +1200

    refactor(geo): remove tilebelt from projection

--- packages/geo/src/__tests__/projection.test.ts
+++ packages/geo/src/__tests__/projection.test.ts
@@ -1,8 +1,5 @@
 import * as o from 'ospec';
 import { Projection } from '../projection';
-import { Approx } from '@basemaps/test';
-
-const { Wgs84Bound } = Projection;
 
 o.spec('Projection', () => {
     const proj256 = new Projection(256);
@@ -14,25 +11,5 @@ o.spec('Projection', () => {
 
         const bounds512 = proj512.getPixelsFromTile(1, 2);
         o(bounds512.toJson()).deepEquals({ x: 512, y: 1024, width: 512, height: 512 });
-    });
-
-    o('should get center of tile', () => {
-        const latLon0 = proj256.getLatLonCenterFromTile(0, 0, 0);
-        o(latLon0).deepEquals({ lat: 0, lon: 0 });
-
-        const latLonA = proj256.getLatLonCenterFromTile(0, 0, 1);
-        const latLonB = proj256.getLatLonCenterFromTile(0, 1, 1);
-        const latLonC = proj256.getLatLonCenterFromTile(1, 1, 1);
-        const latLonD = proj256.getLatLonCenterFromTile(1, 0, 1);
-
-        Approx.latLon(latLonA, { lat: Wgs84Bound.lat / 2, lon: -Wgs84Bound.lon / 2 });
-        Approx.latLon(latLonB, { lat: -Wgs84Bound.lat / 2, lon: -Wgs84Bound.lon / 2 });
-        Approx.latLon(latLonC, { lat: -Wgs84Bound.lat / 2, lon: Wgs84Bound.lon / 2 });
-        Approx.latLon(latLonD, { lat: Wgs84Bound.lat / 2, lon: Wgs84Bound.lon / 2 });
-    });
-
-    o('constants', () => {
-        o(Wgs84Bound).deepEquals({ lat: 85.0511287798066, lon: 180 });
-        o(Projection.GoogleScaleDenominator).equals(559082264.029);
     });
 });
--- packages/geo/src/projection.ts
+++ packages/geo/src/projection.ts
@@ -1,5 +1,4 @@
 import { Bounds, Point } from './bounds';
-import { tileToQuadkey, tileToBBOX } from '@mapbox/tilebelt';
 
 export interface LatLon {
     lat: number;
@@ -9,10 +8,6 @@ export interface LatLon {
 export class Projection {
     /** Size of the earth EPSG:3857 constant */
     public static readonly A = 6378137.0;
-
-    public static readonly GoogleScaleDenominator = 559082264.029;
-
-    public static readonly Wgs84Bound = { lat: 85.0511287798066, lon: 180 };
 
     /** EPSG:3857 origin shift */
     public static readonly OriginShift = (2 * Math.PI * Projection.A) / 2.0;
@@ -39,15 +34,6 @@ export class Projection {
     }
 
     /**
-     * Convert a google tile index into a quad key
-     * @param tX tile X offset
-     * @param tY tile Y offset
-     * @param zoom WebMercator zoom level
-     */
-    public getQuadKeyFromTile(tX: number, tY: number, zoom: number): string {
-        return tileToQuadkey([tX, tY, zoom]);
-    }
-    /**
      * Convert a XYZ tile into the raster bounds for the tile
      * @param tX tile X offset
      * @param tY tile Y offset
@@ -58,20 +44,6 @@ export class Projection {
         const pX = (tX + Projection.OriginShift) / res;
         const pY = (tY + Projection.OriginShift) / res;
         return { x: pX, y: pY };
-    }
-
-    /**
-     * Get the center of a XYZ tile in LatLon
-     * @param tX Tile X offset
-     * @param tY Tile Y offset
-     * @param zoom WebMercator zoom level
-     */
-    public getLatLonCenterFromTile(tX: number, tY: number, zoom: number): LatLon {
-        const [minX, minY, maxX, maxY] = tileToBBOX([tX, tY, zoom]);
-        return {
-            lat: (maxY + minY) / 2,
-            lon: (maxX + minX) / 2,
-        };
     }
 
     /**
