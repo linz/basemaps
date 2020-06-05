d1165afcaf2c67eff9236f535cf029865b1d2114
Geoff Jacobsen <gjacobson@linz.govt.nz>
2020-06-05 12:40:51 +1200

    refactor(geo): remove QuadKeyTrie

--- packages/geo/src/index.ts
+++ packages/geo/src/index.ts
@@ -1,7 +1,6 @@
 export { Bounds, BoundingBox, Size } from './bounds';
 export { QuadKey } from './quad.key';
 export { GeoJson } from './geo.json';
-export { QuadKeyTrie } from './quad.key.trie';
 export { Epsg, EpsgCode } from './epsg';
 export { TileMatrixSet, Tile } from './tile.matrix.set';
 export { WmtsLayer, WmtsProvider } from './wmts/wmts';
