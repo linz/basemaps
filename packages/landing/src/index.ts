c35ee8e9a15feb88512961811160b9251c32a9a6
Blayne Chard <bchard@linz.govt.nz>
2020-06-25 17:25:15 +1200

    feat(landing): use webp only if the browser supports it

--- packages/landing/src/index.ts
+++ packages/landing/src/index.ts
@@ -2,10 +2,26 @@ import './global';
 
 import { Basemaps } from './map';
 import { addDebugLayer } from './debug';
+import { WindowUrl } from './url';
+
+// Source https://stackoverflow.com/questions/5573096/detecting-webp-support
+function canUseWebP(): boolean {
+    const elem = document.createElement('canvas');
+
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // was able or not to get WebP representation
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+
+    // very old browser like IE 8, canvas not supported
+    return false;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const mapEl = document.getElementById('map');
     if (mapEl == null) throw new Error('Cannot find #map element');
+    if (canUseWebP()) WindowUrl.ImageFormat = 'webp';
+
     const basemaps = new Basemaps(mapEl);
     window.basemaps = basemaps;
 
