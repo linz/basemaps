8b84574c45da1eb75fa88f8f6534e9e85d2d1207
Blayne Chard <bchard@linz.govt.nz>
2020-07-03 13:44:11 +1200

    feat(landing): report tile loading stats
    record how long tiles take to load for users

--- packages/landing/src/config.ts
+++ packages/landing/src/config.ts
@@ -16,14 +16,22 @@ export const Config = {
 // Inject google analytics after everything has loaded
 if (Config.GoogleAnalytics != null && typeof window != 'undefined') {
     window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]): void {
+    window.gtag = function gtag(...args: any[]): void {
         window.dataLayer.push(args);
-    }
-    gtag('js', new Date());
-    gtag('config', `${Config.GoogleAnalytics}`);
-
+    };
+    window.gtag('js', new Date());
+    window.gtag('config', `${Config.GoogleAnalytics}`);
     const script = document.createElement('script');
     script.setAttribute('async', '');
     script.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${Config.GoogleAnalytics}`);
     document.head.appendChild(script);
+}
+
+export const enum GaEvent {
+    TileTiming = 'TileTiming',
+}
+
+export function gaEvent(category: GaEvent, label: string, value: number): void {
+    if (Config.GoogleAnalytics == null) return;
+    window.gtag('event', { category, label, value });
 }
