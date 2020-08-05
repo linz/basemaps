adecdb7fb114f073bb6310540e4ff69bfc290e86
Geoff Jacobsen <gjacobson@linz.govt.nz>
2020-08-06 10:31:00 +1200

    refactor(geojson): move geojson builders to @linzjs/geojson package (#1013)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/geo/src/index.ts
+++ packages/geo/src/index.ts
@@ -1,6 +1,5 @@
 export { BoundingBox, Bounds, Size } from './bounds';
 export { Epsg, EpsgCode } from './epsg';
-export { GeoJson } from './geo.json';
 export { QuadKey } from './quad.key';
 export { Tile, TileMatrixSet } from './tile.matrix.set';
 export { WmtsLayer, WmtsProvider } from './wmts/wmts';
---
+++ packages/linzjs-geojson/src/construct.ts
@@ -0,0 +1,83 @@
+import type { FeatureCollection, Feature, Position, MultiPolygon, Polygon } from 'geojson';
+import { ConvertCoordinates } from './mulitpolygon/convert';
+
+/**
+ * Join a list of features into a Feature collection
+ * @param features
+ */
+export function toFeatureCollection(features: Feature[]): FeatureCollection {
+    return {
+        type: 'FeatureCollection',
+        features,
+    };
+}
+
+export function toPolygon(coordinates: Position[][]): Polygon {
+    return {
+        type: 'Polygon',
+        coordinates,
+    };
+}
+
+/** Create a feature polygon */
+export function toFeaturePolygon(coordinates: Position[][], properties = {}): Feature {
+    return {
+        type: 'Feature',
+        geometry: toPolygon(coordinates),
+        properties,
+    };
+}
+
+/**
+ * Create a feature mult-polygon
+ */
+export function toFeatureMultiPolygon(coordinates: Position[][][], properties = {}): Feature {
+    return {
+        type: 'Feature',
+        geometry: {
+            type: 'MultiPolygon',
+            coordinates,
+        },
+        properties,
+    };
+}
+
+/**
+ * Make a copy of point `p`
+ */
+export const copyPoint: ConvertCoordinates = (p) => [p[0], p[1]];
+
+/**
+ * Convert polygons inside features to one MultiPolygon
+ *
+ * **This only converts Polygon or MultiPolygon**
+ *
+ * @param features list of features to convert
+ * @param removeHoles remove any holes in the polygons
+ * @param transform change the coordinates of each point. Defaults to copying the point
+ */
+export function featuresToMultiPolygon(features: Feature[], removeHoles = false, transform = copyPoint): MultiPolygon {
+    const coordinates: Position[][][] = [];
+    for (const { geometry } of features) {
+        if (geometry.type === 'MultiPolygon') {
+            for (const coords of geometry.coordinates) {
+                if (removeHoles) {
+                    coordinates.push([coords[0].map(transform)]);
+                } else {
+                    coordinates.push(coords.map((ring) => ring.map(transform)));
+                }
+            }
+        } else if (geometry.type === 'Polygon') {
+            if (removeHoles) {
+                coordinates.push([geometry.coordinates[0].map(transform)]);
+            } else {
+                coordinates.push(geometry.coordinates.map((ring) => ring.map(transform)));
+            }
+        }
+    }
+
+    return {
+        type: 'MultiPolygon',
+        coordinates,
+    };
+}
