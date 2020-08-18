922c617b555672d36bd3d2e4986d3b46ad333731
Geoff Jacobsen <gjacobson@linz.govt.nz>
2020-08-18 14:42:10 +1200

    fix(lambda-xyz): Stop health and ping response being cached (#1066)
    Cloudfront was caching these requests

--- packages/lambda-xyz/src/routes/api.ts
+++ packages/lambda-xyz/src/routes/api.ts
@@ -1,15 +1,19 @@
-import { LambdaHttpResponse } from '@basemaps/lambda';
+import { LambdaHttpResponse, HttpHeader } from '@basemaps/lambda';
+
+const OkResponse = new LambdaHttpResponse(200, 'ok');
+OkResponse.header(HttpHeader.CacheControl, 'no-store');
 
 export async function Health(): Promise<LambdaHttpResponse> {
-    return new LambdaHttpResponse(200, 'ok');
+    return OkResponse;
 }
 
 export async function Ping(): Promise<LambdaHttpResponse> {
-    return new LambdaHttpResponse(200, 'ok');
+    return OkResponse;
 }
 
 export async function Version(): Promise<LambdaHttpResponse> {
     const response = new LambdaHttpResponse(200, 'ok');
+    response.header(HttpHeader.CacheControl, 'no-store');
     response.json({ version: process.env.GIT_VERSION, hash: process.env.GIT_HASH });
     return response;
 }
