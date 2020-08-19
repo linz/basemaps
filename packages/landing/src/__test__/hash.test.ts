f09339737958f90f514f4b575159cebc4c500b18
Blayne Chard <bchard@linz.govt.nz>
2020-08-19 14:06:15 +1200

    feat(landing): support urls with z14 or 14z

--- packages/landing/src/__test__/hash.test.ts
+++ packages/landing/src/__test__/hash.test.ts
@@ -16,10 +16,25 @@ o.spec('WindowUrl', () => {
     });
     const googleLoc = { lat: 174.7763921, lon: -41.277848, zoom: 8 };
 
-    o('should encode lon lat', () => {
-        const output = WindowUrl.toHash(googleLoc);
-        o(output).equals('#@174.7763921,-41.277848,z8');
-        o(WindowUrl.fromHash(output)).deepEquals(googleLoc);
+    o.spec('Hash', () => {
+        o('should encode lon lat', () => {
+            const output = WindowUrl.toHash(googleLoc);
+            o(output).equals('#@174.7763921,-41.277848,z8');
+            o(WindowUrl.fromHash(output)).deepEquals(googleLoc);
+            o(WindowUrl.fromHash('#@174.7763921,-41.277848,8z')).deepEquals(googleLoc);
+        });
+
+        o('should encode fractional zooms', () => {
+            o(WindowUrl.fromHash('#@174.7763921,-41.277848,14.25z').zoom).deepEquals(14.25);
+            o(WindowUrl.fromHash('#@174.7763921,-41.277848,z14.25').zoom).deepEquals(14.25);
+        });
+
+        o('should not fail if parts are missing', () => {
+            const missingZoom = WindowUrl.fromHash('#@174.7763921,-41.277848,');
+            o(missingZoom).deepEquals({ lat: googleLoc.lat, lon: googleLoc.lon });
+            const missingParam = WindowUrl.fromHash('#@174.7763921,');
+            o(missingParam).deepEquals({});
+        });
     });
 
     o('should extract information', () => {
