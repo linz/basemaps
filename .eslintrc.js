734e00e3b0105c8eaf93e527139037d1c278bf95
Blayne Chard <bchard@linz.govt.nz>
2020-08-21 10:31:40 +1200

    refactor: convert to linzjs/style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,5 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    extends: [
-        "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
-    ],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module"
-    },
+    ...require('@linzjs/style/.eslintrc.js'),
     rules:{
         "@typescript-eslint/no-unused-vars": "error",
         "@typescript-eslint/explicit-function-return-type": "error"
