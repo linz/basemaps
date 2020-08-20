89c27f950d45634332526ab68d60197a4f3a7107
Blayne Chard <bchard@linz.govt.nz>
2020-08-21 10:41:44 +1200

    refactor: resolve lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
     ...require('@linzjs/style/.eslintrc.js'),
-    rules:{
-        "@typescript-eslint/no-unused-vars": "error",
-        "@typescript-eslint/explicit-function-return-type": "error"
-    }
 };
--- packages/lambda/src/lambda.aws.ts
+++ packages/lambda/src/lambda.aws.ts
@@ -1,7 +1,6 @@
 export function toAlbHeaders<T>(mapInstance: Map<string, T>): Record<string, T> | undefined {
-    if (mapInstance.size == 0) {
-        return undefined;
-    }
+    if (mapInstance.size == 0) return undefined;
+
     const obj: Record<string, T> = {};
     for (const prop of mapInstance) {
         obj[prop[0]] = prop[1];
@@ -9,12 +8,9 @@ export function toAlbHeaders<T>(mapInstance: Map<string, T>): Record<string, T> 
     return obj;
 }
 
-export function toCloudFrontHeaders<T>(
-    mapInstance: Map<string, T>,
-): Record<string, { key: string; value: string }[]> | {} {
-    if (mapInstance.size == 0) {
-        return {};
-    }
+export function toCloudFrontHeaders<T>(mapInstance: Map<string, T>): Record<string, { key: string; value: string }[]> {
+    if (mapInstance.size == 0) return {};
+
     const obj: Record<string, { key: string; value: string }[]> = {};
     for (const prop of mapInstance) {
         obj[prop[0]] = [{ key: prop[0], value: String(prop[1]) }];
