e357c88ac83ae16f0eb86901dc138579432caa97
Blayne Chard <blacha@users.noreply.github.com>
2020-09-10 14:05:18 +1200

    ci: group the deployment steps by env (#1159)
    https://github.com/linz/basemaps/pull/1159

--- packages/shared/src/cli/__test__/git.tag.test.ts
+++ packages/shared/src/cli/__test__/git.tag.test.ts
@@ -4,7 +4,7 @@ import { GitTag } from '../git.tag';
 o.spec('git.tag', () => {
     o('format', () => {
         const ans = GitTag();
-        o(/^v\d+\.\d+\.\d+(-\d+-g[0-9a-f]+)?$/.test(ans.version)).equals(true);
-        o(/^[0-9a-f]+$/.test(ans.hash)).equals(true);
+        o(/^v\d+\.\d+\.\d+(-\d+-g[0-9a-f]+)?$/.test(ans.version)).equals(true)(`Got: ${ans.version}`);
+        o(/^[0-9a-f]+$/.test(ans.hash)).equals(true)(`Got: ${ans.hash}`);
     });
 });
