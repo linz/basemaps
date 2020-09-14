70b8eb04c4a6593ffd77d164ff589c989ae9dfd3
Blayne Chard <blacha@users.noreply.github.com>
2020-09-15 11:17:44 +1200

    docs: explain how cicd is handled in this repo (#1161)
    https://github.com/linz/basemaps/pull/1161

--- scripts/version.bump.sh
+++ scripts/version.bump.sh
@@ -7,9 +7,13 @@ set -e
 git checkout master
 git pull --rebase
 
+# Validate that there are actually changes to be made, this will fail if nothing needs publishing
 npx lerna changed -a
+
 npx lerna version --conventional-commits --no-push -m 'release: %s' 
 
 CURRENT_VERSION=$(node -p "require('./lerna.json').version")
 git checkout -b release/v${CURRENT_VERSION}
+
+# This tag will be created once the pull request is merged
 git tag -d v${CURRENT_VERSION}
