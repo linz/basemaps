65752b99b99d84e6690ebcce26171a15c87a9ef5
Blayne Chard <blacha@users.noreply.github.com>
2020-09-21 13:33:48 +1200

    feat(lambda-analytics): allow analytics to be reprocessed by removing  the cached data (#1195)
    https://github.com/linz/basemaps/pull/1195

--- packages/lambda-analytics/src/file.process.ts
+++ packages/lambda-analytics/src/file.process.ts
@@ -8,7 +8,7 @@ export const FileProcess = {
     reader(fileName: string): AsyncGenerator<string> | Interface {
         return createInterface({ input: s3fs.readStream(fileName).pipe(createGunzip()) });
     },
-    async process(fileName: string, logger: LogType): Promise<void> {
+    async process(fileName: string, stats: LogStats, logger: LogType): Promise<void> {
         const lineReader = FileProcess.reader(fileName);
         for await (const line of lineReader) {
             if (line.startsWith('#')) continue;
@@ -16,6 +16,13 @@ export const FileProcess = {
 
             const date = lineData[0];
             const time = lineData[1];
+
+            const dateStr = `${date}T${time.slice(0, 2)}:00:00.000Z`;
+            if (dateStr !== stats.date) {
+                logger.error({ got: dateStr, expected: stats.date, line }, 'Invalid date record');
+                continue;
+            }
+
             const uri = lineData[7];
             const status = lineData[8];
             const referer = lineData[9] === '-' ? undefined : getUrlHost(lineData[9]);
@@ -24,15 +31,14 @@ export const FileProcess = {
 
             // Ignore requests which are not tile requests
             if (!uri.startsWith('/v1')) continue;
-            const ls = LogStats.getDate(date, time);
             if (!query.startsWith('api=')) {
-                logger.info({ uri, query }, 'NoApiKey');
+                logger.debug({ uri, query }, 'NoApiKey');
                 continue;
             }
             // TODO This could be switched to a QueryString parser
             const endIndex = query.indexOf('&');
             const apiKey = query.slice('api='.length, endIndex == -1 ? query.length : endIndex);
-            ls.track(apiKey, referer, uri.toLowerCase(), parseInt(status), hit);
+            stats.track(apiKey, referer, uri.toLowerCase(), parseInt(status), hit);
         }
     },
 };
