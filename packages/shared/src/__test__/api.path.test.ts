400126c9451819eaebfb7d51d95c4d8298361c0c
Geoff Jacobsen <gjacobson@linz.govt.nz>
2020-09-24 10:17:45 +1200

    fix(lambda-tiler): regression in invalid url parsing causing 500 Error (#1212)
    https://github.com/linz/basemaps/pull/1212

---
+++ packages/shared/src/__test__/api.path.test.ts
@@ -0,0 +1,43 @@
+import { Epsg } from '@basemaps/geo';
+import { ImageFormat } from '@basemaps/tiler';
+import o from 'ospec';
+import { tileAttributionFromPath, TileType, tileWmtsFromPath, tileXyzFromPath } from '../api.path';
+
+o.spec('api.path', () => {
+    o('tileXyzFromPath', () => {
+        o(tileXyzFromPath(['aerial', 'EPSG:3857', '10', '3456', '5432.webp'])).deepEquals({
+            type: TileType.Image,
+            name: 'aerial',
+            projection: Epsg.Google,
+            x: 3456,
+            y: 5432,
+            z: 10,
+            ext: ImageFormat.WEBP,
+        });
+        o(tileXyzFromPath([])).equals(null);
+        o(tileXyzFromPath(['aerial', 'EPSG:3857', '10', '3456'])).equals(null);
+    });
+
+    o('tileWmtsFromPath', () => {
+        o(tileWmtsFromPath(['aerial', 'EPSG:3857', 'WMTSCapabilities.xml'])).deepEquals({
+            type: TileType.WMTS,
+            name: 'aerial',
+            projection: Epsg.Google,
+        });
+        o(tileWmtsFromPath([])).deepEquals({
+            type: TileType.WMTS,
+            name: '',
+            projection: null,
+        });
+    });
+
+    o('tileAttributionFromPath', () => {
+        o(tileAttributionFromPath(['aerial', 'EPSG:3857', 'attribution.json'])).deepEquals({
+            type: TileType.Attribution,
+            name: 'aerial',
+            projection: Epsg.Google,
+        });
+        o(tileAttributionFromPath([])).equals(null);
+        o(tileAttributionFromPath(['aerial', 'attribution.json'])).equals(null);
+    });
+});
