c18bcab8e581fe3af4969049b7ee76381536f501
Wentao Kuang <wkuang@linz.govt.nz>
2020-10-28 16:06:05 +1300

    feat(lambda-tiler): Add more get tile e2e tests and git action to run the test for dev deploy.

--- packages/lambda-tiler/src/__test__/e2e.test.ts
+++ packages/lambda-tiler/src/__test__/e2e.test.ts
@@ -1,10 +1,12 @@
-import fetch from 'node-fetch';
 import o from 'ospec';
+import fetch from 'node-fetch';
 import * as path from 'path';
-import { readFileSync } from 'fs';
-import { ImageFormat } from '@basemaps/tiler';
+import * as fs from 'fs';
+import PixelMatch = require('pixelmatch');
+import { PNG } from 'pngjs';
 import { Epsg, Tile } from '@basemaps/geo';
 import { getApiKey } from '@basemaps/shared';
+import { ImageFormat } from '@basemaps/tiler';
 
 function getExpectedTileName(projection: Epsg, tile: Tile, format: ImageFormat): string {
     return path.join(
@@ -33,21 +35,24 @@ o.spec('E2E test for lambda-tiler', () => {
         o(response.statusText).equals('OK');
         const ResponseJson = await response.json();
         const ResponseVersion = ResponseJson.version.split('-')[0];
-        const packageJson = require('../../package.json');
+        const packagePath = path.join(__dirname, '..', '..', 'package.json');
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const packageJson = require(packagePath);
         const packageVersion = 'v' + packageJson.version;
         o(ResponseVersion).equals(packageVersion);
     });
 
     // TODO: More tests to add here.
     const TestTiles = [
-        { projection: Epsg.Google, format: ImageFormat.WEBP, tile: { x: 1013, y: 629, z: 10 } },
-        // { projection: Epsg.Google, format: ImageFormat.PNG, tile: { x: 5, y: 8, z: 2 } },
-        // { projection: Epsg.Nztm2000, format: ImageFormat.JPEG, tile: { x: 6, y: 8, z: 2 } }
+        { projection: Epsg.Google, format: ImageFormat.PNG, tile: { x: 30, y: 30, z: 6 } },
+        { projection: Epsg.Google, format: ImageFormat.PNG, tile: { x: 0, y: 1, z: 1 } },
+        { projection: Epsg.Google, format: ImageFormat.PNG, tile: { x: 15, y: 10, z: 4 } },
     ];
 
     TestTiles.forEach(({ projection, format, tile }) => {
-        const getTile = `/v1/tiles/aerial/EPSG:${projection}/${tile.z}/${tile.x}/${tile.y}.${format}`;
-        o('should return valid tiles from request', async () => {
+        const imgId = '01EDMTM3P563P06TWYQAZRA9F6';
+        const getTile = `/v1/tiles/${imgId}/EPSG:${projection}/${tile.z}/${tile.x}/${tile.y}.${format}`;
+        o(`should return valid tiles: x${tile.x} y${tile.y} z${tile.z} projection: ${projection}`, async () => {
             o.timeout(5 * 1000);
 
             // Given ... a get tile request
@@ -56,14 +61,16 @@ o.spec('E2E test for lambda-tiler', () => {
             // When ...
             const response = await fetch(getTileUrl);
             const buffer = await response.buffer();
+            const resImg = PNG.sync.read(buffer);
 
             // Then ...
             o(response.status).equals(200);
             o(response.statusText).equals('OK');
-
             const fileName = getExpectedTileName(projection, tile, format);
-            const bytes = readFileSync(fileName);
-            o(buffer).deepEquals(bytes);
+            const bytes = fs.readFileSync(fileName);
+            const testImg = PNG.sync.read(bytes);
+            const missMatchedPixels = PixelMatch(testImg.data, resImg.data, null, 256, 256);
+            o(missMatchedPixels).equals(0);
         });
     });
 });
