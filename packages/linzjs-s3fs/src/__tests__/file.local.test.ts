edd93dbe54d797b7945e524cfbc2c07266356689
Blayne Chard <bchard@linz.govt.nz>
2021-03-02 09:56:49 +1300

    build: macosx does not use /root

--- packages/linzjs-s3fs/src/__tests__/file.local.test.ts
+++ packages/linzjs-s3fs/src/__tests__/file.local.test.ts
@@ -2,8 +2,10 @@ import o from 'ospec';
 import { FsLocal } from '../file.local';
 import { CompositeError } from '../composite.error';
 import { S3Fs } from '..';
+import * as path from 'path';
 
 o.spec('FileLocal', () => {
+    const RootFolder = process.platform === 'darwin' ? '/var/root' : '/root';
     const s3fs = new S3Fs();
     const localFs = new FsLocal();
     o('Should capture not found errors:list', async () => {
@@ -38,7 +40,7 @@ o.spec('FileLocal', () => {
 
     o('Should capture permission errors:list', async () => {
         try {
-            await s3fs.toArray(localFs.list('/root/test'));
+            await s3fs.toArray(localFs.list(path.join(RootFolder, 'test')));
             throw new Error('Failed to throw');
         } catch (e) {
             o(CompositeError.isCompositeError(e)).equals(true);
@@ -48,7 +50,7 @@ o.spec('FileLocal', () => {
 
     o('Should capture permission errors:write', async () => {
         try {
-            await localFs.write('/root/test', Buffer.from('foobar'), false);
+            await localFs.write(path.join(RootFolder, 'test'), Buffer.from('foobar'), false);
             throw new Error('Failed to throw');
         } catch (e) {
             o(CompositeError.isCompositeError(e)).equals(true);
@@ -58,7 +60,7 @@ o.spec('FileLocal', () => {
 
     o('Should capture permission errors:read', async () => {
         try {
-            await localFs.read('/root/test');
+            await localFs.read(path.join(RootFolder, 'test'));
             throw new Error('Failed to throw');
         } catch (e) {
             o(CompositeError.isCompositeError(e)).equals(true);
