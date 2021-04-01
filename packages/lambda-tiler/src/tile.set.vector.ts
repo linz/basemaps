3543f763b5b7439996b828fe1f63815595d0cfa5
Blayne Chard <blayne@chard.com>
2021-04-01 15:35:20 +1300

    fix(lambda-tiler): force vector tiles to be served as protobuf

--- packages/lambda-tiler/src/tile.set.vector.ts
+++ packages/lambda-tiler/src/tile.set.vector.ts
@@ -1,5 +1,5 @@
-import { LambdaContext, LambdaHttpResponse } from '@basemaps/lambda';
-import { Aws, TileDataXyz, TileSetType, TileSetVectorRecord } from '@basemaps/shared';
+import { HttpHeader, LambdaContext, LambdaHttpResponse } from '@basemaps/lambda';
+import { Aws, TileDataXyz, TileSetType, TileSetVectorRecord, VectorFormat } from '@basemaps/shared';
 import { SourceAwsS3 } from '@cogeotiff/source-aws';
 import { Covt } from '@covt/core';
 import { NotFound } from './routes/tile';
@@ -11,6 +11,7 @@ export class TileSetVector extends TileSetHandler<TileSetVectorRecord> {
     type = TileSetType.Vector;
 
     async tile(req: LambdaContext, xyz: TileDataXyz): Promise<LambdaHttpResponse> {
+        if (xyz.ext !== VectorFormat.MapboxVectorTiles) return NotFound;
         if (this.tileSet.layers.length > 1) return new LambdaHttpResponse(500, 'Too many layers in tileset');
         const [layerUri] = this.tileSet.layers;
 
@@ -18,7 +19,7 @@ export class TileSetVector extends TileSetHandler<TileSetVectorRecord> {
         if (covt == null) {
             const source = SourceAwsS3.fromUri(layerUri, Aws.s3);
             const index = SourceAwsS3.fromUri(layerUri + '.index', Aws.s3);
-            if (source == null || index == null) return new LambdaHttpResponse(500, 'Failed to load COVT');
+            if (source == null || index == null) return new LambdaHttpResponse(500, 'Failed to load VectorTiles');
             covt = Covt.create(source, index);
             Covts.set(layerUri, covt);
         }
@@ -27,7 +28,8 @@ export class TileSetVector extends TileSetHandler<TileSetVectorRecord> {
         const tile = await actualCovt.getTile(xyz.x, xyz.y, xyz.z, req.log);
         if (tile == null) return NotFound;
         const response = new LambdaHttpResponse(200, 'Ok');
-        response.buffer(Buffer.from(tile.buffer), tile.mimeType);
+        response.buffer(Buffer.from(tile.buffer), 'application/x-protobuf');
+        if (tile.mimeType.includes('gzip')) response.header(HttpHeader.ContentEncoding, 'gzip');
         return response;
     }
 }
