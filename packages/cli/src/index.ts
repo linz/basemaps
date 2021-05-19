51e2d8aa0d6a74c0c7d0885c83b50979ad00ba68
Wentao Kuang <wkuang@linz.govt.nz>
2021-05-20 11:44:32 +1200

    export invalidate cache function from cli package.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,3 +4,4 @@ export { CogJobFactory } from './cog/job.factory';
 export { Gdal } from './gdal/gdal';
 export { GdalCogBuilder } from './gdal/gdal.cog';
 export { GdalCogBuilderOptions } from './gdal/gdal.config';
+export { invalidateCache } from './cli/util';
