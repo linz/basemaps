d36aa6e0bf6f3ec0ed320a37dbb5395c2218657d
Blayne Chard <blayne@chard.com>
2021-06-23 14:28:04 +1200

    feat: remove api key table too as it is unneeded

--- packages/_infra/src/index.ts
+++ packages/_infra/src/index.ts
@@ -1,7 +1,6 @@
 import { App } from '@aws-cdk/core';
 import { Env } from '@basemaps/shared';
 import { EdgeAnalytics } from './analytics/edge.analytics';
-import { ApiKeyTableStack } from './api.key.db';
 import { CogBuilderStack } from './cogify';
 import { BaseMapsRegion } from './config';
 import { DeployEnv } from './deploy.env';
@@ -22,11 +21,9 @@ async function main(): Promise<void> {
      * Because we are using Lambda@Edge the edge stack has to be deployed into us-east-1,
      * The dynamoDb table needs to be close to our users that has to be deployed in ap-southeast-2
      */
-    const table = new ApiKeyTableStack(basemaps, 'ApiKeyTable', { env: { region: BaseMapsRegion, account } });
     const edge = new EdgeStack(basemaps, 'Edge', { env: { region: 'us-east-1', account } });
     const serve = new ServeStack(basemaps, 'Serve', { env: { region: BaseMapsRegion, account } });
 
-    edge.addDependency(table);
     edge.addDependency(serve);
 
     // TODO is there a better way of handling this,
