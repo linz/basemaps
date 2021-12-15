531c9b0c4b580669df6c69606aa4e418c0dabae4
Wentao Kuang <wkuang@linz.govt.nz>
2021-12-16 11:20:01 +1300

    Fix the html layout for lables and values.

--- packages/landing/src/components/debug.tsx
+++ packages/landing/src/components/debug.tsx
@@ -11,30 +11,23 @@ export class Debug extends Component<{ map: maplibre.Map }> {
     return (
       <div className="debug">
         <div className="debug__info">
-          <label className="debug__label">
-            <div className="debug__value">ImageId: {Config.map.layerId}</div>
-          </label>
+          <label className="debug__label">ImageId</label>
+          <div className="debug__value"> {Config.map.layerId}</div>
         </div>
         <div className="debug__info">
-          <label className="debug__label">
-            <div className="debug__value">Projection: {Config.map.tileMatrix.projection.toEpsgString()}</div>
-          </label>
+          <label className="debug__label">Projection </label>
+          <div className="debug__value">{Config.map.tileMatrix.projection.toEpsgString()}</div>
         </div>
         <div className="debug__info">
-          <label className="debug__label">
-            <div className="debug__value">TileMatrix: {Config.map.tileMatrix.identifier}</div>
-          </label>
+          <label className="debug__label">TileMatrix </label>
+          <div className="debug__value">{Config.map.tileMatrix.identifier}</div>
         </div>
         <div className="debug__info">
-          <label className="debug__label">
-            <div className="debug__value">OSM:</div>
-          </label>
+          <label className="debug__label">OSM</label>
           <input className="osm__slider" type="range" min="0" max="1" step="0.05" value="0" onInput={this.adjustOSM} />
         </div>
         <div className="debug__info">
-          <label className="debug__label">
-            <div className="debug__value">Purple:</div>
-          </label>
+          <label className="debug__label">Purple</label>
           <input type="checkbox" onClick={this.togglePurple} />
         </div>
       </div>
