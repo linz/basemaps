02dc0622021df3de13e207f610dffe1b09f48703
Wentao Kuang <wkuang@linz.govt.nz>
2021-12-17 14:31:12 +1300

    Use path join to join the urls.

--- packages/lambda-tiler/src/routes/tile.ts
+++ packages/lambda-tiler/src/routes/tile.ts
@@ -13,6 +13,7 @@ import { TileEtag } from './tile.etag.js';
 import { Router } from '../router.js';
 import { ValidateTilePath } from '../validate.js';
 import { URL } from 'url';
+import path from 'path';
 
 export const TileComposer = new TileMakerSharp(256);
 
@@ -133,7 +134,7 @@ export const TileRoute = {
     for (const [key, value] of Object.entries(style.sources)) {
       if (value.type === 'vector') {
         if (value.url.startsWith('/')) {
-          const url = new URL(`${host}${value.url}`);
+          const url = new URL(path.join(host, value.url));
           url.searchParams.set('api', apiKey);
           value.url = url.toString().replace(/%7B/g, '{').replace(/%7D/g, '}');
         }
@@ -141,7 +142,7 @@ export const TileRoute = {
         for (let i = 0; i < value.tiles.length; i++) {
           const tile = value.tiles[i];
           if (tile.startsWith('/')) {
-            const url = new URL(`${host}${tile}`);
+            const url = new URL(path.join(host, tile));
             url.searchParams.set('api', apiKey);
             value.tiles[i] = url.toString().replace(/%7B/g, '{').replace(/%7D/g, '}');
           }
