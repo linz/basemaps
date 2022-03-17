a193e4172c63349e745e8bfcbb78ac919e52f1dd
Blayne Chard <bchard@linz.govt.nz>
2022-03-17 16:27:58 +1300

    fix(lambda-tiler): bundle the exact version of sharp from the yarn.lock  (#2114)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/lambda-tiler/scripts/create.deployment.package.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+/**
+ * Create a pacakge.json for the `dist` bundle based off the parent package.json
+ *
+ * This is needed as libsharp has to be "installed" into the dist node_modules so that lambda has access to sharp
+ */
+const fs = require('fs');
+
+const parentPackage = JSON.parse(fs.readFileSync('../package.json').toString());
+
+// Find the exact version of a package in the yarn lock
+function getPackageVersion(packageName) {
+  const parentLock = fs.readFileSync('../../../yarn.lock').toString().split('\n');
+
+  for (let i = 0; i < parentLock.length; i++) {
+    if (parentLock[i].startsWith(packageName + '@')) {
+      const versionList = parentLock[i + 1].trim();
+      if (!versionList.startsWith('version ')) throw new Error('Failed to find sharp version');
+      return JSON.parse(versionList.slice('version '.length));
+    }
+  }
+}
+
+// the bundle is a commonjs module
+parentPackage.type = 'commonjs';
+parentPackage.main = 'index.js';
+parentPackage.dependencies = { sharp: getPackageVersion('sharp') };
+
+// Clean up
+delete parentPackage.types;
+delete parentPackage.devDependencies;
+
+console.log('Installing dependencies', parentPackage.dependencies);
+fs.writeFileSync('./package.json', JSON.stringify(parentPackage, null, 2));
