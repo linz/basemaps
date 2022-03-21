ead4746871fac9610897f159f8dbb4c652f75c10
Blayne Chard <bchard@linz.govt.nz>
2022-03-21 13:00:04 +1300

    refactor: remove unused cli and switch to fastify as it simplifies the typing (#2123)

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,10 +1,10 @@
 import { handler } from '@basemaps/lambda-tiler';
-import { ALBEvent, ALBResult, APIGatewayProxyResultV2, CloudFrontRequestResult, Context } from 'aws-lambda';
-import ulid from 'ulid';
-import express from 'express';
 import { lf } from '@linzjs/lambda';
+import { ALBEvent, ALBResult, APIGatewayProxyResultV2, CloudFrontRequestResult, Context } from 'aws-lambda';
+import fastify from 'fastify';
+import ulid from 'ulid';
 
-export const BasemapsServer = express();
+export const BasemapsServer = fastify();
 
 function isAlbResult(r: ALBResult | CloudFrontRequestResult | APIGatewayProxyResultV2): r is ALBResult {
   if (typeof r !== 'object') return false;
@@ -14,27 +14,28 @@ function isAlbResult(r: ALBResult | CloudFrontRequestResult | APIGatewayProxyRes
 
 const instanceId = ulid.ulid();
 
-BasemapsServer.get('/v1/*', async (req: express.Request, res: express.Response) => {
+BasemapsServer.get<{ Querystring: { api: string } }>('/v1/*', async (req, res) => {
   const event: ALBEvent = {
     httpMethod: 'GET',
     requestContext: { elb: { targetGroupArn: 'arn:fake' } },
-    path: req.path,
+    path: req.url,
     headers: req.headers as Record<string, string>,
     queryStringParameters: req.query as Record<string, string>,
     body: null,
     isBase64Encoded: false,
   };
-  if (req.query['api'] == null) req.query['api'] = 'c' + instanceId;
+  if (req.query.api == null) req.query.api = 'c' + instanceId;
 
-  handler(event, {} as Context, (err, r) => {
+  handler(event, {} as Context, (err, r): void => {
     if (err || !isAlbResult(r)) {
       lf.Logger.fatal({ err }, 'RequestFailed');
-      return res.end(err);
+      res.send(err);
+      return;
     }
 
     res.status(r.statusCode);
     for (const [key, value] of Object.entries(r.headers ?? {})) res.header(key, String(value));
     if (r.body) res.send(Buffer.from(r.body, r.isBase64Encoded ? 'base64' : 'utf8'));
-    res.end();
+    else res.send('Not found');
   });
 });
