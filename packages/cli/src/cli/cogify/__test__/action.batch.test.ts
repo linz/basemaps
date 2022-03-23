bab5cefdc8365cb52bf687230a078ea599c359ce
Blayne Chard <bchard@linz.govt.nz>
2022-03-24 11:10:13 +1300

    fix(cli): allow unicode job names (#2126)
    AWS Batch does not allow unicode in job names so replace the job name with a hex digested hash

--- packages/cli/src/cli/cogify/__test__/action.batch.test.ts
+++ packages/cli/src/cli/cogify/__test__/action.batch.test.ts
@@ -1,5 +1,6 @@
 import o from 'ospec';
-import { extractResolutionFromName } from '../action.batch.js';
+import { CogJob } from '../../../cog/types.js';
+import { ActionBatchJob, extractResolutionFromName } from '../action.batch.js';
 
 o.spec('action.batch', () => {
   o('extractResolutionFromName', () => {
@@ -10,5 +11,26 @@ o.spec('action.batch', () => {
     o(extractResolutionFromName('wellington_urban_2017_0-10m')).equals(100);
     o(extractResolutionFromName('wellington_urban_2017_1.00m')).equals(1000);
     o(extractResolutionFromName('wellington_urban_2017_0.025m')).equals(25);
+  });
+
+  o('should create valid jobNames', () => {
+    const fakeJob = { id: '01FHRPYJ5FV1XAARZAC4T4K6MC', name: 'geographx_nz_texture_shade_2012_8-0m' } as CogJob;
+    o(ActionBatchJob.id(fakeJob, '0')).equals('01FHRPYJ5FV1XAARZAC4T4K6MC-9af5e139bbb3e502-0');
+
+    fakeJob.name = 'ōtorohanga_urban_2021_0.1m_RGB';
+    o(ActionBatchJob.id(fakeJob, '0')).equals('01FHRPYJ5FV1XAARZAC4T4K6MC-5294acface81c107-0');
+  });
+
+  o('should truncate job names to 128 characters', () => {
+    const fakeJob = { id: '01FHRPYJ5FV1XAARZAC4T4K6MC', name: 'geographx_nz_texture_shade_2012_8-0m' } as CogJob;
+
+    o(
+      ActionBatchJob.id(
+        fakeJob,
+        'this is a really long file name it should over flow 128 characters so it should be truncated at some point.tiff',
+      ),
+    ).equals(
+      '01FHRPYJ5FV1XAARZAC4T4K6MC-9af5e139bbb3e502-this is a really long file name it should over flow 128 characters so it should be t',
+    );
   });
 });
