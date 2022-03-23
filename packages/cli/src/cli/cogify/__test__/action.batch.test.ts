a4ec6f970e4558c3dcb5fef9fa9e1811d04d1694
Blayne Chard <bchard@linz.govt.nz>
2022-03-24 10:59:32 +1300

    test: add test for super long file names

--- packages/cli/src/cli/cogify/__test__/action.batch.test.ts
+++ packages/cli/src/cli/cogify/__test__/action.batch.test.ts
@@ -20,4 +20,17 @@ o.spec('action.batch', () => {
     fakeJob.name = 'ōtorohanga_urban_2021_0.1m_RGB';
     o(ActionBatchJob.id(fakeJob, '0')).equals('01FHRPYJ5FV1XAARZAC4T4K6MC-5294acface81c107-0');
   });
+
+  o('should truncate job names to 128 characters', () => {
+    const fakeJob = { id: '01FHRPYJ5FV1XAARZAC4T4K6MC', name: 'geographx_nz_texture_shade_2012_8-0m' } as CogJob;
+
+    o(
+      ActionBatchJob.id(
+        fakeJob,
+        'this is a really long file name it should over flow 128 characters so it should be truncated at some point.tiff',
+      ),
+    ).equals(
+      '01FHRPYJ5FV1XAARZAC4T4K6MC-9af5e139bbb3e502-this is a really long file name it should over flow 128 characters so it should be t',
+    );
+  });
 });
