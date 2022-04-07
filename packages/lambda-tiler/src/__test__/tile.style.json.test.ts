5c9beb3a8e312b35a20a096c46abb318bab600db
Blayne Chard <bchard@linz.govt.nz>
2022-04-07 15:37:47 +1200

    fix(lambda-tiler): do not destroy database config when serving style.json

--- packages/lambda-tiler/src/__test__/tile.style.json.test.ts
+++ packages/lambda-tiler/src/__test__/tile.style.json.test.ts
@@ -1,6 +1,7 @@
+import { StyleJson } from '@basemaps/config';
 import { Env } from '@basemaps/shared';
 import o from 'ospec';
-import { convertRelativeUrl } from '../routes/tile.style.json.js';
+import { convertRelativeUrl, convertStyleJson } from '../routes/tile.style.json.js';
 
 o.spec('TileStyleJson', () => {
   const host = 'https://tiles.test';
@@ -36,5 +37,58 @@ o.spec('TileStyleJson', () => {
 
   o('should not convert full urls', () => {
     o(convertRelativeUrl('https://foo.com/foo?bar=baz', 'abc')).equals('https://foo.com/foo?bar=baz');
+  });
+
+  const baseStyleJson: StyleJson = {
+    version: 8,
+    id: 'style.id',
+    name: 'style.name',
+    sources: {
+      vector: { type: 'vector', url: '/v1/tiles/topographic/EPSG:3857/tile.json' },
+      raster: { type: 'raster', tiles: ['/v1/tiles/aerial/EPSG:3857/{z}/{x}/{y}.webp'] },
+    },
+    layers: [],
+    metadata: {},
+    glyphs: '/v1/glyphs',
+    sprite: '/v1/sprites',
+  };
+
+  o('should not destroy the original configuration', () => {
+    const apiKey = 'abc123';
+    const converted = convertStyleJson(baseStyleJson, apiKey);
+
+    console.log(converted);
+
+    o(converted.sources.vector).deepEquals({
+      type: 'vector',
+      url: 'https://tiles.test/v1/tiles/topographic/EPSG:3857/tile.json?api=abc123',
+    });
+    o(converted.sources.raster).deepEquals({
+      type: 'raster',
+      tiles: ['https://tiles.test/v1/tiles/aerial/EPSG:3857/{z}/{x}/{y}.webp?api=abc123'],
+    });
+
+    o(JSON.stringify(baseStyleJson).includes(apiKey)).equals(false);
+
+    const convertedB = convertStyleJson(baseStyleJson, '0x1234');
+    o(convertedB.sources.vector).deepEquals({
+      type: 'vector',
+      url: 'https://tiles.test/v1/tiles/topographic/EPSG:3857/tile.json?api=0x1234',
+    });
+    o(convertedB.sources.raster).deepEquals({
+      type: 'raster',
+      tiles: ['https://tiles.test/v1/tiles/aerial/EPSG:3857/{z}/{x}/{y}.webp?api=0x1234'],
+    });
+
+    o(JSON.stringify(baseStyleJson).includes('0x1234')).equals(false);
+  });
+
+  o('should convert relative glyphs and sprites', () => {
+    const apiKey = '0x9f9f';
+    const converted = convertStyleJson(baseStyleJson, apiKey);
+    o(converted.sprite).equals('https://tiles.test/v1/sprites');
+    o(converted.glyphs).equals('https://tiles.test/v1/glyphs');
+
+    o(JSON.stringify(baseStyleJson).includes(apiKey)).equals(false);
   });
 });
--- packages/lambda-tiler/src/routes/tile.style.json.ts
+++ packages/lambda-tiler/src/routes/tile.style.json.ts
@@ -23,6 +23,37 @@ export function convertRelativeUrl(url?: string, apiKey?: string): string {
   return fullUrl.toString().replace(/%7B/g, '{').replace(/%7D/g, '}');
 }
 
+/**
+ * Create a new style json that has absolute urls to the current host and  API Keys where required
+ * @param style style to update
+ * @param apiKey api key to inject
+ * @returns new stylejson
+ */
+export function convertStyleJson(style: StyleJson, apiKey: string): StyleJson {
+  const sources: Sources = JSON.parse(JSON.stringify(style.sources));
+  for (const [key, value] of Object.entries(sources)) {
+    if (value.type === 'vector') {
+      value.url = convertRelativeUrl(value.url, apiKey);
+    } else if (value.type === 'raster' && Array.isArray(value.tiles)) {
+      for (let i = 0; i < value.tiles.length; i++) {
+        value.tiles[i] = convertRelativeUrl(value.tiles[i], apiKey);
+      }
+    }
+    sources[key] = value;
+  }
+
+  return {
+    version: 8,
+    id: style.id,
+    name: style.name,
+    sources,
+    layers: style.layers,
+    metadata: style.metadata ?? {},
+    glyphs: convertRelativeUrl(style.glyphs),
+    sprite: convertRelativeUrl(style.sprite),
+  } as StyleJson;
+}
+
 export async function styleJson(req: LambdaHttpRequest, fileName: string): Promise<LambdaHttpResponse> {
   const apiKey = Router.apiKey(req);
   if (apiKey == null) return new LambdaHttpResponse(400, 'Invalid API Key.');
@@ -34,35 +65,8 @@ export async function styleJson(req: LambdaHttpRequest, fileName: string): Promi
   if (styleConfig == null) return NotFound;
 
   // Prepare sources and add linz source
-  const style = styleConfig.style;
-  const sources: Sources = {};
-  for (const [key, value] of Object.entries(style.sources)) {
-    if (value.type === 'vector') {
-      value.url = convertRelativeUrl(value.url, apiKey);
-    } else if (value.type === 'raster' && Array.isArray(value.tiles)) {
-      for (let i = 0; i < value.tiles.length; i++) {
-        value.tiles[i] = convertRelativeUrl(value.tiles[i], apiKey);
-      }
-    }
-    sources[key] = value;
-  }
-
-  // prepare Style.json
-  const styleJson: StyleJson = {
-    /** Style.json version 8 */
-    version: 8,
-    id: style.id,
-    name: style.name,
-    sources,
-    layers: style.layers,
-    metadata: style.metadata || {},
-    glyphs: convertRelativeUrl(style.glyphs),
-    sprite: convertRelativeUrl(style.sprite),
-  };
-
-  const json = JSON.stringify(styleJson);
-
-  const data = Buffer.from(json);
+  const style = convertStyleJson(styleConfig.style, apiKey);
+  const data = Buffer.from(JSON.stringify(style));
 
   const cacheKey = createHash('sha256').update(data).digest('base64');
 
