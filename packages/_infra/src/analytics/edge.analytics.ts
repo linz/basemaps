e15fbcada9ae990578d2a238a2b2baf7ec7fd992
Blayne Chard <bchard@linz.govt.nz>
2022-04-19 14:01:34 +1200

    refactor: use lf name space as "Function" is a horrible type name

--- packages/_infra/src/analytics/edge.analytics.ts
+++ packages/_infra/src/analytics/edge.analytics.ts
@@ -2,7 +2,7 @@ import { Env } from '@basemaps/shared';
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import lf from 'aws-cdk-lib/aws-lambda';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
@@ -18,7 +18,7 @@ export interface EdgeAnalyticsProps extends StackProps {
  * Every hour create analytics based off the logs given to us from cloudwatch
  */
 export class EdgeAnalytics extends Stack {
-  public lambda: Function;
+  public lambda: lf.Function;
 
   public constructor(scope: Construct, id: string, props: EdgeAnalyticsProps) {
     super(scope, id, props);
@@ -28,12 +28,12 @@ export class EdgeAnalytics extends Stack {
     const logBucket = Bucket.fromBucketName(this, 'EdgeLogBucket', logBucketName);
 
     const cacheBucket = new Bucket(this, 'AnalyticCacheBucket');
-    this.lambda = new Function(this, 'AnalyticLambda', {
-      runtime: Runtime.NODEJS_14_X,
+    this.lambda = new lf.Function(this, 'AnalyticLambda', {
+      runtime: lf.Runtime.NODEJS_14_X,
       memorySize: 2048,
       timeout: Duration.minutes(10),
       handler: 'index.handler',
-      code: Code.fromAsset(CODE_PATH),
+      code: lf.Code.fromAsset(CODE_PATH),
       environment: {
         [Env.Analytics.CloudFrontId]: distributionId,
         [Env.Analytics.CacheBucket]: `s3://${cacheBucket.bucketName}`,
