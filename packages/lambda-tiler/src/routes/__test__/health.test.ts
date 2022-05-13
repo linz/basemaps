9ef9a1be5aa362ede9a261ab5a1b6689bd73063b
Blayne Chard <bchard@linz.govt.nz>
2022-05-13 14:02:16 +1200

    feat(lambda-tiler): include some basic vector checks in the /v1/health endpoint
    BREAKING_CHANGE: this requires a "health-vector" and "health-raster"
    tileset to be configured

--- packages/lambda-tiler/src/routes/__test__/health.test.ts
+++ packages/lambda-tiler/src/routes/__test__/health.test.ts
@@ -1,12 +1,15 @@
+import { ConfigTileSetVector } from '@basemaps/config';
 import { GoogleTms } from '@basemaps/geo';
-import { LogConfig } from '@basemaps/shared';
+import { Env, LogConfig } from '@basemaps/shared';
 import { LambdaAlbRequest, LambdaHttpRequest, LambdaHttpResponse } from '@linzjs/lambda';
 import { Context } from 'aws-lambda';
 import o from 'ospec';
 import sinon from 'sinon';
 import { TileSets } from '../../tile.set.cache.js';
+import { TileSet } from '../../tile.set.js';
 import { TileSetRaster } from '../../tile.set.raster.js';
-import { getTestBuffer, Health, TestTiles } from '../health.js';
+import { TileSetVector } from '../../tile.set.vector.js';
+import { getTestBuffer, Health, healthRaster, healthVector, TestRasterTiles } from '../health.js';
 
 const ctx: LambdaHttpRequest = new LambdaAlbRequest(
   {
@@ -24,64 +27,113 @@ o.spec('health', async () => {
   o.specTimeout(1000);
   const sandbox = sinon.createSandbox();
 
-  const tileSet = new TileSetRaster('health', GoogleTms);
+  const tileSet = new TileSetRaster('health-raster', GoogleTms);
+  const tileSetVector = new TileSetVector('health-vector', GoogleTms);
+  const Response1 = new LambdaHttpResponse(200, 'ok');
+  const Response2 = new LambdaHttpResponse(200, 'ok');
+  o.before(async () => {
+    const testTileFile1 = await getTestBuffer(TestRasterTiles[0]);
+    Response1.buffer(testTileFile1);
+    const testTileFile2 = await getTestBuffer(TestRasterTiles[1]);
+    Response2.buffer(testTileFile2);
+  });
+
+  const startingUrlBase = process.env[Env.PublicUrlBase];
   o.beforeEach(() => {
-    sandbox.stub(TileSets, 'get').resolves(tileSet);
+    // Ensure no public URL base is set
+    delete process.env[Env.PublicUrlBase];
+    tileSetVector.init({ maxZoom: 15 } as ConfigTileSetVector);
+    tileSet.init({ layers: [] } as any);
+    sandbox.stub(TileSets, 'get').callsFake(async (name): Promise<TileSet> => {
+      if (name === tileSet.fullName) return tileSet;
+      if (name === tileSetVector.fullName) return tileSetVector;
+      throw new Error('Invalid tile set: ' + name);
+    });
   });
 
   o.afterEach(() => {
+    process.env[Env.PublicUrlBase] = startingUrlBase;
     sandbox.restore();
   });
 
-  o('Should return bad response', async () => {
-    // Given ... a bad get tile response
-    const BadResponse = new LambdaHttpResponse(500, 'Can not get Tile Set.');
-    sandbox.stub(tileSet, 'tile').resolves(BadResponse);
-
-    // When ...
-    const res = await Health(ctx);
-
-    // Then ...
-    o(res.status).equals(500);
-    o(res.statusDescription).equals('Can not get Tile Set.');
-  });
-
-  const Response1 = new LambdaHttpResponse(200, 'ok');
-  const Response2 = new LambdaHttpResponse(200, 'ok');
-
-  o.before(async () => {
-    const testTileFile1 = await getTestBuffer(TestTiles[0]);
-    Response1.buffer(testTileFile1);
-    const testTileFile2 = await getTestBuffer(TestTiles[1]);
-    Response2.buffer(testTileFile2);
-  });
-  // Prepare mock test tile response based on the static test tiles
-
-  o('Should give a 200 response', async () => {
-    // Given ... a series good get tile response
+  o('should run health', async () => {
     const callback = sandbox.stub(tileSet, 'tile');
     callback.onCall(0).resolves(Response1);
     callback.onCall(1).resolves(Response2);
 
-    // When ...
     const res = await Health(ctx);
 
-    // Then ...
     o(res.status).equals(200);
-    o(res.statusDescription).equals('ok');
   });
 
-  o('Should return mis-match tile response', async () => {
-    // Given ... a bad get tile response for second get tile
-    const callback = sandbox.stub(tileSet, 'tile');
-    callback.onCall(0).resolves(Response1);
-    callback.onCall(1).resolves(Response1);
+  o.spec('healthVector', () => {
+    o('should validate with health-vector', async () => {
+      const res = await healthVector(ctx);
+      o(res).equals(undefined);
+    });
 
-    // When ...
-    const res = await Health(ctx);
+    o('should validate tile json', async () => {
+      delete tileSetVector.tileSet.maxZoom;
 
-    // Then ...
-    o(res.status).equals(500);
-    o(res.statusDescription).equals('TileSet does not match.');
+      const res = await healthVector(ctx);
+
+      o(res?.status).equals(500);
+      o(res?.statusDescription).equals('Vector.TileJSON: missing max zoom');
+    });
+  });
+
+  o.spec('healthRaster', () => {
+    o('Should return bad response', async () => {
+      // Given ... a bad get tile response
+      const BadResponse = new LambdaHttpResponse(500, 'Can not get Tile Set.');
+      sandbox.stub(tileSet, 'tile').resolves(BadResponse);
+
+      // When ...
+      const res = await healthRaster(ctx);
+
+      // Then ...
+      o(res?.status).equals(500);
+      o(res?.statusDescription).equals('Can not get Tile Set.');
+    });
+
+    // Prepare mock test tile response based on the static test tiles
+
+    o('Should give a 200 response', async () => {
+      // Given ... a series good get tile response
+      const callback = sandbox.stub(tileSet, 'tile');
+      callback.onCall(0).resolves(Response1);
+      callback.onCall(1).resolves(Response2);
+
+      // When ...
+      const res = await healthRaster(ctx);
+
+      // Then ...
+      o(res).equals(undefined);
+    });
+
+    o('Should return mis-match tile response', async () => {
+      // Given ... a bad get tile response for second get tile
+      const callback = sandbox.stub(tileSet, 'tile');
+      callback.onCall(0).resolves(Response1);
+      callback.onCall(1).resolves(Response1);
+
+      // When ...
+      const res = await healthRaster(ctx);
+
+      // Then ...
+      o(res?.status).equals(500);
+      o(res?.statusDescription).equals('Raster: Tile "tile_NZTM2000Quad_30_33_z6.png" does not match.');
+    });
+
+    o('should error if invalid tilejson', async () => {
+      const callback = sandbox.stub(tileSet, 'tile');
+      callback.onCall(0).resolves(Response1);
+      callback.onCall(1).resolves(Response2);
+
+      tileSet.tileSet.format = 'fake' as any;
+      const res = await healthRaster(ctx);
+      o(res?.status).equals(500);
+      o(res?.statusDescription.startsWith('Raster.TileJSON: invalid tileUrl /v1/tiles')).equals(true);
+    });
   });
 });
--- packages/lambda-tiler/src/routes/health.ts
+++ packages/lambda-tiler/src/routes/health.ts
@@ -1,18 +1,27 @@
-import { GoogleTms, Nztm2000QuadTms, ImageFormat } from '@basemaps/geo';
+import { GoogleTms, Nztm2000QuadTms, ImageFormat, TileJson } from '@basemaps/geo';
 import { TileDataXyz, TileType } from '@basemaps/shared';
-import { HttpHeader, LambdaHttpRequest, LambdaHttpResponse } from '@linzjs/lambda';
+import {
+  HttpHeader,
+  HttpHeaderRequestId,
+  LambdaAlbRequest,
+  LambdaHttpRequest,
+  LambdaHttpResponse,
+} from '@linzjs/lambda';
+import { ALBEvent } from 'aws-lambda';
+import { Context } from 'aws-lambda/handler';
 import * as fs from 'fs';
 import * as path from 'path';
 import PixelMatch from 'pixelmatch';
 import Sharp from 'sharp';
 import url from 'url';
 import { TileSets } from '../tile.set.cache.js';
+import { tileJson } from './tile.json.js';
 
 interface TestTile extends TileDataXyz {
   buf?: Buffer;
 }
 
-export const TestTiles: TestTile[] = [
+export const TestRasterTiles: TestTile[] = [
   { type: TileType.Tile, name: 'health', tileMatrix: GoogleTms, ext: ImageFormat.Png, x: 252, y: 156, z: 8 },
   { type: TileType.Tile, name: 'health', tileMatrix: Nztm2000QuadTms, ext: ImageFormat.Png, x: 30, y: 33, z: 6 },
 ];
@@ -41,23 +50,18 @@ export async function updateExpectedTile(test: TestTile, newTileData: Buffer, di
   await fs.promises.writeFile(`${expectedFileName}.diff.png`, imgPng);
 }
 
-/**
- * Health request get health TileSets and validate with test TileSets
- * - Valid response from get heath tile request
- * - Valid tile get from the request
- *
- * @throws LambdaHttpResponse for failure health test
- */
-export async function Health(req: LambdaHttpRequest): Promise<LambdaHttpResponse> {
-  for (const test of TestTiles) {
-    const tileSet = await TileSets.get('health', test.tileMatrix);
-    if (tileSet == null) throw new LambdaHttpResponse(500, 'TileSet: "health" not found');
+/** Validate the raster service by creating a number of raster tiles */
+export async function healthRaster(req: LambdaHttpRequest): Promise<LambdaHttpResponse | void> {
+  for (const test of TestRasterTiles) {
+    const tileSet = await TileSets.get('health-raster', test.tileMatrix);
+    if (tileSet == null) throw new LambdaHttpResponse(500, 'TileSet: "health-raster" not found');
     // Get the parse response tile to raw buffer
     const response = await tileSet.tile(req, test);
     if (response.status !== 200) return new LambdaHttpResponse(500, response.statusDescription);
-    if (!Buffer.isBuffer(response._body)) throw new LambdaHttpResponse(500, 'Not a Buffer response content.');
+    if (!Buffer.isBuffer(response._body)) throw new LambdaHttpResponse(500, 'Raster: Not a Buffer response content.');
     const resImgBuffer = await Sharp(response._body).raw().toBuffer();
 
+    const tileId = `tile_${test.tileMatrix.identifier}_${test.x}_${test.y}_z${test.z}.${test.ext}`;
     // Get test tile to compare
     const testBuffer = await getTestBuffer(test);
     test.buf = testBuffer;
@@ -72,9 +76,83 @@ export async function Health(req: LambdaHttpRequest): Promise<LambdaHttpResponse
         { missMatchedPixels, projection: test.tileMatrix.identifier, xyz: { x: test.x, y: test.y, z: test.z } },
         'Health:MissMatch',
       );
-      return new LambdaHttpResponse(500, 'TileSet does not match.');
+      return new LambdaHttpResponse(500, `Raster: Tile "${tileId}" does not match.`);
     }
   }
+
+  // Validate TileJson for both NZTM and Google
+  for (const tileMatrix of [GoogleTms, Nztm2000QuadTms]) {
+    const tilePathPrefix = `/v1/tiles/health-raster/${tileMatrix.projection.toEpsgString()}`;
+    const tileRequest = createRequest(req, `${tilePathPrefix}/tile.json`);
+    const tileJsonResponse = await tileJson(tileRequest);
+    if (tileJsonResponse.status !== 200) {
+      return new LambdaHttpResponse(500, `Raster.TileJSON: Failed to generate tile.json for ${tileMatrix.identifier}`);
+    }
+    const tileJsonData: TileJson = loadJson(tileJsonResponse);
+    const [tileUrl] = tileJsonData.tiles;
+    if (tileUrl == null) {
+      return new LambdaHttpResponse(500, `Raster.TileJSON: Missing tileUrl ${tileMatrix.identifier}`);
+    }
+    if (!tileUrl.endsWith(`.webp?api=c${req.correlationId}`)) {
+      return new LambdaHttpResponse(500, `Raster.TileJSON: invalid tileUrl ${tileUrl}`);
+    }
+  }
+}
+
+function loadJson<T>(res: LambdaHttpResponse): T {
+  return JSON.parse(res.isBase64Encoded ? Buffer.from(res.body, 'base64').toString() : res.body);
+}
+
+/** Create a fake sub request to pass to internal routes */
+function createRequest(req: LambdaHttpRequest, path: string): LambdaHttpRequest {
+  return new LambdaAlbRequest(
+    {
+      httpMethod: 'get',
+      path: path,
+      body: null,
+      isBase64Encoded: false,
+      // Ensure the sub request has the same trace id
+      headers: { [HttpHeaderRequestId.CorrelationId]: req.correlationId },
+      queryStringParameters: { api: `c${req.correlationId}` },
+    } as unknown as ALBEvent,
+    {} as Context,
+    req.log,
+  );
+}
+
+/** Validate vector service is generating tile json  */
+export async function healthVector(req: LambdaHttpRequest): Promise<LambdaHttpResponse | void> {
+  const tileSet = await TileSets.get('health-vector', GoogleTms);
+  if (tileSet == null) throw new LambdaHttpResponse(500, 'TileSet: "health-vector" not found');
+
+  // Validate TileJSON
+  const tilePathPrefix = `/v1/tiles/health-vector/${GoogleTms.projection.toEpsgString()}`;
+  const tileRequest = createRequest(req, `${tilePathPrefix}/tile.json`);
+  const tileJsonResponse = await tileJson(tileRequest);
+  if (tileJsonResponse.status !== 200) return new LambdaHttpResponse(500, 'Vector: Failed to generate tile.json');
+  const tileJsonData: TileJson = loadJson(tileJsonResponse);
+
+  // Vector tiles should always have a max zoom
+  if (tileJsonData.maxzoom == null) return new LambdaHttpResponse(500, 'Vector.TileJSON: missing max zoom');
+  // Tiles should always point to a sub path
+  const [tileUrl] = tileJsonData.tiles;
+  // There should only be one url that is a sub path of the tile.json and should be served as a protobuf file
+  if (tileJsonData.tiles.length !== 1 || !tileUrl.startsWith(tilePathPrefix) || !tileUrl.includes('.pbf')) {
+    return new LambdaHttpResponse(500, 'Vector.TileJSON: invalid tile path ' + tileUrl);
+  }
+}
+
+/**
+ * Health request get health TileSets and validate with test TileSets
+ * - Valid response from get heath tile request
+ * - Valid tile get from the request
+ *
+ * @throws LambdaHttpResponse for failure health test
+ */
+export async function Health(req: LambdaHttpRequest): Promise<LambdaHttpResponse> {
+  const healthChecks = await Promise.all([healthRaster(req), healthVector(req)]);
+  const firstFailedCheck = healthChecks.find((f) => f != null);
+  if (firstFailedCheck) return firstFailedCheck;
 
   // Return Ok response when all health test passed.
   const OkResponse = new LambdaHttpResponse(200, 'ok');
