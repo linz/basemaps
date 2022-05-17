c62891ff6a049edc55359c24d4998c083d96afc0
Blayne Chard <bchard@linz.govt.nz>
2022-05-18 09:39:19 +1200

    refactor: expose a function to create server not the variable

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,15 +1,13 @@
 import { handler } from '@basemaps/lambda-tiler';
-import { LogConfig } from '@basemaps/shared';
+import { LogType } from '@basemaps/shared';
 import fastifyStatic from '@fastify/static';
 import { lf } from '@linzjs/lambda';
 import { ALBEvent, ALBResult, APIGatewayProxyResultV2, CloudFrontRequestResult, Context } from 'aws-lambda';
-import fastify from 'fastify';
+import fastify, { FastifyInstance } from 'fastify';
 import { createRequire } from 'module';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import ulid from 'ulid';
 import { URL } from 'url';
-
-export const BasemapsServer = fastify();
 
 function isAlbResult(r: ALBResult | CloudFrontRequestResult | APIGatewayProxyResultV2): r is ALBResult {
   if (typeof r !== 'object') return false;
@@ -19,37 +17,45 @@ function isAlbResult(r: ALBResult | CloudFrontRequestResult | APIGatewayProxyRes
 
 const instanceId = ulid.ulid();
 
-const require = createRequire(import.meta.url);
-const landingLocation = require.resolve('@basemaps/landing/dist');
-if (landingLocation == null) {
-  LogConfig.get().warn('Failed to find @basemaps/landing location');
-} else {
-  BasemapsServer.register(fastifyStatic, { root: dirname(landingLocation) });
-}
+export function createServer(logger: LogType): FastifyInstance {
+  const BasemapsServer = fastify();
 
-BasemapsServer.get<{ Querystring: { api: string } }>('/v1/*', async (req, res) => {
-  const url = new URL(`${req.protocol}://${req.hostname}${req.url}`);
-  const event: ALBEvent = {
-    httpMethod: 'GET',
-    requestContext: { elb: { targetGroupArn: 'arn:fake' } },
-    path: url.pathname,
-    headers: req.headers as Record<string, string>,
-    queryStringParameters: req.query as Record<string, string>,
-    body: null,
-    isBase64Encoded: false,
-  };
-  if (req.query.api == null) req.query.api = 'c' + instanceId;
+  const require = createRequire(import.meta.url);
+  const landingLocation = require.resolve('@basemaps/landing');
+  if (landingLocation == null) {
+    logger.warn('Server:Landing:Failed');
+  } else {
+    const root = path.join(path.dirname(landingLocation), '..', 'dist');
+    logger.info({ path: root }, 'Server:Landing');
+    BasemapsServer.register(fastifyStatic, { root });
+  }
 
-  handler(event, {} as Context, (err, r): void => {
-    if (err || !isAlbResult(r)) {
-      lf.Logger.fatal({ err }, 'RequestFailed');
-      res.send(err);
-      return;
-    }
+  BasemapsServer.get<{ Querystring: { api: string } }>('/v1/*', async (req, res) => {
+    const url = new URL(`${req.protocol}://${req.hostname}${req.url}`);
+    const event: ALBEvent = {
+      httpMethod: 'GET',
+      requestContext: { elb: { targetGroupArn: 'arn:fake' } },
+      path: url.pathname,
+      headers: req.headers as Record<string, string>,
+      queryStringParameters: req.query as Record<string, string>,
+      body: null,
+      isBase64Encoded: false,
+    };
+    if (req.query.api == null) req.query.api = 'c' + instanceId;
 
-    res.status(r.statusCode);
-    for (const [key, value] of Object.entries(r.headers ?? {})) res.header(key, String(value));
-    if (r.body) res.send(Buffer.from(r.body, r.isBase64Encoded ? 'base64' : 'utf8'));
-    else res.send('Not found');
+    handler(event, {} as Context, (err, r): void => {
+      if (err || !isAlbResult(r)) {
+        lf.Logger.fatal({ err }, 'RequestFailed');
+        res.send(err);
+        return;
+      }
+
+      res.status(r.statusCode);
+      for (const [key, value] of Object.entries(r.headers ?? {})) res.header(key, String(value));
+      if (r.body) res.send(Buffer.from(r.body, r.isBase64Encoded ? 'base64' : 'utf8'));
+      else res.send('Not found');
+    });
   });
-});
+
+  return BasemapsServer;
+}
