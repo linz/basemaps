e2aef5e460e059a7f8f59cc143a606255d7d77e0
Wentao Kuang <wkuang@linz.govt.nz>
2022-05-26 15:13:08 +1200

    feat(cli): Make a sseperate folder for each tiff for group cogs batch job. (#2213)

--- packages/cli/src/cli/folder.ts
+++ packages/cli/src/cli/folder.ts
@@ -10,3 +10,11 @@ export async function makeTempFolder(folder: string): Promise<string> {
   await fs.mkdir(folderPath, { recursive: true });
   return folderPath;
 }
+
+/** Make a tiff folder inside TEMP_FOLDER's path */
+export async function makeTiffFolder(tmpFolder: string, name: string): Promise<string> {
+  const folderPath = path.join(tmpFolder, name);
+
+  await fs.mkdir(folderPath, { recursive: true });
+  return folderPath;
+}
