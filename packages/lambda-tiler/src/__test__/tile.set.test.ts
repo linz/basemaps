4cd49487dddc3a70123437d1644ff3e22d9dc12f
Blayne Chard <bchard@linz.govt.nz>
2022-06-01 09:26:48 +1200

    feat(lambda-tiler): tiff paths should allow trailing slashes (#2223)
    * feat(lambda-tiler): tiff paths should allow trailing slashes
    
    * refactor: remove console.log

--- packages/lambda-tiler/src/__test__/tile.set.test.ts
+++ packages/lambda-tiler/src/__test__/tile.set.test.ts
@@ -1,15 +1,8 @@
-import { ConfigImagery } from '@basemaps/config';
 import { GoogleTms, Nztm2000Tms } from '@basemaps/geo';
 import o from 'ospec';
 import { TileSetRaster } from '../tile.set.raster.js';
 
 o.spec('tile.set', () => {
-  o('basePath', () => {
-    const rec = { uri: 's3://test-bucket/3857/aerail/job123' } as ConfigImagery;
-    o(TileSetRaster.basePath(rec)).equals('s3://test-bucket/3857/aerail/job123');
-    o(TileSetRaster.basePath(rec, '31223')).equals('s3://test-bucket/3857/aerail/job123/31223.tiff');
-  });
-
   o('extent', () => {
     o(new TileSetRaster('google', GoogleTms).extent.toBbox()).deepEquals([
       -20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892,
