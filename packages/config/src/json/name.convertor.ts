5c629c225c95b3697f1f32e6319121c493bac90c
Blayne Chard <blayne@chard.com>
2022-07-07 10:35:34 +1200

    feat(config): make layer names more standard

---
+++ packages/config/src/json/name.convertor.ts
@@ -0,0 +1,41 @@
+/** Ensure GSDs are listed with a "." not "-" eg 0-10m to 0.10m */
+function fixChunkGsd(chunk: string): string | null {
+  if (chunk.endsWith('m') && chunk.startsWith('0-')) return chunk.replace('0-', '0.');
+  return null;
+}
+/** convert a 2022-23 year into a full year 2022-2023 */
+function fixYear(chunk: string): string | null {
+  const parts = chunk.split('-');
+  if (parts.length !== 2) return null;
+  if (parts[0].length !== 4) return null;
+  if (parts[1].length === 4) return chunk; // 2022-2023
+  // 2022-23
+  if (parts[1].length === 2) return `${parts[0]}-${parts[0].slice(0, 2)}${parts[1]}`; // 2022-2023
+  return null;
+}
+
+const chunkFixes = [fixChunkGsd, fixYear];
+
+export function standardizeLayerName(name: string): string {
+  const chunks = new Set(name.split('_'));
+  // Remove RGBA/RGBI etc..
+  chunks.delete('RGB');
+  chunks.delete('RGBI');
+  chunks.delete('RGBA');
+
+  const output = [];
+  for (const chunk of chunks) {
+    let fixed = false;
+    for (const fixer of chunkFixes) {
+      const ret = fixer(chunk);
+      if (ret == null) continue;
+      fixed = true;
+      output.push(ret);
+      break;
+    }
+
+    if (!fixed) output.push(chunk);
+  }
+
+  return output.join('-');
+}
