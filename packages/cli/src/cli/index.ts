d7dd830968647a8d11aefcd3f28325a0585ddff7
Wentao Kuang <wkuang@linz.govt.nz>
2022-07-07 14:36:14 +1200

    New Cli to bundle the assets into cotar file.

--- packages/cli/src/cli/index.ts
+++ packages/cli/src/cli/index.ts
@@ -4,6 +4,7 @@ import 'source-map-support/register.js';
 import { CommandList } from './aws/action.aws.list.js';
 import { CommandCogCreate } from './cogify/action.cog.js';
 import { CommandJobCreate } from './cogify/action.job.js';
+import { CommandBundleAssets } from './config/action.bundle.assets.js';
 import { CommandBundle } from './config/action.bundle.js';
 import { CommandImport } from './config/action.import.js';
 import { CommandScreenShot } from './screenshot/action.screenshot.js';
@@ -20,6 +21,7 @@ export class BasemapsConfigCommandLine extends BaseCommandLine {
     this.addAction(new CommandJobCreate());
 
     this.addAction(new CommandBundle());
+    this.addAction(new CommandBundleAssets());
     this.addAction(new CommandImport());
 
     this.addAction(new CommandScreenShot());
