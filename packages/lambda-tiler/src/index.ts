ab5dac63324975857853cce7dc634438ca9d85ad
Blayne Chard <blayne@chard.com>
2022-07-08 10:05:13 +1200

    feat(lambda-tiler): prefer using route handler for managing routes

--- packages/lambda-tiler/src/index.ts
+++ packages/lambda-tiler/src/index.ts
@@ -1,59 +1,52 @@
-import { lf, LambdaHttpRequest, LambdaHttpResponse } from '@linzjs/lambda';
 import { LogConfig } from '@basemaps/shared';
-import { Ping, Version } from './routes/api.js';
-import { Health } from './routes/health.js';
-import { Tiles } from './routes/tile.js';
-import { Router } from './router.js';
+import { LambdaHttpRequest, LambdaHttpResponse, lf } from '@linzjs/lambda';
 import { createHash } from 'crypto';
-import { Imagery } from './routes/imagery.js';
-import { Esri } from './routes/esri/rest.js';
-import { St } from './source.tracer.js';
-import { spriteGet } from './routes/sprites.js';
+import { Router } from './router.js';
+import { Ping, Version } from './routes/api.js';
 import { fontGet, fontList } from './routes/fonts.js';
+import { Health } from './routes/health.js';
+import { imageryGet } from './routes/imagery.js';
+import { spriteGet } from './routes/sprites.js';
+import { Tiles } from './routes/tile.js';
+import { St } from './source.tracer.js';
 
 const app = new Router();
 
-app.get('ping', Ping);
-app.get('health', Health);
-app.get('version', Version);
 app.get('tiles', Tiles);
-app.get('imagery', Imagery);
-app.get('esri', Esri);
 
-let slowTimer: NodeJS.Timer | null = null;
 export async function handleRequest(req: LambdaHttpRequest): Promise<LambdaHttpResponse> {
-  // Reset the request tracing
-  St.reset();
-
-  // Warn if a request takes more than 10 seconds to process
-  if (slowTimer) clearTimeout(slowTimer);
-  slowTimer = setTimeout(() => req.log.warn(req.logContext, 'Lambda:Slow'), 10_000);
-  slowTimer.unref();
-
-  req.set('name', 'LambdaTiler');
-  try {
-    const apiKey = Router.apiKey(req);
-    if (apiKey != null) {
-      const apiKeyHash = createHash('sha256').update(apiKey).digest('base64');
-      req.set('api', apiKeyHash);
-    }
-    const ret = await app.handle(req);
-
-    // TODO this could be relaxed to every say 5% of requests if logging gets too verbose.
-    req.set('requests', St.requests.slice(0, 100)); // limit to 100 requests (some tiles need 100s of requests)
-    req.set('requestCount', St.requests.length);
-
-    return ret;
-  } finally {
-    if (slowTimer) clearTimeout(slowTimer);
-    slowTimer = null;
+  const apiKey = Router.apiKey(req);
+  if (apiKey != null) {
+    const apiKeyHash = createHash('sha256').update(apiKey).digest('base64');
+    req.set('api', apiKeyHash);
   }
+  return await app.handle(req);
 }
 
 export const handler = lf.http(LogConfig.get());
+
+handler.router.hook('request', (req) => {
+  req.set('name', 'LambdaTiler');
+  // Reset the request tracing before every request
+  St.reset();
+});
+
+handler.router.hook('response', (req) => {
+  if (St.requests.length > 0) {
+    // TODO this could be relaxed to every say 5% of requests if logging gets too verbose.
+    req.set('requests', St.requests.slice(0, 100)); // limit to 100 requests (some tiles need 100s of requests)
+    req.set('requestCount', St.requests.length);
+  }
+});
+
+handler.router.get('/v1/ping', Ping);
+handler.router.get('/v1/health', Health);
+handler.router.get('/v1/version', Version);
+handler.router.get('/v1/imagery/:imageryId/:fileName', imageryGet);
 
 handler.router.get('/v1/sprites/:spriteName', spriteGet);
 handler.router.get('/v1/fonts.json', fontList);
 handler.router.get('/v1/fonts/:fontStack/:range.pbf', fontGet);
 
+// Catch all for old requests
 handler.router.get('*', handleRequest);
