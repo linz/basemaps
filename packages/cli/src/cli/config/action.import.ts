39eba95b1f297f794592d9eddef10b05b5bfe08b
Blayne Chard <bchard@linz.govt.nz>
2022-07-28 16:20:16 +1200

    fix(cli): do not trace everything by default (#2383)

--- packages/cli/src/cli/config/action.import.ts
+++ packages/cli/src/cli/config/action.import.ts
@@ -45,7 +45,6 @@ export class CommandImport extends CommandLineAction {
 
   async onExecute(): Promise<void> {
     const logger = LogConfig.get();
-    logger.level = 'trace';
     const commit = this.commit.value ?? false;
     const config = this.config.value;
     const backup = this.backup.value;
