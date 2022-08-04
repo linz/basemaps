c145f283bf5875d5e7b15909cc37811b029303f4
Wentao Kuang <wkuang@linz.govt.nz>
2022-08-04 14:29:02 +1200

    feat(lambda-tiler): Assets provider to get assets from any location. (#2374)
    * Squash into one commit
    
    * Fix the AssetProvider.set fuction to be sync
    
    * Add assets path into config bundle record instead of config bunlde json.
    
    * Server assets from config  url query.
    
    * Revert "Add assets path into config bundle record instead of config bunlde json."
    
    This reverts commit 2935d5bc7b494d60aef0b15d65864f4974ff5487.
    
    * Cache the config mem provider in lambda.
    
    * Fix linting
    
    * Cache asset providers
    
    * Fix linting

--- packages/lambda-tiler/src/index.ts
+++ packages/lambda-tiler/src/index.ts
@@ -1,4 +1,4 @@
-import { LogConfig } from '@basemaps/shared';
+import { Env, LogConfig } from '@basemaps/shared';
 import { LambdaHttpResponse, lf } from '@linzjs/lambda';
 import { arcgisStyleJsonGet } from './arcgis/arcgis.style.json.js';
 import { arcgisTileServerGet } from './arcgis/vector.tile.server.js';
@@ -13,6 +13,7 @@ import { styleJsonGet } from './routes/tile.style.json.js';
 import { wmtsCapabilitiesGet } from './routes/tile.wmts.js';
 import { tileXyzGet } from './routes/tile.xyz.js';
 import { versionGet } from './routes/version.js';
+import { assetProvider } from './util/assets.provider.js';
 import { NotFound } from './util/response.js';
 import { CoSources } from './util/source.cache.js';
 import { St } from './util/source.tracer.js';
@@ -21,6 +22,10 @@ export const handler = lf.http(LogConfig.get());
 
 handler.router.hook('request', (req) => {
   req.set('name', 'LambdaTiler');
+
+  // Set the asset location for asset provider
+  const assetLocation = Env.get(Env.AssetLocation);
+  assetProvider.set(assetLocation);
 
   // Reset the request tracing before every request
   St.reset();
