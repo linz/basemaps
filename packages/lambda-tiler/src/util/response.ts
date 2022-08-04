26d65d1070a4e9481192e466a1f7f65ed2adb121
Wentao Kuang <wkuang@linz.govt.nz>
2022-08-05 11:26:10 +1200

    Fix typo

--- packages/lambda-tiler/src/util/response.ts
+++ packages/lambda-tiler/src/util/response.ts
@@ -4,4 +4,4 @@ export const NotFound = (): LambdaHttpResponse => new LambdaHttpResponse(404, 'N
 export const NotModified = (): LambdaHttpResponse => new LambdaHttpResponse(304, 'Not modified');
 export const NoContent = (): LambdaHttpResponse => new LambdaHttpResponse(204, 'No Content');
 export const OkResponse = (req: LambdaHttpRequest): LambdaHttpResponse =>
-  new LambdaHttpResponse(200, 'ok').json({ id: req.id, correladtionId: req.correlationId, message: 'ok' });
+  new LambdaHttpResponse(200, 'ok').json({ id: req.id, correlationId: req.correlationId, message: 'ok' });
