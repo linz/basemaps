8fe8d638f0f69b79d20355d183e698373d4ddd14
Wentao Kuang <wkuang@linz.govt.nz>
2022-08-05 11:05:18 +1200

    Add etag and content type for the info api.

--- packages/lambda-tiler/src/arcgis/arcgis.info.ts
+++ packages/lambda-tiler/src/arcgis/arcgis.info.ts
@@ -1,6 +1,7 @@
 import { Env } from '@basemaps/shared';
 import { HttpHeader, LambdaHttpRequest, LambdaHttpResponse } from '@linzjs/lambda';
-import { NotFound } from '../util/response.js';
+import { Etag } from '../util/etag.js';
+import { NotFound, NotModified } from '../util/response.js';
 
 export async function arcgisInfoGet(req: LambdaHttpRequest): Promise<LambdaHttpResponse> {
   const host = Env.get(Env.PublicUrlBase);
@@ -14,9 +15,12 @@ export async function arcgisInfoGet(req: LambdaHttpRequest): Promise<LambdaHttpR
   const json = JSON.stringify(info);
   const data = Buffer.from(json);
 
+  const cacheKey = Etag.key(data);
+  if (Etag.isNotModified(req, cacheKey)) return NotModified();
+
   const response = new LambdaHttpResponse(200, 'ok');
   response.header(HttpHeader.CacheControl, 'public, max-age=604800, stale-while-revalidate=86400');
-  response.buffer(data);
+  response.buffer(data, 'application/json');
   req.set('bytes', data.byteLength);
   return response;
 }
