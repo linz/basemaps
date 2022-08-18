bd28cc3b6ab77fd032a3d1c63b4b6afd362579cb
Wentao Kuang <wkuang@linz.govt.nz>
2022-08-18 15:18:54 +1200

    Set Default asset location when config.asset is null

--- packages/lambda-tiler/src/routes/__tests__/fonts.test.ts
+++ packages/lambda-tiler/src/routes/__tests__/fonts.test.ts
@@ -1,8 +1,11 @@
+import { base58, ConfigProviderMemory } from '@basemaps/config';
 import { Env, getDefaultConfig } from '@basemaps/shared';
 import { fsa } from '@chunkd/fs';
 import o from 'ospec';
 import { handler } from '../../index.js';
-import { mockRequest } from '../../__tests__/xyz.util.js';
+import { CachedConfig } from '../../util/config.cache.js';
+import { CoSources } from '../../util/source.cache.js';
+import { Api, mockRequest, mockUrlRequest } from '../../__tests__/xyz.util.js';
 import { fontList } from '../fonts.js';
 import { FsMemory } from './memory.fs.js';
 
@@ -14,12 +17,14 @@ o.spec('/v1/fonts', () => {
   const assetLocation = process.env[Env.AssetLocation];
 
   o.beforeEach(() => {
-    process.env[Env.AssetLocation] = 'memory://';
+    process.env[Env.AssetLocation] = 'memory://config';
     getDefaultConfig().assets = 'memory://';
   });
 
   o.afterEach(() => {
     getDefaultConfig().assets = assetLocation;
+    CachedConfig.cache.clear();
+    CoSources.cache.clear();
     memory.files.clear();
   });
 
@@ -66,5 +71,38 @@ o.spec('/v1/fonts', () => {
     getDefaultConfig().assets = undefined;
     const res = await fontList(mockRequest('/v1/fonts.json'));
     o(res.status).equals(404);
+  });
+
+  o('should get the correct utf8 font with default assets', async () => {
+    const cfgBundle = new ConfigProviderMemory();
+    await fsa.write('memory://linz-basemaps/bar.json', JSON.stringify(cfgBundle.toJson()));
+    await fsa.write('memory://config/fonts/🦄 🌈/0-255.pbf', Buffer.from(''));
+
+    const configLocation = base58.encode(Buffer.from('memory://linz-basemaps/bar.json'));
+    const res255 = await handler.router.handle(
+      mockUrlRequest('/v1/fonts/🦄 🌈/0-255.pbf', `?config=${configLocation}`, Api.header),
+    );
+    o(res255.status).equals(200);
+    o(res255.header('content-type')).equals('application/x-protobuf');
+    o(res255.header('content-encoding')).equals(undefined);
+    o(res255.header('etag')).notEquals(undefined);
+    o(res255.header('cache-control')).equals('public, max-age=604800, stale-while-revalidate=86400');
+  });
+
+  o('should get the correct utf8 font with config assets', async () => {
+    const cfgBundle = new ConfigProviderMemory();
+    cfgBundle.assets = 'memory://config/assets';
+    await fsa.write('memory://linz-basemaps/bar.json', JSON.stringify(cfgBundle.toJson()));
+    await fsa.write('memory://config/assets/fonts/🦄 🌈/0-255.pbf', Buffer.from(''));
+
+    const configLocation = base58.encode(Buffer.from('memory://linz-basemaps/bar.json'));
+    const res255 = await handler.router.handle(
+      mockUrlRequest('/v1/fonts/🦄 🌈/0-255.pbf', `?config=${configLocation}`, Api.header),
+    );
+    o(res255.status).equals(200);
+    o(res255.header('content-type')).equals('application/x-protobuf');
+    o(res255.header('content-encoding')).equals(undefined);
+    o(res255.header('etag')).notEquals(undefined);
+    o(res255.header('cache-control')).equals('public, max-age=604800, stale-while-revalidate=86400');
   });
 });
