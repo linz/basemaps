819ed35a8b09bf8ed778386784a30cfbb497b62d
Wentao Kuang <wkuang@linz.govt.nz>
2022-09-14 15:38:24 +1200

    Add bucket as a cli parameter

--- packages/cli/src/cli/cogify/action.make.cog.ts
+++ packages/cli/src/cli/cogify/action.make.cog.ts
@@ -1,5 +1,5 @@
 import { Epsg, TileMatrixSet, TileMatrixSets } from '@basemaps/geo';
-import { fsa, LogConfig, LogType } from '@basemaps/shared';
+import { fsa, LogConfig } from '@basemaps/shared';
 import { CommandLineAction, CommandLineIntegerParameter, CommandLineStringParameter } from '@rushstack/ts-command-line';
 import { CogStacJob, JobCreationContext } from '../../cog/cog.stac.job.js';
 import { ProjectionLoader } from '../../cog/projection.loader.js';
@@ -7,15 +7,9 @@ import * as ulid from 'ulid';
 import { getCutline } from './cutline.js';
 import { CogJobFactory } from '../../cog/job.factory.js';
 
-function guessProjection(imageryName: string): Epsg | undefined {
-  if (imageryName.startsWith('chatham')) return Epsg.Citm2000;
-  if (imageryName.includes('rural')) return Epsg.Nztm2000;
-  if (imageryName.includes('urban')) return Epsg.Nztm2000;
-  return undefined;
-}
-
 export class CommandMakeCog extends CommandLineAction {
   private imagery: CommandLineStringParameter;
+  private bucket: CommandLineStringParameter;
   private name: CommandLineStringParameter;
   private target: CommandLineStringParameter;
   private cutline: CommandLineStringParameter;
@@ -36,6 +30,13 @@ export class CommandMakeCog extends CommandLineAction {
       parameterShortName: '-i',
       parameterLongName: '--imagery',
       description: 'Path of source imagery to import.',
+      required: true,
+    });
+    this.bucket = this.defineStringParameter({
+      argumentName: 'BUCKET',
+      parameterShortName: '-b',
+      parameterLongName: '--bucket',
+      description: 'Bucket for job.json',
       required: true,
     });
     this.name = this.defineStringParameter({
@@ -70,7 +71,7 @@ export class CommandMakeCog extends CommandLineAction {
       argumentName: 'OUTPUT',
       parameterShortName: '-o',
       parameterLongName: '--output',
-      description: 'Output the job.json paths',
+      description: 'Output job.json path',
       required: false,
     });
   }
@@ -78,16 +79,15 @@ export class CommandMakeCog extends CommandLineAction {
   async onExecute(): Promise<void> {
     const logger = LogConfig.get();
     const imagery = this.imagery.value;
-    const name = this.name.value;
+    let name = this.name.value;
     if (imagery == null) throw new Error('Please provide a valid imagery source');
     await Promise.all([3791, 3790, 3789, 3788].map((code) => ProjectionLoader.load(code)));
 
     const source = imagery.endsWith('/') ? imagery : imagery + '/';
     logger.info({ imagery: source }, 'FindImagery');
-    const imagerySetName = source.split('/').filter(Boolean).pop();
+    if (name == null) name = source.split('/').filter(Boolean).pop();
+    if (name == null) throw new Error('Failed to find imagery set name');
 
-    if (imagerySetName == null) throw new Error('Failed to find imagery set name');
-    const uri = await this.findImagery(source, logger);
     const target = this.target.value;
     const targets: string[] = target ? [target] : ['NZTM2000Quad', 'WebMercatorQuad'];
 
@@ -97,17 +97,20 @@ export class CommandMakeCog extends CommandLineAction {
       const tileMatrix = TileMatrixSets.find(target);
       if (tileMatrix == null) throw new Error(`Cannot find tile matrix: ${target}`);
       logger.info({ id, tileMatrix: tileMatrix.identifier }, 'SetTileMatrix');
-      const job = await this.makeCog(id, name ? name : imagerySetName, tileMatrix, uri);
+      const job = await this.makeCog(id, name, tileMatrix, source);
       outputs.push(job.getJobPath('job.json'));
-      // Write the output job json
-      const output = this.output.value;
-      if (output) {
-        fsa.write(output, JSON.stringify(outputs));
-      }
+    }
+
+    // Write the output job json
+    const output = this.output.value;
+    if (output) {
+      fsa.write(output, JSON.stringify(outputs));
     }
   }
 
   async makeCog(id: string, imageryName: string, tileMatrix: TileMatrixSet, uri: string): Promise<CogStacJob> {
+    const bucket = this.bucket.value;
+    if (bucket == null) throw new Error('Please provide a validate bucket for output job.json');
     let resampling;
     /** Process Gebco 2193 as one cog of full extent to avoid antimeridian problems */
     if (tileMatrix.projection === Epsg.Nztm2000 && imageryName.includes('gebco')) {
@@ -133,8 +136,8 @@ export class CommandMakeCog extends CommandLineAction {
 
     const ctx: JobCreationContext = {
       imageryName,
-      override: { id, projection: guessProjection(imageryName), resampling },
-      outputLocation: { type: 's3' as const, path: 's3://linz-basemaps' },
+      override: { id, projection: Epsg.Nztm2000, resampling },
+      outputLocation: { type: 's3' as const, path: `s3://${bucket}` },
       sourceLocation: { type: 's3', path: uri },
       cutline,
       batch: false, // Only create the job.json in the make cog cli
@@ -143,16 +146,5 @@ export class CommandMakeCog extends CommandLineAction {
     };
     const job = (await CogJobFactory.create(ctx)) as CogStacJob;
     return job;
-  }
-
-  async findImagery(imagery: string, logger: LogType): Promise<string> {
-    const keys = fsa.list(imagery);
-    for await (const key of keys) {
-      if (key != null && (key.endsWith('.tiff') || key.endsWith('.tif'))) {
-        logger.info({ imagery }, 'FoundFiles');
-        return imagery;
-      }
-    }
-    throw new Error('Unable to find imagery: ' + imagery);
   }
 }
