5286e474eb5cc9fe12bcd0aba7730b753eb34255
Wentao Kuang <wkuang@linz.govt.nz>
2022-11-22 10:00:07 +1300

    Allow to load config json from linz-basemaps-staging bucket.

--- packages/lambda-tiler/src/util/config.loader.ts
+++ packages/lambda-tiler/src/util/config.loader.ts
@@ -6,7 +6,7 @@ import { CachedConfig } from './config.cache.js';
 import { getDefaultConfig } from '@basemaps/shared';
 
 // FIXME load this from process.env COG BUCKETS?
-const SafeBuckets = new Set(['linz-workflow-artifacts', 'linz-basemaps']);
+const SafeBuckets = new Set(['linz-workflow-artifacts', 'linz-basemaps', 'linz-basemaps-staging']);
 const SafeProtocols = new Set(['s3', 'memory']);
 
 export class ConfigLoader {
