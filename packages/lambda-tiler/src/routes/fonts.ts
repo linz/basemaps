a678c36cf9fca326f5ee9e3e713e259f6c53002e
Wentao Kuang <wkuang@linz.govt.nz>
2023-01-16 10:23:20 +1300

    Revert "feat(tiler): Support fonts array to fallback to next font if not fond. (#2633) (#2642)
    This reverts commit 5d8e0bea0ab0c29c2aa5264c9011a7eb6f51a946.

--- packages/lambda-tiler/src/routes/fonts.ts
+++ packages/lambda-tiler/src/routes/fonts.ts
@@ -1,20 +1,14 @@
 import { LambdaHttpRequest, LambdaHttpResponse } from '@linzjs/lambda';
 import path from 'path';
 import { assetProvider } from '../util/assets.provider.js';
-import { NotFound } from '../util/response.js';
 
 interface FontGet {
   Params: { fontStack: string; range: string };
 }
 
 export async function fontGet(req: LambdaHttpRequest<FontGet>): Promise<LambdaHttpResponse> {
-  const targetFonts = req.params.fontStack.split(',');
-  for (const font of targetFonts) {
-    const targetFile = path.join('fonts', font, req.params.range) + '.pbf';
-    const response = await assetProvider.serve(req, targetFile, 'application/x-protobuf');
-    if (response.status !== 404) return response;
-  }
-  return NotFound();
+  const targetFile = path.join('fonts', req.params.fontStack, req.params.range) + '.pbf';
+  return assetProvider.serve(req, targetFile, 'application/x-protobuf');
 }
 
 export async function fontList(req: LambdaHttpRequest): Promise<LambdaHttpResponse> {
