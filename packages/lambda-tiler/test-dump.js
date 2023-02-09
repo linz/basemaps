f0300a50a166dbf7469df854277cea95d8355e1a
Blayne Chard <bchard@linz.govt.nz>
2023-02-09 16:18:48 +1300

    feat(lambda-tiler): simple cli to trace the rendering of a tile

--- packages/lambda-tiler/test-dump.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-/**
- * Render a single tile and save it as a PNG
- */
-import 'source-map-support/register.js';
-import './build/cli/dump.js';
--- packages/lambda-tiler/test-imagery.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register.js';
-import './build/cli/validate.js';
