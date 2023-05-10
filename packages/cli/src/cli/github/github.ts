f74ecc5925859ce213bebb3bf06b7496fd0c280e
Wentao Kuang <wkuang@linz.govt.nz>
2023-05-10 13:14:41 +1200

    Fix remove the ref from the repository and get the repo name from it.

--- packages/cli/src/cli/github/github.ts
+++ packages/cli/src/cli/github/github.ts
@@ -4,10 +4,12 @@ import { execFileSync } from 'child_process';
 export class Github {
   repo: string;
   logger: LogType;
+  repoName: string;
 
   constructor(repo: string, logger: LogType) {
     this.repo = repo;
     this.logger = logger;
+    this.repoName = repo.split('/').slice(-1)[0];
   }
 
   /**
@@ -28,12 +30,12 @@ export class Github {
   getBranch(branch: string): string {
     this.logger.info({ branch }, 'GitHub: Get branch');
     try {
-      execFileSync('git', ['checkout', branch], { cwd: this.repo }).toString().trim();
+      execFileSync('git', ['checkout', branch], { cwd: this.repoName }).toString().trim();
       this.logger.info({ branch }, 'GitHub: Branch Checkout');
       return branch;
     } catch {
       this.logger.info({ branch }, 'GitHub: Create New Branch');
-      execFileSync('git', ['checkout', '-b', branch], { cwd: this.repo }).toString().trim();
+      execFileSync('git', ['checkout', '-b', branch], { cwd: this.repoName }).toString().trim();
       return branch;
     }
   }
@@ -46,9 +48,9 @@ export class Github {
     const email = Env.get('GIT_USER_EMAIL') ?? 'basemaps@linz.govt.nz';
     const name = Env.get('GIT_USER_NAME') ?? 'basemaps[bot]';
     this.logger.info({ repository: this.repo }, 'GitHub: Config User Email');
-    execFileSync('git', ['config', 'user.email', email], { cwd: this.repo }).toString().trim();
+    execFileSync('git', ['config', 'user.email', email], { cwd: this.repoName }).toString().trim();
     this.logger.info({ repository: this.repo }, 'GitHub: Config User Name');
-    execFileSync('git', ['config', 'user.name', name], { cwd: this.repo }).toString().trim();
+    execFileSync('git', ['config', 'user.name', name], { cwd: this.repoName }).toString().trim();
   }
 
   /**
@@ -57,7 +59,7 @@ export class Github {
    */
   commit(message: string): void {
     this.logger.info({ repository: this.repo }, 'GitHub: Commit all');
-    execFileSync('git', ['commit', '-am', `"${JSON.stringify(message)}"`], { cwd: this.repo })
+    execFileSync('git', ['commit', '-am', `"${JSON.stringify(message)}"`], { cwd: this.repoName })
       .toString()
       .trim();
   }
@@ -68,6 +70,6 @@ export class Github {
    */
   push(): void {
     this.logger.info({ repository: this.repo }, 'GitHub: Push');
-    execFileSync('git', ['push', 'origin', 'HEAD'], { cwd: this.repo }).toString().trim();
+    execFileSync('git', ['push', 'origin', 'HEAD'], { cwd: this.repoName }).toString().trim();
   }
 }
