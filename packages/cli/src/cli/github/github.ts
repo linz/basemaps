7870d79148b98c678882028123555778686be235
Wentao Kuang <wkuang@linz.govt.nz>
2023-05-21 11:18:53 +1200

    Remove the first and last double quotes in the commit message.

--- packages/cli/src/cli/github/github.ts
+++ packages/cli/src/cli/github/github.ts
@@ -61,7 +61,7 @@ export class Github {
    */
   commit(message: string): void {
     this.logger.info({ repository: this.repo }, 'GitHub: Commit all');
-    execFileSync('git', ['commit', '-am', JSON.stringify(message)], { cwd: this.repoName })
+    execFileSync('git', ['commit', '-am', JSON.stringify(message).replace(/"/g, '')], { cwd: this.repoName })
       .toString()
       .trim();
   }
