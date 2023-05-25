c3b64065641f50c007936098bcc57ad2e1778959
Blayne Chard <bchard@linz.govt.nz>
2023-05-26 10:58:02 +1200

    refactor: move to descrive it syntax

--- packages/smoke/src/health.test.ts
+++ packages/smoke/src/health.test.ts
@@ -1,22 +1,27 @@
 import assert from 'node:assert';
-import { test } from 'node:test';
+import { describe, it } from 'node:test';
 import { ctx } from './base.js';
 
-test('GET /v1/ping', async () => {
-  const res = await ctx.req('/v1/ping');
-  assert.equal(res.status, 200);
-});
+describe('health', () => {
+  it('should 200 when GET /v1/ping', async () => {
+    const res = await ctx.req('/v1/ping');
+    assert.equal(res.status, 200);
+  });
 
-test('GET /v1/health', async () => {
-  const res = await ctx.req('/v1/health');
-  assert.equal(res.status, 200);
-});
+  it('should 200 when GET /v1/health', async () => {
+    const res = await ctx.req('/v1/health');
+    assert.equal(res.status, 200);
+  });
 
-test('GET /v1/version', async () => {
-  const res = await ctx.req('/v1/version');
-  assert.equal(res.status, 200);
-  const data = await res.json();
+  it('should 200 when GET /v1/version', async () => {
+    const res = await ctx.req('/v1/version');
+    assert.equal(res.status, 200);
+    const data = await res.json();
 
-  assert.ok(data.version);
-  assert.ok(data.hash);
+    assert.equal(typeof data.version, 'string');
+    assert.ok(data.version);
+
+    assert.equal(typeof data.hash, 'string');
+    assert.ok(data.hash);
+  });
 });
