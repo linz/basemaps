eb2ff7bbe7791ca0f866c07bffd4874a83ea2757
Blayne Chard <bchard@linz.govt.nz>
2023-11-29 12:11:13 +1300

    fix: use mjs for all scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,16 @@ const cfg = {
 
 cfg.rules['no-console'] = ['error', { allow: ['warn', 'error'] }];
 // Allow console.log in tests and scripts
-cfg.overrides.push({ files: ['**/*.js', '**/__tests__/**/*.ts'], rules: { 'no-console': 'off' } });
+cfg.overrides.push({
+  files: ['**/*.mjs', '**/__tests__/**/*.ts'],
+  rules: { 'no-console': 'off', '@typescript-eslint/no-explicit-any': 'off' },
+});
+
+// Disable required importing of react as typescript does this for us
+cfg.overrides.push({
+  files: '**/*.tsx',
+  rules: { 'react/react-in-jsx-scope': 'off' },
+});
 
 // cfg.parserOptions.ecmaVersion = 2020;
 
