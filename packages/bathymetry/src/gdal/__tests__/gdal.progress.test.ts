9cfe62b0799c430d1c02f6dc044af3bf47568e0d
Blayne Chard <bchard@linz.govt.nz>
2023-12-12 14:24:59 +1300

    more o replacements

--- packages/bathymetry/src/gdal/__tests__/gdal.progress.test.ts
+++ packages/bathymetry/src/gdal/__tests__/gdal.progress.test.ts
@@ -1,10 +1,10 @@
 import assert from 'node:assert';
-import { describe } from 'node:test';
+import { describe, it } from 'node:test';
 
 import { GdalProgressParser } from '../gdal.progress.js';
 
 describe('GdalProgressParser', () => {
-  o('should emit on progress', () => {
+  it('should emit on progress', () => {
     const prog = new GdalProgressParser();
     assert.equal(prog.progress, 0);
 
@@ -12,7 +12,7 @@ describe('GdalProgressParser', () => {
     assert.equal(prog.progress.toFixed(2), '3.23');
   });
 
-  o('should take 31 dots to finish', () => {
+  it('should take 31 dots to finish', () => {
     const prog = new GdalProgressParser();
     let processCount = 0;
     prog.data(Buffer.from('\n'));
