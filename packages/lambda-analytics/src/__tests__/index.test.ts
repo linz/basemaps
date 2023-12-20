aa4472caf996413d253769bc46fc3226ac9080c9
Blayne Chard <bchard@linz.govt.nz>
2023-12-21 09:52:54 +1300

    fix: some tests commented out

--- packages/lambda-analytics/src/__tests__/index.test.ts
+++ packages/lambda-analytics/src/__tests__/index.test.ts
@@ -1,153 +1,156 @@
-// import assert from 'node:assert';
-// import { afterEach, describe, it } from 'node:test';
+import assert from 'node:assert';
+import { afterEach, before, describe, it } from 'node:test';
 
-// import { Env, LogConfig } from '@basemaps/shared';
-// import PLimit from 'p-limit';
-// import sinon from 'sinon';
+import { Env, fsa, FsMemory, LogConfig } from '@basemaps/shared';
+import PLimit from 'p-limit';
+import sinon from 'sinon';
 
-// import { FileProcess } from '../file.process.js';
-// import { dateByHour, getMaxDate, handler, listCacheFolder, MaxToProcess, Q } from '../index.js';
-// import { LogStartDate, RollupVersion } from '../stats.js';
-// import { ExampleLogs, lineReader } from './file.process.test.js';
+import { FileProcess } from '../file.process.js';
+import { dateByHour, getMaxDate, handler, listCacheFolder, MaxToProcess, Q } from '../index.js';
+import { LogStartDate, RollupVersion } from '../stats.js';
+import { ExampleLogs, lineReader } from './file.process.test.js';
 
-// const sandbox = sinon.createSandbox();
-// LogConfig.get().level = 'silent';
-// const currentYear = new Date().getUTCFullYear();
-// // Concurrency breaks the order of tests
-// Q.time = PLimit(1);
+const sandbox = sinon.createSandbox();
+LogConfig.get().level = 'silent';
+const currentYear = new Date().getUTCFullYear();
+// Concurrency breaks the order of tests
+Q.time = PLimit(1);
 
-// describe('hourByHour', () => {
-//   const firstLogHour = (hour: number): string =>
-//     LogStartDate.toISOString().replace('T00', 'T' + hour.toString().padStart(2, '0'));
-//   it(`should start around ${currentYear}-01-01`, () => {
-//     const startDate = LogStartDate.getTime();
-//     const iter = dateByHour(startDate);
-//     for (let i = 0; i < 5; i++) {
-//       assert.equal(firstLogHour(i), new Date(iter.next().value).toISOString());
-//     }
-//   });
+describe('hourByHour', () => {
+  const firstLogHour = (hour: number): string =>
+    LogStartDate.toISOString().replace('T00', 'T' + hour.toString().padStart(2, '0'));
+  it(`should start around ${currentYear}-01-01`, () => {
+    const startDate = LogStartDate.getTime();
+    const iter = dateByHour(startDate);
+    for (let i = 0; i < 5; i++) {
+      assert.equal(firstLogHour(i), new Date(iter.next().value).toISOString());
+    }
+  });
 
-//   it('should work for thousands of hours', () => {
-//     const startDate = new Date(LogStartDate).getTime();
-//     const iter = dateByHour(startDate);
-//     let startHour = 0;
-//     let lastDate = startDate - 60 * 60 * 1000;
-//     // Loop over the next 10,000 hours and verify we always increase by one UTC hour
-//     // I have manually run this for 10,000,000 hours and seems to work (about 1000 years)
-//     for (let i = 0; i < 10000; i++) {
-//       const nextDate = new Date(iter.next().value);
-//       const timeInc = nextDate.getTime() - lastDate;
-//       lastDate = nextDate.getTime();
+  it('should work for thousands of hours', () => {
+    const startDate = new Date(LogStartDate).getTime();
+    const iter = dateByHour(startDate);
+    let startHour = 0;
+    let lastDate = startDate - 60 * 60 * 1000;
+    // Loop over the next 10,000 hours and verify we always increase by one UTC hour
+    // I have manually run this for 10,000,000 hours and seems to work (about 1000 years)
+    for (let i = 0; i < 10000; i++) {
+      const nextDate = new Date(iter.next().value);
+      const timeInc = nextDate.getTime() - lastDate;
+      lastDate = nextDate.getTime();
 
-//       assert.equal(nextDate.getUTCHours(), startHour, `${i} - ${nextDate.toISOString()}`);
-//       assert.equal(timeInc, 60 * 60 * 1000, `${i} - ${timeInc}`);
-//       startHour++;
-//       if (startHour === 24) startHour = 0;
-//     }
-//   });
-// });
+      assert.equal(nextDate.getUTCHours(), startHour, `${i} - ${nextDate.toISOString()}`);
+      assert.equal(timeInc, 60 * 60 * 1000, `${i} - ${timeInc}`);
+      startHour++;
+      if (startHour === 24) startHour = 0;
+    }
+  });
+});
 
-// describe('getStartDate', () => {
-//   const originalList = fsa.list;
-//   afterEach(() => {
-//     fsa.list = originalList;
-//   });
+describe('getStartDate', () => {
+  const fsMemory = new FsMemory();
+  before(() => {
+    fsa.register('s3://', fsMemory);
+  });
 
-//   it('should use the start date if no files found', async () => {
-//     fsa.list = async function* listFiles(): AsyncGenerator<string> {
-//       // yield nothing
-//     };
-//     const cacheData = await listCacheFolder('s3://foo/bar');
-//     assert.equal(cacheData.size, 0);
-//   });
+  it('should use the start date if no files found', async () => {
+    const cacheData = await listCacheFolder(new URL('s3://foo/bar'));
+    assert.equal(cacheData.size, 0);
+  });
 
-//   it('should not use the start date if files are found', async () => {
-//     fsa.list = async function* listFiles(key: string): AsyncGenerator<string> {
-//       yield `${key}baz.txt`;
-//       yield `${key}2020-01-01T01.ndjson`;
-//     };
-//     const cacheData = await listCacheFolder('s3://foo/bar/');
-//     assert.equal(cacheData.size, 1);
-//     assert.equal(cacheData.has('2020-01-01T01'), true);
-//   });
-// });
+  it('should not use the start date if files are found', async () => {
+    // fsa.write('');
+    // fsa.list = async function* listFiles(key: string): AsyncGenerator<string> {
+    //   yield `${key}baz.txt`;
+    //   yield `${key}2020-01-01T01.ndjson`;
+    // };
+    const cacheData = await listCacheFolder(new URL('s3://foo/bar/'));
+    assert.equal(cacheData.size, 1);
+    assert.equal(cacheData.has('2020-01-01T01'), true);
+  });
+});
 
-// describe('handler', () => {
-//   const sourceBucket = `s3://cloudfront-logs`;
-//   const cloudFrontId = `E1WKYJII8YDTO0`;
+describe('handler', () => {
+  const sourceBucket = `fake-s3://cloudfront-logs`;
+  const cloudFrontId = `E1WKYJII8YDTO0`;
 
-//   process.env[Env.Analytics.CloudFrontId] = cloudFrontId;
-//   process.env[Env.Analytics.CloudFrontSourceBucket] = sourceBucket;
-//   process.env[Env.Analytics.CacheBucket] = `s3://analytics-cache`;
+  process.env[Env.Analytics.CloudFrontId] = cloudFrontId;
+  process.env[Env.Analytics.CloudFrontSourceBucket] = sourceBucket;
+  process.env[Env.Analytics.CacheBucket] = `fake-s3://analytics-cache`;
 
-//   afterEach(() => {
-//     sandbox.restore();
-//   });
+  const fsMemory = new FsMemory();
+  before(() => {
+    fsa.register('fake-s3://', fsMemory);
+  });
 
-//   it('should list and process files', async () => {
-//     const cachePath = `s3://analytics-cache/RollUpV${RollupVersion}/${currentYear}`;
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-//     const sourceFiles = [
-//       `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-00.hash.gz`,
-//       `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-01.hash.gz`,
-//       `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-01.hashB.gz`,
-//       `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-02.hash.gz`,
-//       // Old
-//       `${sourceBucket}/${cloudFrontId}.2019-07-28-02.hash.gz`,
-//     ];
-//     sandbox.stub(FileProcess, 'reader').callsFake(lineReader(ExampleLogs, `${currentYear}-01-01T02`));
-//     const writeStub = sandbox.stub(fsa, 'write');
+  it('should list and process files', async () => {
+    const cachePath = `fake-s3://analytics-cache/RollUpV${RollupVersion}/${currentYear}`;
 
-//     const listStub = sandbox.stub(fsa, 'list').callsFake(async function* ListFiles(
-//       source: string,
-//     ): AsyncGenerator<string> {
-//       if (source.startsWith(sourceBucket)) {
-//         for (const filePath of sourceFiles) {
-//           if (filePath.startsWith(source)) yield filePath;
-//         }
-//       }
-//       if (source.startsWith(cachePath)) {
-//         yield `${cachePath}/${currentYear}-01-01T00.ndjson`;
-//         yield `${cachePath}/${currentYear}-01-01T03.ndjson`;
-//       }
-//     });
+    // const sourceFiles = [
+    //   `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-00.hash.gz`,
+    //   `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-01.hash.gz`,
+    //   `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-01.hashB.gz`,
+    //   `${sourceBucket}/${cloudFrontId}.${currentYear}-01-01-02.hash.gz`,
+    //   // Old
+    //   `${sourceBucket}/${cloudFrontId}.2019-07-28-02.hash.gz`,
+    // ];
+    sandbox.stub(FileProcess, 'reader').callsFake(lineReader(ExampleLogs, `${currentYear}-01-01T02`));
+    const writeStub = sandbox.stub(fsa, 'write');
 
-//     await handler();
+    // const listStub = sandbox.stub(fsa, 'list').callsFake(async function* ListFiles(
+    //   source: string,
+    // ): AsyncGenerator<string> {
+    //   if (source.startsWith(sourceBucket)) {
+    //     for (const filePath of sourceFiles) {
+    //       if (filePath.startsWith(source)) yield filePath;
+    //     }
+    //   }
+    //   if (source.startsWith(cachePath)) {
+    //     yield `${cachePath}/${currentYear}-01-01T00.ndjson`;
+    //     yield `${cachePath}/${currentYear}-01-01T03.ndjson`;
+    //   }
+    // });
 
-//     // Get the expected number of writes to be called
-//     const maxDate = getMaxDate().getTime();
-//     let expectedCount = 0;
-//     for (const hour of dateByHour(LogStartDate.getTime())) {
-//       if (hour >= maxDate || expectedCount > MaxToProcess) break;
-//       expectedCount++;
-//     }
+    await handler();
 
-//     assert.equal(expectedCount > 0, true);
+    // Get the expected number of writes to be called
+    const maxDate = getMaxDate().getTime();
+    let expectedCount = 0;
+    for (const hour of dateByHour(LogStartDate.getTime())) {
+      if (hour >= maxDate || expectedCount > MaxToProcess) break;
+      expectedCount++;
+    }
 
-//     // Two files are already processed
-//     assert.equal(writeStub.callCount, Math.min(expectedCount - 1, MaxToProcess));
+    assert.equal(expectedCount > 0, true);
 
-//     assert.equal(writeStub.args[0][0], `${cachePath}/${currentYear}-01-01T01.ndjson`);
-//     assert.equal(writeStub.args[0][1].toString(), '');
+    // Two files are already processed
+    assert.equal(writeStub.callCount, Math.min(expectedCount - 1, MaxToProcess));
 
-//     assert.equal(writeStub.args[1][0], `${cachePath}/${currentYear}-01-01T02.ndjson`);
-//     assert.notEqual(writeStub.args[1][1].toString(), '');
+    assert.equal(writeStub.args[0][0], `${cachePath}/${currentYear}-01-01T01.ndjson`);
+    assert.equal(writeStub.args[0][1].toString(), '');
 
-//     // Should write empty files when data is not found
-//     assert.equal(writeStub.args[2][0], `${cachePath}/${currentYear}-01-01T04.ndjson`);
-//     assert.equal(writeStub.args[2][1].toString(), '');
+    assert.equal(writeStub.args[1][0], `${cachePath}/${currentYear}-01-01T02.ndjson`);
+    assert.notEqual(writeStub.args[1][1].toString(), '');
 
-//     assert.equal(writeStub.args[3][0], `${cachePath}/${currentYear}-01-01T05.ndjson`);
-//     assert.equal(writeStub.args[3][1].toString(), '');
+    // Should write empty files when data is not found
+    assert.equal(writeStub.args[2][0], `${cachePath}/${currentYear}-01-01T04.ndjson`);
+    assert.equal(writeStub.args[2][1].toString(), '');
 
-//     // Will do lots of lists upto todays date to find more data
-//     const minExpected = Math.min(expectedCount, 100);
-//     assert.equal(listStub.callCount >= minExpected, true);
+    assert.equal(writeStub.args[3][0], `${cachePath}/${currentYear}-01-01T05.ndjson`);
+    assert.equal(writeStub.args[3][1].toString(), '');
 
-//     assert.equal(listStub.args[0][0], `s3://analytics-cache/RollUpV${RollupVersion}/${currentYear}/`);
-//     assert.equal(listStub.args[1][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-01`);
-//     assert.equal(listStub.args[2][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-02`);
-//     assert.equal(listStub.args[3][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-04`);
-//     assert.equal(listStub.args[4][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-05`);
-//   });
-// });
+    // Will do lots of lists upto todays date to find more data
+    // const minExpected = Math.min(expectedCount, 100);
+    // assert.equal(listStub.callCount >= minExpected, true);
+
+    // assert.equal(listStub.args[0][0], `s3://analytics-cache/RollUpV${RollupVersion}/${currentYear}/`);
+    // assert.equal(listStub.args[1][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-01`);
+    // assert.equal(listStub.args[2][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-02`);
+    // assert.equal(listStub.args[3][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-04`);
+    // assert.equal(listStub.args[4][0], `s3://cloudfront-logs/E1WKYJII8YDTO0.${currentYear}-01-01-05`);
+  });
+});
