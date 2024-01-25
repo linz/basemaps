98963088aff978639c7721e493c63b5582f3686e
Blayne Chard <bchard@linz.govt.nz>
2024-01-26 11:06:25 +1300

    fix: some tests commented out (#3066)
    #### Motivation
    
    _What does this change aim to achieve?_
    
    #### Modification
    
    _Why is this change being made? What implications or other
    considerations are there?_
    
    #### Checklist
    
    _If not applicable, provide explanation of why._
    
    - [ ] Tests updated
    - [ ] Docs updated
    - [ ] Issue linked in Title

--- packages/lambda-tiler/src/routes/__tests__/fonts.test.ts
+++ packages/lambda-tiler/src/routes/__tests__/fonts.test.ts
@@ -1,108 +1,112 @@
-// import assert from 'node:assert';
-// import { afterEach, beforeEach, describe, it } from 'node:test';
+import assert from 'node:assert';
+import { afterEach, beforeEach, describe, it } from 'node:test';
 
-// import { base58, ConfigProviderMemory } from '@basemaps/config';
-// import { getDefaultConfig } from '@basemaps/shared';
-// import { fsa } from '@chunkd/fs';
-// import { FsMemory } from '@chunkd/source-memory';
-// import { createSandbox } from 'sinon';
+import { base58, BaseConfig, ConfigProviderMemory } from '@basemaps/config';
+import { setDefaultConfig } from '@basemaps/shared';
+import { fsa, FsMemory } from '@chunkd/fs';
 
-// import { Api, mockRequest, mockUrlRequest } from '../../__tests__/xyz.util.js';
-// import { handler } from '../../index.js';
-// import { CachedConfig } from '../../util/config.cache.js';
-// import { ConfigLoader } from '../../util/config.loader.js';
-// import { CoSources } from '../../util/source.cache.js';
-// import { fontList } from '../fonts.js';
+import { Api, mockRequest, mockUrlRequest } from '../../__tests__/xyz.util.js';
+import { handler } from '../../index.js';
+import { CachedConfig } from '../../util/config.cache.js';
+import { CoSources } from '../../util/source.cache.js';
+import { fontList } from '../fonts.js';
 
-// describe('/v1/fonts', () => {
-//   const memory = new FsMemory();
-//   const sandbox = createSandbox();
-//   const config = new ConfigProviderMemory();
+describe('/v1/fonts', () => {
+  const memory = new FsMemory();
+  const config = new ConfigProviderMemory();
 
-//   beforeEach(() => {
-//     fsa.register('memory://', memory);
-//     config.assets = 'memory://';
-//     sandbox.stub(ConfigLoader, 'getDefaultConfig').resolves(config);
-//   });
+  beforeEach(() => {
+    config.objects.clear();
+    fsa.register('memory://', memory);
+    config.assets = 'memory://assets/';
+    setDefaultConfig(config);
+  });
 
-//   afterEach(() => {
-//     sandbox.restore();
-//     CachedConfig.cache.clear();
-//     CoSources.cache.clear();
-//     memory.files.clear();
-//   });
+  afterEach(() => {
+    CachedConfig.cache.clear();
+    CoSources.cache.clear();
+    memory.files.clear();
+  });
 
-//   it('should return 404 if no font found', async () => {
-//     const res = await fontList(mockRequest('/v1/fonts.json'));
-//     assert.equal(res.status, 404);
-//   });
+  it('should return 404 if no font found', async () => {
+    const res = await fontList(mockRequest('/v1/fonts.json'));
+    assert.equal(res.status, 404);
+  });
 
-//   it('should return a list of fonts found', async () => {
-//     await fsa.write('memory://fonts/fonts.json', Buffer.from(JSON.stringify(['Roboto Black', 'Roboto Thin'])));
-//     const res = await fontList(mockRequest('/v1/fonts.json'));
-//     assert.equal(res.status, 200);
-//     assert.equal(res.header('content-type'), 'application/json');
-//     assert.equal(res.header('content-encoding'), undefined);
-//     assert.equal(res._body?.toString(), JSON.stringify(['Roboto Black', 'Roboto Thin']));
-//   });
+  it('should return a list of fonts found', async () => {
+    await fsa.write(
+      new URL('memory://assets/fonts/fonts.json'),
+      Buffer.from(JSON.stringify(['Roboto Black', 'Roboto Thin'])),
+    );
+    const res = await fontList(mockRequest('/v1/fonts.json'));
+    assert.equal(res.status, 200);
+    assert.equal(res.header('content-type'), 'application/json');
+    assert.equal(res.header('content-encoding'), undefined);
+    assert.equal(res._body?.toString(), JSON.stringify(['Roboto Black', 'Roboto Thin']));
+  });
 
-//   it('should get the correct font', async () => {
-//     await fsa.write('memory://fonts/Roboto Thin/0-255.pbf', Buffer.from(''));
-//     const res255 = await handler.router.handle(mockRequest('/v1/fonts/Roboto Thin/0-255.pbf'));
-//     assert.equal(res255.status, 200);
-//     assert.equal(res255.header('content-type'), 'application/x-protobuf');
-//     assert.equal(res255.header('content-encoding'), undefined);
-//     assert.notEqual(res255.header('etag'), undefined);
-//     assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
+  it('should get the correct font', async () => {
+    await fsa.write(new URL('memory://assets/fonts/Roboto Thin/0-255.pbf'), Buffer.from(''));
+    const res255 = await handler.router.handle(mockRequest('/v1/fonts/Roboto Thin/0-255.pbf'));
+    assert.equal(res255.status, 200);
+    assert.equal(res255.header('content-type'), 'application/x-protobuf');
+    assert.equal(res255.header('content-encoding'), undefined);
+    assert.notEqual(res255.header('etag'), undefined);
+    assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
 
-//     const res404 = await handler.router.handle(mockRequest('/v1/fonts/Roboto Thin/256-512.pbf'));
-//     assert.equal(res404.status, 404);
-//   });
+    const res404 = await handler.router.handle(mockRequest('/v1/fonts/Roboto Thin/256-512.pbf'));
+    assert.equal(res404.status, 404);
+  });
 
-//   it('should get the correct utf8 font', async () => {
-//     await fsa.write('memory://fonts/🦄 🌈/0-255.pbf', Buffer.from(''));
-//     const res255 = await handler.router.handle(mockRequest('/v1/fonts/🦄 🌈/0-255.pbf'));
-//     assert.equal(res255.status, 200);
-//     assert.equal(res255.header('content-type'), 'application/x-protobuf');
-//     assert.equal(res255.header('content-encoding'), undefined);
-//     assert.notEqual(res255.header('etag'), undefined);
-//     assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
-//   });
+  it('should get the correct utf8 font', async () => {
+    await fsa.write(new URL('memory://assets/fonts/🦄 🌈/0-255.pbf'), Buffer.from(''));
+    const res255 = await handler.router.handle(mockRequest('/v1/fonts/🦄 🌈/0-255.pbf'));
+    assert.equal(res255.status, 200);
+    assert.equal(res255.header('content-type'), 'application/x-protobuf');
+    assert.equal(res255.header('content-encoding'), undefined);
+    assert.notEqual(res255.header('etag'), undefined);
+    assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
+  });
 
-//   it('should return 404 if no asset location set', async () => {
-//     getDefaultConfig().assets = undefined;
-//     const res = await fontList(mockRequest('/v1/fonts.json'));
-//     assert.equal(res.status, 404);
-//   });
+  it('should return 404 if no asset location set', async () => {
+    config.assets = undefined;
+    const res = await fontList(mockRequest('/v1/fonts.json'));
+    assert.equal(res.status, 404);
+  });
 
-//   it('should get the correct utf8 font with default assets', async () => {
-//     getDefaultConfig().assets = undefined;
-//     sandbox
-//       .stub(config.ConfigBundle, 'get')
-//       .resolves({ id: 'cb_latest', name: 'latest', path: 'latest', hash: 'hash', assets: 'memory://' });
-//     await fsa.write('memory://fonts/Roboto Thin/0-255.pbf', Buffer.from(''));
-//     const res255 = await handler.router.handle(mockRequest('/v1/fonts/Roboto Thin/0-255.pbf'));
-//     assert.equal(res255.status, 200);
-//     assert.equal(res255.header('content-type'), 'application/x-protobuf');
-//     assert.equal(res255.header('content-encoding'), undefined);
-//     assert.notEqual(res255.header('etag'), undefined);
-//     assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
-//   });
+  it('should get the correct utf8 font with default assets', async () => {
+    config.assets = undefined;
+    config.objects.set('cb_latest', {
+      id: 'cb_latest',
+      name: 'latest',
+      path: 'latest',
+      hash: 'hash',
+      assets: 'memory://new-location/',
+    } as BaseConfig);
 
-//   it('should get the correct utf8 font with config assets', async () => {
-//     const cfgBundle = new ConfigProviderMemory();
-//     cfgBundle.assets = 'memory://config/assets';
-//     await fsa.write('memory://linz-basemaps/bar.json', JSON.stringify(cfgBundle.toJson()));
-//     await fsa.write('memory://config/assets/fonts/🦄 🌈/0-255.pbf', Buffer.from(''));
+    await fsa.write(new URL('memory://new-location/fonts/Roboto Thin/0-255.pbf'), Buffer.from(''));
+    const res255 = await handler.router.handle(mockRequest('/v1/fonts/Roboto Thin/0-255.pbf'));
+    assert.equal(res255.status, 200, res255.statusDescription);
+    assert.equal(res255.header('content-type'), 'application/x-protobuf');
+    assert.equal(res255.header('content-encoding'), undefined);
+    assert.notEqual(res255.header('etag'), undefined);
+    assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
+  });
 
-//     const configLocation = base58.encode(Buffer.from('memory://linz-basemaps/bar.json'));
-//     const res255 = await handler.router.handle(
-//       mockUrlRequest('/v1/fonts/🦄 🌈/0-255.pbf', `?config=${configLocation}`, Api.header),
-//     );
-//     assert.equal(res255.status, 200);
-//     assert.equal(res255.header('content-type'), 'application/x-protobuf');
-//     assert.equal(res255.header('content-encoding'), undefined);
-//     assert.notEqual(res255.header('etag'), undefined);
-//     assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
-//   });
-// });
+  it('should get the correct utf8 font with config assets', async () => {
+    const cfgBundle = new ConfigProviderMemory();
+    cfgBundle.assets = 'memory://config/assets/';
+    await fsa.write(new URL('memory://linz-basemaps/bar.json'), JSON.stringify(cfgBundle.toJson()));
+    await fsa.write(new URL('memory://config/assets/fonts/🦄 🌈/0-255.pbf'), Buffer.from(''));
+
+    const configLocation = base58.encode(Buffer.from('memory://linz-basemaps/bar.json'));
+    const res255 = await handler.router.handle(
+      mockUrlRequest('/v1/fonts/🦄 🌈/0-255.pbf', `?config=${configLocation}`, Api.header),
+    );
+    assert.equal(res255.status, 200);
+    assert.equal(res255.header('content-type'), 'application/x-protobuf');
+    assert.equal(res255.header('content-encoding'), undefined);
+    assert.notEqual(res255.header('etag'), undefined);
+    assert.equal(res255.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
+  });
+});
--- packages/lambda-tiler/src/routes/__tests__/sprites.test.ts
+++ packages/lambda-tiler/src/routes/__tests__/sprites.test.ts
@@ -1,72 +1,74 @@
-// import assert from 'node:assert';
-// import { afterEach, before, beforeEach, describe, it } from 'node:test';
+import assert from 'node:assert';
+import { afterEach, before, beforeEach, describe, it } from 'node:test';
 
-// import { ConfigProviderMemory } from '@basemaps/config';
-// import { fsa } from '@chunkd/fs';
-// import { FsMemory } from '@chunkd/source-memory';
-// import { createSandbox } from 'sinon';
-// import { gunzipSync, gzipSync } from 'zlib';
+import { ConfigProviderMemory } from '@basemaps/config';
+import { fsa, FsMemory } from '@chunkd/fs';
+import { createSandbox } from 'sinon';
+import { gunzipSync, gzipSync } from 'zlib';
 
-// import { mockRequest } from '../../__tests__/xyz.util.js';
-// import { handler } from '../../index.js';
-// import { ConfigLoader } from '../../util/config.loader.js';
+import { mockRequest } from '../../__tests__/xyz.util.js';
+import { handler } from '../../index.js';
+import { ConfigLoader } from '../../util/config.loader.js';
 
-// describe('/v1/sprites', () => {
-//   const memory = new FsMemory();
-//   const sandbox = createSandbox();
-//   const config = new ConfigProviderMemory();
+describe('/v1/sprites', () => {
+  const memory = new FsMemory();
+  const sandbox = createSandbox();
+  const config = new ConfigProviderMemory();
 
-//   before(() => {
-//     fsa.register('memory://', memory);
-//   });
+  before(() => {
+    fsa.register('fake-s3://assets/', memory);
+  });
 
-//   beforeEach(() => {
-//     config.assets = 'memory://';
-//     sandbox.stub(ConfigLoader, 'getDefaultConfig').resolves(config);
-//   });
+  beforeEach(() => {
+    config.assets = 'fake-s3://assets/';
+    sandbox.stub(ConfigLoader, 'getDefaultConfig').resolves(config);
+  });
 
-//   afterEach(() => {
-//     memory.files.clear();
-//     sandbox.restore();
-//   });
+  afterEach(() => {
+    memory.files.clear();
+    sandbox.restore();
+  });
 
-//   it('should fetch a json document', async () => {
-//     await Promise.all([
-//       fsa.write('memory://sprites/topographic.json', Buffer.from(JSON.stringify({ test: true }))),
-//       fsa.write('memory://sprites/topographic.png', Buffer.from('')),
-//     ]);
-//     const res = await handler.router.handle(mockRequest('/v1/sprites/topographic.json'));
-//     assert.equal(res.status, 200);
-//     assert.equal(res.header('content-type'), 'application/json');
-//     assert.equal(res.header('content-encoding'), undefined);
-//     assert.notEqual(res.header('etag'), undefined);
-//     assert.equal(res.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
+  it('should fetch a json document', async () => {
+    await Promise.all([
+      fsa.write(new URL('fake-s3://assets/sprites/topographic.json'), Buffer.from(JSON.stringify({ test: true }))),
+      fsa.write(new URL('fake-s3://assets/sprites/topographic.png'), Buffer.from('')),
+    ]);
+    const res = await handler.router.handle(mockRequest('/v1/sprites/topographic.json'));
+    assert.equal(res.status, 200);
+    assert.equal(res.header('content-type'), 'application/json');
+    assert.equal(res.header('content-encoding'), undefined);
+    assert.notEqual(res.header('etag'), undefined);
+    assert.equal(res.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
 
-//     assert.deepEqual(JSON.parse(Buffer.from(res.body, 'base64').toString()), { test: true });
-//   });
+    assert.deepEqual(JSON.parse(Buffer.from(res.body, 'base64').toString()), { test: true });
+  });
 
-//   it('should fetch a png', async () => {
-//     await Promise.all([
-//       fsa.write('memory://sprites/topographic.json', Buffer.from(JSON.stringify({ test: true }))),
-//       fsa.write('memory://sprites/topographic@2x.png', Buffer.from('')),
-//     ]);
-//     const res = await handler.router.handle(mockRequest('/v1/sprites/topographic@2x.png'));
-//     assert.equal(res.status, 200);
-//     assert.equal(res.header('content-type'), 'image/png');
-//     assert.notEqual(res.header('etag'), undefined);
-//     assert.equal(res.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
-//   });
+  it('should fetch a png', async () => {
+    await Promise.all([
+      fsa.write(new URL('fake-s3://assets/sprites/topographic.json'), Buffer.from(JSON.stringify({ test: true }))),
+      fsa.write(new URL('fake-s3://assets/sprites/topographic@2x.png'), Buffer.from('')),
+    ]);
+    const res = await handler.router.handle(mockRequest('/v1/sprites/topographic@2x.png'));
+    assert.equal(res.status, 200);
+    assert.equal(res.header('content-type'), 'image/png');
+    assert.notEqual(res.header('etag'), undefined);
+    assert.equal(res.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
+  });
 
-//   it('should detect gziped files and set content-encoding', async () => {
-//     await Promise.all([
-//       fsa.write('memory://sprites/topographic.json', gzipSync(Buffer.from(JSON.stringify({ test: true })))),
-//     ]);
-//     const res = await handler.router.handle(mockRequest('/v1/sprites/topographic.json'));
-//     assert.equal(res.status, 200);
-//     assert.equal(res.header('content-type'), 'application/json');
-//     assert.equal(res.header('content-encoding'), 'gzip');
-//     assert.notEqual(res.header('etag'), undefined);
-//     assert.equal(res.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
-//     assert.deepEqual(JSON.parse(gunzipSync(Buffer.from(res.body, 'base64')).toString()), { test: true });
-//   });
-// });
+  it('should detect gziped files and set content-encoding', async () => {
+    await Promise.all([
+      fsa.write(
+        new URL('fake-s3://assets/sprites/topographic.json'),
+        gzipSync(Buffer.from(JSON.stringify({ test: true }))),
+      ),
+    ]);
+    const res = await handler.router.handle(mockRequest('/v1/sprites/topographic.json'));
+    assert.equal(res.status, 200);
+    assert.equal(res.header('content-type'), 'application/json');
+    assert.equal(res.header('content-encoding'), 'gzip');
+    assert.notEqual(res.header('etag'), undefined);
+    assert.equal(res.header('cache-control'), 'public, max-age=604800, stale-while-revalidate=86400');
+    assert.deepEqual(JSON.parse(gunzipSync(Buffer.from(res.body, 'base64')).toString()), { test: true });
+  });
+});
