66db6189519c12bbefbbaadc04036b7d3d118e42
Blayne Chard <bchard@linz.govt.nz>
2024-02-02 14:23:28 +1300

    refactor: fixup lint

--- packages/config-loader/src/json/__tests__/tiff.config.test.ts
+++ packages/config-loader/src/json/__tests__/tiff.config.test.ts
@@ -1,10 +1,10 @@
 import assert from 'node:assert';
 import { afterEach, beforeEach, describe, it } from 'node:test';
 
+import { ConfigImagery } from '@basemaps/config';
 import { fsa, FsMemory } from '@basemaps/shared';
 
-import { loadStacFromURL, isRgbOrRgba } from '../tiff.config.js';
-import { ConfigImagery } from '@basemaps/config';
+import { isRgbOrRgba, loadStacFromURL } from '../tiff.config.js';
 
 describe('loadStacFromURL', () => {
   const mem = new FsMemory();
@@ -39,30 +39,29 @@ describe('loadStacFromURL', () => {
   });
 });
 
-
 describe('isRgbOrRgba', () => {
   const uint8 = { type: 'uint', bits: 8 };
   it('should allow imagery with no band information', () => {
     assert.equal(isRgbOrRgba({} as ConfigImagery), true);
-  })
+  });
 
   it('should allow 3 or 4 band imagery', () => {
     assert.equal(isRgbOrRgba({ bands: [uint8, uint8, uint8] } as ConfigImagery), true);
     assert.equal(isRgbOrRgba({ bands: [uint8, uint8, uint8, uint8] } as ConfigImagery), true);
-  })
+  });
 
   it('should not allow 1,2,5,6 band imagery', () => {
     assert.equal(isRgbOrRgba({ bands: [uint8] } as ConfigImagery), false);
     assert.equal(isRgbOrRgba({ bands: [uint8, uint8] } as ConfigImagery), false);
     assert.equal(isRgbOrRgba({ bands: [uint8, uint8, uint8, uint8, uint8] } as ConfigImagery), false);
     assert.equal(isRgbOrRgba({ bands: [uint8, uint8, uint8, uint8, uint8, uint8] } as ConfigImagery), false);
-  })
+  });
 
   it('should not allow float32 imagery', () => {
     assert.equal(isRgbOrRgba({ bands: [uint8, uint8, uint8, { type: 'float', bits: 32 }] } as ConfigImagery), false);
-  })
+  });
 
   it('should not allow uint16', () => {
     assert.equal(isRgbOrRgba({ bands: [uint8, uint8, uint8, { type: 'uint', bits: 16 }] } as ConfigImagery), false);
-  })
-})
+  });
+});
