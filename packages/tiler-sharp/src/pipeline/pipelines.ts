92e8bc6c4f0fdec85abe23c03c1d323830ab3dd3
Blayne Chard <bchard@linz.govt.nz>
2024-02-08 11:51:28 +1300

    feat(tiler-sharp): add terrain-rgb pipeline

--- packages/tiler-sharp/src/pipeline/pipelines.ts
+++ packages/tiler-sharp/src/pipeline/pipelines.ts
@@ -1,6 +1,8 @@
 import { Pipeline } from './decompressor.js';
 import { PipelineColorRamp } from './pipeline.color.ramp.js';
+import { PipelineTerrainRgb } from './pipeline.terrain.rgb.js';
 
 export const Pipelines: Record<string, Pipeline> = {
   [PipelineColorRamp.type]: PipelineColorRamp,
+  [PipelineTerrainRgb.type]: PipelineTerrainRgb,
 };
