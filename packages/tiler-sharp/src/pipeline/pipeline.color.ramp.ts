2c7b8e5383527ba3c854790fbf27b99d54625b4e
Blayne Chard <bchard@linz.govt.nz>
2024-03-08 10:47:45 +1300

    feat(config): improve the default color ramp with more color ranges (#3172)
    #### Motivation
    
    The current default color ramp doesn't use very many colors and makes
    new zealand very green, use another color ramp with more stops and
    colors
    
    #### Modification
    
    adjusts the default color ramp to a more complex color ramp.
    
    #### Checklist
    
    _If not applicable, provide explanation of why._
    
    - [ ] Tests updated
    - [ ] Docs updated
    - [ ] Issue linked in Title

--- packages/tiler-sharp/src/pipeline/pipeline.color.ramp.ts
+++ packages/tiler-sharp/src/pipeline/pipeline.color.ramp.ts
@@ -1,36 +1,31 @@
+import { DefaultColorRamp } from '@basemaps/config';
 import { Tiff } from '@cogeotiff/core';
 
 import { DecompressedInterleaved, Pipeline } from './decompressor.js';
 
 export class ColorRamp {
-  noData: { v: number; color: [number, number, number, number] } = { v: NaN, color: [0, 0, 0, 0] };
   ramps: { v: number; color: [number, number, number, number] }[] = [];
-  constructor(ramp: string, noDataValue: number) {
-    const ramps = ramp.split('\n');
+  constructor(ramp: string) {
+    const ramps = ramp.trim().split('\n');
 
     for (const ramp of ramps) {
       const parts = ramp.trim().split(' ');
-      if (parts[0] === 'nv') {
-        this.noData = { v: noDataValue, color: parts.slice(1).map(Number) as [number, number, number, number] };
-        continue;
-      }
       const numbers = parts.map(Number);
       this.ramps.push({ v: numbers[0], color: numbers.slice(1) as [number, number, number, number] });
     }
   }
 
   get(num: number): [number, number, number, number] {
-    if (num === this.noData.v) return this.noData.color;
-
     const first = this.ramps[0];
-    if (num < first.v) return first.color;
+    if (num <= first.v) return first.color;
 
     for (let i = 0; i < this.ramps.length - 1; i++) {
       const ramp = this.ramps[i];
-      const rampNext = this.ramps[i + 1];
-      if (num >= rampNext.v) continue;
       if (num < ramp.v) continue;
       if (ramp.v === num) return ramp.color;
+
+      const rampNext = this.ramps[i + 1];
+      if (num >= rampNext.v) continue;
 
       const range = rampNext.v - ramp.v;
       const offset = num - ramp.v;
@@ -47,23 +42,7 @@ export class ColorRamp {
   }
 }
 
-// Stolen from https://github.com/andrewharvey/srtm-stylesheets/blob/master/stylesheets/color-ramps/srtm-Australia-color-ramp.gdaldem.txt
-export const ramp = new ColorRamp(
-  `nv 0 0 0 0
--8764 0 0 0 255
--4000 3 45 85 255
--100 0 101 199 255
-0 192 224 255 255
-1 108 220 108 255
-55 50 180 50 255
-390 240 250 150 255
-835 190 185 135 255
-1114 180 128 107 255
-1392 235 220 220 255
-4000 215 244 244 255
-7000 255 255 255 255`,
-  -9999,
-);
+export const ramp = new ColorRamp(DefaultColorRamp);
 
 export const PipelineColorRamp: Pipeline = {
   type: 'color-ramp',
