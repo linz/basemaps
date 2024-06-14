9c277fd6714e78219debeaabc50a2117de1819bf
Wentao Kuang <wkuang@linz.govt.nz>
2024-06-14 12:18:33 +1200

    Enable elevation source in the individual raster style json.

--- packages/lambda-tiler/src/__tests__/config.data.ts
+++ packages/lambda-tiler/src/__tests__/config.data.ts
@@ -6,6 +6,8 @@ import {
   ConfigProviderMemory,
   ConfigTileSetRaster,
   ConfigTileSetVector,
+  DefaultColorRampOutput,
+  DefaultTerrainRgbOutput,
   TileSetType,
 } from '@basemaps/config';
 import { fsa, FsMemory } from '@basemaps/shared';
@@ -27,6 +29,24 @@ export const TileSetAerial: ConfigTileSetRaster = {
     },
   ],
 };
+
+export const TileSetElevation: ConfigTileSetRaster = {
+  id: 'ts_elevation',
+  name: 'elevation',
+  type: TileSetType.Raster,
+  description: 'elevation__description',
+  title: 'Elevation',
+  category: 'Elevation',
+  layers: [
+    {
+      3857: 'im_01FYWKATAEK2ZTJQ2PX44Y0XNT',
+      title: 'New Zealand 8m DEM (2012)',
+      name: 'new-zealand_2012_dem_8m',
+    },
+  ],
+  outputs: [DefaultTerrainRgbOutput, DefaultColorRampOutput],
+};
+
 export const TileSetVector: ConfigTileSetVector = {
   id: 'ts_topographic',
   type: TileSetType.Vector,
@@ -249,6 +269,15 @@ export const Provider: ConfigProvider = {
 
 export class FakeData {
   static tileSetRaster(name: string): ConfigTileSetRaster {
+    const tileSet = JSON.parse(JSON.stringify(TileSetAerial));
+
+    tileSet.name = name;
+    tileSet.id = `ts_${name}`;
+
+    return tileSet;
+  }
+
+  static tileSetTerrain(name: string): ConfigTileSetRaster {
     const tileSet = JSON.parse(JSON.stringify(TileSetAerial));
 
     tileSet.name = name;
