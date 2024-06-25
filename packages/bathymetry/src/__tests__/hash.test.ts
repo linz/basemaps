4aa868e08e7f093f2e3cf84296eacaf5c60f6212
Blayne Chard <bchard@linz.govt.nz>
2024-06-25 15:26:25 +1200

    refactor: remove testing file

--- packages/bathymetry/src/__tests__/hash.test.ts
+++ packages/bathymetry/src/__tests__/hash.test.ts
@@ -1,13 +1,14 @@
 import assert from 'node:assert';
-import { writeFileSync } from 'node:fs';
+import * as fs from 'node:fs';
 import { describe, it } from 'node:test';
 
 import { Hash } from '../hash.js';
 
 describe('hash', () => {
   it('hash', async () => {
-    writeFileSync('./test-file', Buffer.from('I am a test file for hashing\n'));
+    fs.writeFileSync('./test-file', Buffer.from('I am a test file for hashing\n'));
     const ans = await Hash.hash('./test-file');
     assert.equal(ans, '122076427149ca45100f317f16821ef934885cc49352447ee64c9f5e9655c95c695e');
+    fs.unlinkSync('./test-file');
   });
 });
