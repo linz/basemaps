70f4bd458cb1082c042a84a304989628db522671
Blayne Chard <bchard@linz.govt.nz>
2024-06-25 15:25:39 +1200

    refactor: move to local files

--- packages/bathymetry/src/__tests__/hash.test.ts
+++ packages/bathymetry/src/__tests__/hash.test.ts
@@ -1,15 +1,13 @@
 import assert from 'node:assert';
+import { writeFileSync } from 'node:fs';
 import { describe, it } from 'node:test';
 
-import path, { resolve } from 'path';
-import url from 'url';
-
 import { Hash } from '../hash.js';
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 describe('hash', () => {
   it('hash', async () => {
-    const ans = await Hash.hash(resolve(__dirname.replace('/build/', '/src/'), 'test-file.txt'));
+    writeFileSync('./test-file', Buffer.from('I am a test file for hashing\n'));
+    const ans = await Hash.hash('./test-file');
     assert.equal(ans, '122076427149ca45100f317f16821ef934885cc49352447ee64c9f5e9655c95c695e');
   });
 });
