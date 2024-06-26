13aedf8944adda80d985a7e642c4408788369497
Wentao Kuang <wkuang@linz.govt.nz>
2024-06-27 11:13:33 +1200

    feat(lambda-tiler): Ensure terrain source for all style json configs. (#3299)
    ### Motivation
    
    We need to ensure a terrain source in all style json configs before the
    production release. Then we could do another config release for add
    terrain source into style config.
    https://github.com/linz/basemaps-config/pull/916
    
    ### Modifications
    
    Check and add terrain source for get style config api.
    
    ### Verification
    
    
    ![image](https://github.com/linz/basemaps/assets/12163920/01544ba9-469f-4a5f-ba9a-9d97502e2fc3)

--- packages/lambda-tiler/src/routes/tile.style.json.ts
+++ packages/lambda-tiler/src/routes/tile.style.json.ts
@@ -268,6 +268,10 @@ export async function styleJsonGet(req: LambdaHttpRequest<StyleGet>): Promise<La
     styleConfig.style.layers.filter((f) => !excluded.has(f.id.toLowerCase())),
   );
 
+  // Ensure elevation for style json config
+  // TODO: We should remove this after adding terrain source into style configs. PR-916
+  await ensureTerrain(req, tileMatrix, apiKey, style);
+
   // Add terrain in style
   if (terrain) setStyleTerrain(style, terrain);
 
