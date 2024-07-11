f9f0f23463272b559d9e648ad9c328101f57b1a8
Wentao Kuang <wkuang@linz.govt.nz>
2024-07-12 07:31:37 +1200

    Fix format

--- packages/landing/src/components/debug.tsx
+++ packages/landing/src/components/debug.tsx
@@ -457,7 +457,9 @@ export class Debug extends Component<{ map: maplibregl.Map }, DebugState> {
   getSourcesIds(type: string): string[] {
     const style = this.props.map.getStyle();
     if (type === 'raster-dem') {
-      return Object.keys(style.sources).filter((id) => !id.startsWith(HillShadePrefix) && style.sources[id].type === type);
+      return Object.keys(style.sources).filter(
+        (id) => !id.startsWith(HillShadePrefix) && style.sources[id].type === type,
+      );
     } else if (type === 'raster') {
       return Object.keys(style.sources).filter((id) => id.startsWith('basemaps') && style.sources[id].type === type);
     } else throw new Error('Only support to get raster or raster-dem sources for debug dropdown.');
