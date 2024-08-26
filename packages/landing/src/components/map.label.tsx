48027e06c193777c31b9e677f747c5986ce7f1ce
Blayne Chard <bchard@linz.govt.nz>
2024-08-26 16:30:31 +1200

    fix: remove labels from style requests for topograhic

--- packages/landing/src/components/map.label.tsx
+++ packages/landing/src/components/map.label.tsx
@@ -2,7 +2,7 @@ import { IControl } from 'maplibre-gl';
 
 import { Config } from '../config.js';
 
-const LabelsDisabledLayers = new Set(['topographic', 'topolite']);
+export const LabelsDisabledLayers = new Set(['topographic', 'topolite']);
 
 export class MapLabelControl implements IControl {
   map?: maplibregl.Map;
