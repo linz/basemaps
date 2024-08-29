960b92652da686c18712fa566e63c7eb6453a2f7
Blayne Chard <bchard@linz.govt.nz>
2024-08-29 15:10:42 +1200

    feat(lambda-tiler): automatically rescale style JSON's into NZTM2000Quad when requests (#3339)
    ### Motivation
    
    NZTM2000Quad is offset by two zoom levels from web mercator quad, when
    the stylejsons are created for both projections the style json needs to
    adjust all the zoom levels by two
    
    ### Modifications
    
    Convert styles zoom offsets automatically when asking for a NZTM2000Quad
    style json
    updated comments across the style json endpoint
    refactored the style generation into a single point.
    
    ### Verification
    
    unit tests

---
+++ packages/lambda-tiler/src/util/__test__/nztm.style.test.ts
@@ -0,0 +1,100 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+
+import { StyleJson } from '@basemaps/config';
+
+import { convertStyleToNztmStyle } from '../nztm.style.js';
+
+describe('NZTM2000QuadStyle', () => {
+  const fakeStyle: StyleJson = {
+    version: 8,
+    id: 'test',
+    name: 'topographic',
+    sources: {},
+    layers: [],
+    glyphs: '/glyphs',
+    sprite: '/sprite',
+    metadata: { id: 'test' },
+  };
+
+  it('should not modify the source style', () => {
+    const baseStyle = {
+      ...fakeStyle,
+      terrain: { exaggeration: 1.1, source: 'abc' },
+    };
+
+    convertStyleToNztmStyle(baseStyle);
+    assert.equal(baseStyle.terrain?.exaggeration, 1.1);
+
+    convertStyleToNztmStyle(baseStyle, false);
+    assert.equal(baseStyle.terrain?.exaggeration, 4.4);
+  });
+
+  it('should convert min/maxzooms', () => {
+    const newStyle = convertStyleToNztmStyle({
+      ...fakeStyle,
+      layers: [{ minzoom: 5, maxzoom: 10, id: 'something', type: '' }],
+    });
+
+    assert.deepEqual(newStyle.layers[0], { minzoom: 3, maxzoom: 8, id: 'something', type: '' });
+  });
+
+  it('should offset terrain', () => {
+    const newStyle = convertStyleToNztmStyle({
+      ...fakeStyle,
+      terrain: { exaggeration: 1.1, source: 'abc' },
+    });
+
+    assert.deepEqual(newStyle.terrain, { exaggeration: 4.4, source: 'abc' });
+  });
+
+  it('should convert stops inside of paint and layout', () => {
+    const newStyle = convertStyleToNztmStyle({
+      ...fakeStyle,
+      layers: [
+        {
+          layout: {
+            'line-width': {
+              stops: [
+                [16, 0.75],
+                [24, 1.5],
+              ],
+            },
+          },
+
+          paint: {
+            'line-width': {
+              stops: [
+                [16, 0.75],
+                [24, 1.5],
+              ],
+            },
+          },
+          id: 'something',
+          type: '',
+        },
+      ],
+    });
+
+    assert.deepEqual(newStyle.layers[0], {
+      layout: {
+        'line-width': {
+          stops: [
+            [14, 0.75],
+            [22, 1.5],
+          ],
+        },
+      },
+      paint: {
+        'line-width': {
+          stops: [
+            [14, 0.75],
+            [22, 1.5],
+          ],
+        },
+      },
+      id: 'something',
+      type: '',
+    });
+  });
+});
