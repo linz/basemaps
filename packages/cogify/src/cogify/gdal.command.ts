fd69421fbca12ac5c49a999fdcf7e9e975bd5da6
Wentao Kuang <wkuang@linz.govt.nz>
2024-12-13 14:32:38 +1300

    Try 4096

--- packages/cogify/src/cogify/gdal.command.ts
+++ packages/cogify/src/cogify/gdal.command.ts
@@ -112,7 +112,7 @@ export function gdalCreate(targetTiff: URL, opt: CogifyCreationOptions): GdalCom
     output: targetTiff,
     args: [
       ['-of', 'GTiff'],
-      ['-outsize', 512, 512],
+      ['-outsize', 4096, 4096],
       ['-bands', '4'],
       ['-burn', `${bg.r} ${bg.g} ${bg.b} ${bg.alpha}`], // this is the color
       ['-a_srs', tileMatrix.projection.toEpsgString()],
