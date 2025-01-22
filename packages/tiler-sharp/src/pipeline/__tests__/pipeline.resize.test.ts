ec87a0c93d9bad0b32a963df09a74f6c8bd6fa8a
Blayne Chard <bchard@linz.govt.nz>
2025-01-22 15:42:48 +1300

    fix(tiler-sharp): when resampling uint round numbers rather than truncate

---
+++ packages/tiler-sharp/src/pipeline/__tests__/pipeline.resize.test.ts
@@ -0,0 +1,26 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+
+import { CompositionTiff } from '@basemaps/tiler';
+
+import { resizeBilinear } from '../pipeline.resize.js';
+
+describe('resize-bilinear', () => {
+  it('should round numbers when working with uint arrays', () => {
+    const ret = resizeBilinear(
+      {
+        pixels: new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+        depth: 'uint8',
+        width: 4,
+        height: 4,
+        channels: 1,
+      },
+      { source: { width: 4, height: 4 } } as unknown as CompositionTiff,
+      { x: 0, y: 0, width: 1, height: 1 },
+      { width: 256, height: 256, scale: 123.123123123 },
+    );
+
+    // All values should be rounded to 1 and not truncated down to 0
+    assert.ok(ret.pixels.every((f) => f === 1));
+  });
+});
