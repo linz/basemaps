dece1a3dd64a0d7885b011805e2bad8cf47f7458
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-04-15 09:20:33 +1200

    feat(cli): add cli-config to cli package as subcommands

--- packages/cli/src/cli/index.ts
+++ packages/cli/src/cli/index.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import { subcommands } from 'cmd-ts';
 
-import { BundleAssetsCommand } from './config/action.bundle.assets.js';
-import { BundleCommand } from './config/action.bundle.js';
-import { ImportCommand } from './config/action.import.js';
+import { BundleAssetsCommand } from '@basemaps/cli-config';
+import { BundleCommand } from '@basemaps/cli-config';
+import { ImportCommand } from '@basemaps/cli-config';
 
 export const Cli = subcommands({
   name: 'bmc',
