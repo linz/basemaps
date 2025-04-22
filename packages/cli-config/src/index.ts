493b55e2f73f5473373c42788b06d78bcbb71e01
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-04-22 15:11:16 +1200

    fix(cli): cli naming

--- packages/cli-config/src/index.ts
+++ packages/cli-config/src/index.ts
@@ -8,7 +8,7 @@ import { BundleAssetsCommand } from './cli/action.bundle.assets.js';
 import { BundleCommand } from './cli/action.bundle.js';
 import { ImportCommand } from './cli/action.import.js';
 
-export const CliConfig = subcommands({
+export const ConfigCli = subcommands({
   name: 'config',
   cmds: {
     bundle: BundleCommand,
--- packages/cli/src/cli/index.ts
+++ packages/cli/src/cli/index.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import { CliConfig } from '@basemaps/cli-config';
+import { ConfigCli } from '@basemaps/cli-config';
 import { subcommands } from 'cmd-ts';
 
 export const Cli = subcommands({
   name: 'bmc',
   description: 'Basemaps command tools',
   cmds: {
-    config: CliConfig,
+    config: ConfigCli,
   },
 });
