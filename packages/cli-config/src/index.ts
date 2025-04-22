8463c1b3ea089df62df2d7254eaac46b92c3c103
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-04-22 15:12:02 +1200

    fix(cli): don't export individual commands

--- packages/cli-config/src/index.ts
+++ packages/cli-config/src/index.ts
@@ -1,7 +1,3 @@
-export { BundleAssetsCommand } from './cli/action.bundle.assets.js';
-export { BundleCommand } from './cli/action.bundle.js';
-export { ImportCommand } from './cli/action.import.js';
-
 import { subcommands } from 'cmd-ts';
 
 import { BundleAssetsCommand } from './cli/action.bundle.assets.js';
