4b5d33a3d3b3af8e2e95ed682225a46c8ac13cbb
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-04-22 14:57:49 +1200

    fix(cli): export Cli subcommand

--- packages/cli-config/src/index.ts
+++ packages/cli-config/src/index.ts
@@ -1,3 +1,18 @@
 export { BundleAssetsCommand } from './cli/action.bundle.assets.js';
 export { BundleCommand } from './cli/action.bundle.js';
 export { ImportCommand } from './cli/action.import.js';
+
+import { subcommands } from 'cmd-ts';
+
+import { BundleAssetsCommand } from './cli/action.bundle.assets.js';
+import { BundleCommand } from './cli/action.bundle.js';
+import { ImportCommand } from './cli/action.import.js';
+
+export const CliConfig = subcommands({
+  name: 'config',
+  cmds: {
+    bundle: BundleCommand,
+    'bundle-assets': BundleAssetsCommand,
+    import: ImportCommand,
+  },
+});
--- packages/cli/src/cli/index.ts
+++ packages/cli/src/cli/index.ts
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
-import { BundleAssetsCommand, BundleCommand, ImportCommand } from '@basemaps/cli-config';
+import { CliConfig } from '@basemaps/cli-config';
 import { subcommands } from 'cmd-ts';
 
 export const Cli = subcommands({
   name: 'bmc',
   description: 'Basemaps command tools',
   cmds: {
-    config: subcommands({
-      name: 'config',
-      cmds: {
-        bundle: BundleCommand,
-        'bundle-assets': BundleAssetsCommand,
-        import: ImportCommand,
-      },
-    }),
+    config: CliConfig,
   },
 });
