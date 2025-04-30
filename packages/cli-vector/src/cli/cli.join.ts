ba52e89180f3c862e13c2e3f101116462f244647
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-05-01 10:50:04 +1200

    feat(cli-vector): new cli to join mbtiles

---
+++ packages/cli-vector/src/cli/cli.join.ts
@@ -0,0 +1,49 @@
+import { fsa, Url } from '@basemaps/shared';
+import { CliInfo } from '@basemaps/shared/build/cli/info.js';
+import { getLogger, logArguments } from '@basemaps/shared/build/cli/log.js';
+import { command, option } from 'cmd-ts';
+
+import { tileJoin } from '../transform/tippecanoe.js';
+
+async function fromFile(path: URL): Promise<URL[]> {
+  const toProcess = await fsa.readJson(path);
+  const paths: URL[] = [];
+  if (!Array.isArray(toProcess)) throw new Error(`File ${path.href} is not an array`);
+  for (const tasks of toProcess) {
+    if (Array.isArray(tasks)) {
+      for (const task of tasks) {
+        if (typeof task !== 'string') throw new Error(`File ${path.href} is not an array of strings`);
+        paths.push(new URL(task));
+      }
+    } else if (typeof tasks === 'string') {
+      paths.push(new URL(tasks));
+    } else {
+      throw new Error(`File ${path.href} is not an array of strings`);
+    }
+  }
+  return paths;
+}
+
+export const JoinArgs = {
+  ...logArguments,
+  fromFile: option({
+    type: Url,
+    long: 'from-file',
+    description: 'Path to JSON file containing array of paths to mbtiles.',
+  }),
+};
+
+export const JoinCommand = command({
+  name: 'join',
+  version: CliInfo.version,
+  description: 'Join vector mbtiles',
+  args: JoinArgs,
+  async handler(args) {
+    const logger = getLogger(this, args, 'cli-vector');
+    const filePaths = await fromFile(args.fromFile);
+
+    logger.info({ files: filePaths.length }, 'JoinMbtiles: Start');
+
+    await tileJoin(filePaths, '/home/c/bm/basemaps/tmp/one.mbtiles', logger);
+  },
+});
--- packages/cli-vector/src/transform/tippecanoe.ts
+++ packages/cli-vector/src/transform/tippecanoe.ts
@@ -57,7 +57,7 @@ export async function tippecanoe(input: URL, output: URL, layer: Layer, logger: 
  *
  * @returns { cmd: string; args: string[] } cmd and arguments for tippecanoe tile-join docker command
  */
-export async function tileJoin(inputs: string[], output: string, logger: LogType): Promise<void> {
+export async function tileJoin(inputs: URL[], output: string, logger: LogType): Promise<void> {
   const cmd = Command.create('/usr/bin/tile-join');
 
   cmd.mount(dirname(output));
@@ -65,9 +65,9 @@ export async function tileJoin(inputs: string[], output: string, logger: LogType
   cmd.args.push('-pk');
   cmd.args.push('-o', output);
   for (const input of inputs) {
-    if (input.endsWith('mbtiles')) {
-      cmd.mount(dirname(input));
-      cmd.args.push(input);
+    if (input.pathname.endsWith('mbtiles')) {
+      cmd.mount(dirname(input.pathname));
+      cmd.args.push(input.pathname);
     }
   }
   cmd.args.push('--force');
