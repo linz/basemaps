834dba26d5ea8a522a29c67d7f30f42b2f2562ff
Tawera Manaena <TManaena@linz.govt.nz>
2025-05-08 09:10:40 +1200

    fix(cli): refine the place_labels feature processing functions

--- packages/cli-vector/src/override/layers/place_labels.ts
+++ packages/cli-vector/src/override/layers/place_labels.ts
@@ -10,79 +10,91 @@ import {
 } from '../parser.js';
 import { VectorGeoPlaceLabelsFeature } from '../schema.js';
 
-const Features = new Map<string, VectorGeoPlaceLabelsFeature>();
+export const PlaceLabelsFeatures = new Map<string, VectorGeoPlaceLabelsFeature>();
 
+/**
+ *  Processes a 'place_labels' layer feature. Specifically, features of the gazatteer dataset.
+ *
+ *  The gazatteer dataset contains multiple features that technically describe the same feature, such that:
+ * - 1. One feature will contain all of the expected properties.
+ * - 2. The other features will have 'null' values for all expected properties, except `label` and `zoom_level`.
+ * - 3. All of the features describing the same feature will have the same `label` value, but different `zoom_level` values.
+ *
+ * @param feature - the feature to process
+ * @param options - the layer's options
+ * @param logger
+ * @returns the processed feature
+ */
 export function handleLayerPlaceLabels(feature: VectorGeoFeature, logger: LogType): VectorGeoFeature | null {
   logger.info({}, 'HandlePlaceLabels:Start');
   feature = structuredClone(feature);
 
-  let properties: zTypePlaceLabelsProperties;
-  let tippecanoe: zTypePlaceLabelsTippecanoe;
+  // parse the feature's `label` and `zoom_level` properties
+  // every feature should define these two properties at minimum
+  const label = feature.properties['label'];
+  if (typeof label !== 'string') throw new Error('Label property is not a string');
 
-  try {
-    properties = zPlaceLabelsProperties.parse({
-      name: feature.properties['label'],
-      kind: feature.properties['style'],
-      place: feature.properties['place'],
-      adminlevel: feature.properties['adminlevel'],
-      natural: feature.properties['natural'],
-      water: feature.properties['water'],
-    });
+  const zoomLevel = feature.properties['zoom_level'];
+  if (typeof zoomLevel !== 'number') throw new Error('Zoom level is not a number');
 
-    tippecanoe = zPlaceLabelsTippecanoe.parse({
-      layer: 'place_labels',
-      minzoom: feature.properties['zoom_level'],
-      maxzoom: feature.properties['zoom_level'],
-    });
-  } catch (e) {
-    // each feature in the gazatteer dataset contains one record with all
-    // expected properties, and duplicate records with 'null' properties.
-    // when we encounter such duplicate records, we return null to discard them
-    if (e instanceof z.ZodError) {
-      return null;
-    } else {
-      throw new Error('Something went wrong');
-    }
-  }
-
-  const newFeature = {
-    type: feature.type,
-    properties: {
-      ...properties,
-      // we convert the 'kind' property after zod validates 'kind' and 'minzoom'
-      kind: convertStyle(properties.kind, tippecanoe.minzoom),
-    },
-    geometry: feature.geometry,
-    tippecanoe,
-  };
-
-  const name = newFeature.properties.name;
-
-  const storedFeature = Features.get(name);
+  // check if we already store a feature with the same 'label' value
+  const storedFeature = PlaceLabelsFeatures.get(label);
 
   if (storedFeature == null) {
-    Features.set(name, newFeature);
+    let properties: zTypePlaceLabelsProperties;
+    let tippecanoe: zTypePlaceLabelsTippecanoe;
+
+    try {
+      properties = zPlaceLabelsProperties.parse({
+        name: label,
+        kind: feature.properties['style'],
+        place: feature.properties['place'],
+        adminlevel: feature.properties['adminlevel'],
+        natural: feature.properties['natural'],
+        water: feature.properties['water'],
+      });
+
+      tippecanoe = zPlaceLabelsTippecanoe.parse({
+        layer: 'place_labels',
+        minzoom: zoomLevel,
+        maxzoom: zoomLevel,
+      });
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        logger.warn({ label }, 'Failed to parse expected properties. Discarding feature.');
+        return null;
+      } else {
+        throw new Error('Unexpected error');
+      }
+    }
+
+    const newFeature = {
+      type: feature.type,
+      properties: {
+        ...properties,
+        // convert the feature's 'kind' property after zod validates 'kind' and 'minzoom'
+        kind: convertKind(properties.kind, tippecanoe.minzoom),
+      },
+      geometry: feature.geometry,
+      tippecanoe,
+    };
+
+    PlaceLabelsFeatures.set(label, newFeature);
 
     logger.info({}, 'HandlePlaceLabels:End');
     return newFeature;
   }
 
-  const zoom = newFeature.tippecanoe.minzoom;
-
-  if (zoom < storedFeature.tippecanoe.minzoom) {
-    storedFeature.tippecanoe.minzoom = zoom;
-    Features.set(name, storedFeature);
+  // update the 'minzoom' value of the stored feature
+  if (zoomLevel < storedFeature.tippecanoe.minzoom) {
+    storedFeature.tippecanoe.minzoom = zoomLevel;
+    PlaceLabelsFeatures.set(label, storedFeature);
   }
 
-  if (zoom > storedFeature.tippecanoe.maxzoom) {
-    storedFeature.tippecanoe.maxzoom = zoom;
-    Features.set(name, storedFeature);
-  }
-
-  // REVIEW: this code doesn't make sense as 'kind' is a string.
-  if (newFeature.properties.kind > storedFeature.properties.kind) {
-    storedFeature.properties.kind = newFeature.properties.kind;
-    Features.set(name, storedFeature);
+  // update the 'maxzoom' value of the stored feature
+  if (zoomLevel > storedFeature.tippecanoe.maxzoom) {
+    storedFeature.tippecanoe.maxzoom = zoomLevel;
+    PlaceLabelsFeatures.set(label, storedFeature);
   }
 
   logger.info({}, 'HandlePlaceLabels:End');
@@ -90,17 +102,17 @@ export function handleLayerPlaceLabels(feature: VectorGeoFeature, logger: LogTyp
 }
 
 /**
- * Logic to tag gazetteer data based on the styles.
- * @param style from gazetteer data
+ * Logic to tag gazetteer data based on a feature's 'styles' property value.
+ * @param kind - 'style' property from gazetteer data
  */
-function convertStyle(style: string, minzoom: number): string {
-  if (style.startsWith('ANT')) return 'ant';
-  if (style.startsWith('GEO')) return 'geo';
-  if (style.startsWith('TWN')) {
+function convertKind(kind: string, minzoom: number): string {
+  if (kind.startsWith('ANT')) return 'ant';
+  if (kind.startsWith('GEO')) return 'geo';
+  if (kind.startsWith('TWN')) {
     if (minzoom <= 8) return 'city';
-    if (style === 'TWN1') return 'suburb';
-    if (style === 'TWN2') return 'town';
-    if (style === 'TWN3') return 'city';
+    if (kind === 'TWN1') return 'suburb';
+    if (kind === 'TWN2') return 'town';
+    if (kind === 'TWN3') return 'city';
   }
   return '';
 }
