5693800a0e9f25b427a9653fdd9c230e4be349c4
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-05-12 15:17:37 +1200

    feat(cli-vector): analyse mbtiles

--- packages/cli-vector/src/index.ts
+++ packages/cli-vector/src/index.ts
@@ -2,11 +2,13 @@
 import { subcommands } from 'cmd-ts';
 import { ExtractCommand } from './cli/cli.extract.js';
 import { CreateCommand } from './cli/cli.create.js';
+import { AnalyseCommand } from './cli/cli.analyse.js';
 
 export const vectorCli = subcommands({
   name: 'vector',
   cmds: {
     extract: ExtractCommand,
     create: CreateCommand,
+    analyse: AnalyseCommand,
   },
 });
