c43dea61d025f3faa026e6d3513e4ed6e9e4b8c4
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-05-15 10:41:07 +1200

    feat(cli-vector): review fixes

--- packages/cli-vector/src/cli/cli.join.ts
+++ packages/cli-vector/src/cli/cli.join.ts
@@ -1,8 +1,10 @@
-import { TileMatrixSets } from '@basemaps/geo';
-import { fsa, Url } from '@basemaps/shared';
-import { CliInfo } from '@basemaps/shared/build/cli/info.js';
+import { Epsg, TileMatrixSets } from '@basemaps/geo';
+import { fsa, LogType, Url } from '@basemaps/shared';
+import { CliId, CliInfo } from '@basemaps/shared/build/cli/info.js';
 import { getLogger, logArguments } from '@basemaps/shared/build/cli/log.js';
 import { command, option, string } from 'cmd-ts';
+import { basename } from 'path';
+import { createGzip } from 'zlib';
 
 import { createStacFiles } from '../stac.js';
 import { toTarIndex } from '../transform/covt.js';
@@ -13,19 +15,44 @@ async function fromFile(path: URL): Promise<URL[]> {
   const toProcess = await fsa.readJson(path);
   const paths: URL[] = [];
   if (!Array.isArray(toProcess)) throw new Error(`File ${path.href} is not an array`);
-  for (const tasks of toProcess) {
-    if (Array.isArray(tasks)) {
-      for (const task of tasks) {
-        if (typeof task !== 'string') throw new Error(`File ${path.href} is not an array of strings`);
-        paths.push(new URL(task));
-      }
-    } else if (typeof tasks === 'string') {
-      paths.push(new URL(tasks));
+  for (const task of toProcess) {
+    if (typeof task !== 'string') throw new Error(`File ${path.href} is not an array of strings`);
+    const sourceFile = new URL(task);
+    if (sourceFile.protocol === 'file:') {
+      paths.push(sourceFile);
     } else {
-      throw new Error(`File ${path.href} is not an array of strings`);
+      const fileName = basename(sourceFile.pathname);
+      if (fileName == null) throw new Error(`Unsupported source pathname ${sourceFile.pathname}`);
+      const localFile = new URL(fileName, fsa.toUrl('tmp/join/'));
+      const stream = fsa.readStream(sourceFile);
+      await fsa.write(localFile, stream);
+      paths.push(sourceFile);
     }
   }
   return paths;
+}
+
+/**
+ * Upload output file into s3 bucket
+ *
+ */
+async function upload(file: URL, bucketPath: string, logger: LogType): Promise<URL> {
+  logger.info({ file: file }, 'Load:Start');
+  let filename = basename(file.pathname);
+  let stream = fsa.readStream(file);
+
+  // gzip index file
+  if (filename.endsWith('index')) {
+    filename = `${filename}.gz`;
+    stream = stream.pipe(createGzip());
+  }
+
+  // Upload to s3
+  let path = fsa.toUrl(`${bucketPath}/${CliId}/${filename}`);
+  if (filename.endsWith('catalog.json')) path = fsa.toUrl(`${bucketPath}/${filename}`); // Upload catalog to root directory
+  await fsa.write(path, stream);
+  logger.info({ file: file, path: path }, 'Load:Finish');
+  return path;
 }
 
 export const JoinArgs = {
@@ -39,7 +66,7 @@ export const JoinArgs = {
     type: string,
     long: 'filename',
     description: 'Output filename default topographic',
-    defaultValue: () => 'topographic',
+    defaultValue: () => 'topographic-v2',
     defaultValueIsSerializable: true,
   }),
   tileMatrix: option({
@@ -82,10 +109,20 @@ export const JoinCommand = command({
 
     await toTarTiles(outputMbtiles, outputCotar, logger);
 
-    await toTarIndex(outputCotar, 'tmp/', args.filename, logger);
+    const [outputIndex, outputTar] = await toTarIndex(outputCotar, 'tmp/', args.filename, logger);
 
     const tileMatrix = TileMatrixSets.find(args.tileMatrix);
     if (tileMatrix == null) throw new Error(`Tile matrix ${args.tileMatrix} is not supported`);
-    await createStacFiles(filePaths, args.target, args.filename, tileMatrix, args.title, logger);
+    const stacFiles = await createStacFiles(filePaths, args.target, args.filename, tileMatrix, args.title, logger);
+
+    // Upload output to s3
+    const bucketPath = `${args.target}/vector/${Epsg.Google.code.toString()}/${args.filename}`;
+    await upload(outputMbtiles, bucketPath, logger);
+    await upload(fsa.toUrl(outputTar), bucketPath, logger);
+    await upload(fsa.toUrl(outputIndex), bucketPath, logger);
+    // Upload stac Files
+    for (const file of stacFiles) {
+      await upload(file, bucketPath, logger);
+    }
   },
 });
