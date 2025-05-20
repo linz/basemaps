c1affe101bedbb48f64b479c096ab25907601059
Tawera Manaena <TManaena@linz.govt.nz>
2025-05-21 09:04:55 +1200

    refactor(cli-vector): implement shared function for handling road features

--- packages/cli-vector/src/modify/layers/street_labels.ts
+++ packages/cli-vector/src/modify/layers/street_labels.ts
@@ -2,14 +2,14 @@ import { LogType } from '@basemaps/shared';
 
 import { VectorCreationOptions } from '../../stac.js';
 import { VectorGeoFeature } from '../../types/VectorGeoFeature.js';
-import { MajorHighWays } from '../consts.js';
+import { handleRoadFeature } from '../shared.js';
 
 export function handleLayerStreetLabels(
   feature: VectorGeoFeature,
   options: VectorCreationOptions,
   logger: LogType,
 ): VectorGeoFeature {
-  logger.info({}, 'HandleStreetLabels:Start');
+  logger.debug({}, 'HandleStreetLabels:Start');
   feature = structuredClone(feature);
   const kind = options.layer.tags['kind'];
 
@@ -19,49 +19,14 @@ export function handleLayerStreetLabels(
       break;
   }
 
-  logger.info({}, 'HandleStreetLabels:End');
+  logger.debug({}, 'HandleStreetLabels:End');
   return feature;
 }
 
 function handleKindRoad(feature: VectorGeoFeature, options: VectorCreationOptions, logger: LogType): VectorGeoFeature {
-  logger.info({}, 'HandleKindRoad:Start');
-  feature = structuredClone(feature);
+  logger.trace({}, 'HandleKindRoad:Start');
+  feature = handleRoadFeature(feature, options, logger);
 
-  const highwayNum = feature.properties['hway_num'];
-  if (typeof highwayNum === 'string') {
-    // append/override tags
-    const kind = 'motorway';
-    feature.properties['kind'] = kind;
-    const ref = highwayNum;
-    feature.properties['ref'] = ref;
-    logger.info({ kind, ref }, 'new/overidden tags');
-
-    // override styles
-    const minzoom = MajorHighWays.has(highwayNum) ? 2 : 8;
-    feature.tippecanoe.minzoom = minzoom;
-    logger.info({ minzoom }, 'overidden styles');
-
-    // return feature
-    logger.info({}, 'HandleKindRoad:End');
-    return feature;
-  }
-
-  const laneCount = feature.properties['lane_count'];
-  if (typeof laneCount === 'number') {
-    // append/override tags
-    const kind = laneCount >= 4 ? 'primary' : 'secondary';
-    feature.properties['kind'] = kind;
-    logger.info({ kind }, 'new/overidden tags');
-  }
-
-  // override styles
-  if (options.layer.style.minZoom < 10) {
-    const minzoom = 10;
-    feature.tippecanoe.minzoom = minzoom;
-    logger.info({ minzoom }, 'overidden styles');
-  }
-
-  // return feature
-  logger.info({}, 'HandleKindRoad:End');
+  logger.trace({}, 'HandleKindRoad:End');
   return feature;
 }
--- packages/cli-vector/src/modify/layers/streets.ts
+++ packages/cli-vector/src/modify/layers/streets.ts
@@ -2,53 +2,16 @@ import { LogType } from '@basemaps/shared';
 
 import { VectorCreationOptions } from '../../stac.js';
 import { VectorGeoFeature } from '../../types/VectorGeoFeature.js';
-import { MajorHighWays } from '../consts.js';
+import { handleRoadFeature } from '../shared.js';
 
 export function handleLayerStreets(
   feature: VectorGeoFeature,
   options: VectorCreationOptions,
   logger: LogType,
 ): VectorGeoFeature {
-  logger.info({}, 'HandleStreets:Start');
-  feature = structuredClone(feature);
+  logger.debug({}, 'HandleStreets:Start');
+  feature = handleRoadFeature(feature, options, logger);
 
-  // REVIEW: the following logic is very similar (but different)
-  // to the handleKindRoad function's logic. We should reusue.
-  const highwayNum = feature.properties['hway_num'];
-  if (typeof highwayNum === 'string') {
-    // append/override tags
-    const kind = 'motorway';
-    feature.properties['kind'] = kind;
-    const ref = highwayNum;
-    feature.properties['ref'] = ref;
-    logger.info({ kind, ref }, 'new/overidden tags');
-
-    // override styles
-    const minzoom = MajorHighWays.has(highwayNum) ? 2 : 8;
-    feature.tippecanoe.minzoom = minzoom;
-    logger.info({ minzoom }, 'overidden styles');
-
-    // return feature
-    logger.info({}, 'HandleStreets:End');
-    return feature;
-  }
-
-  const laneCount = feature.properties['lane_count'];
-  if (typeof laneCount === 'number') {
-    // append/override tags
-    const kind = laneCount >= 4 ? 'primary' : 'secondary';
-    feature.properties['kind'] = kind;
-    logger.info({ kind }, 'new/overidden tags');
-  }
-
-  // override styles
-  if (options.layer.style.minZoom < 10) {
-    const minzoom = 10;
-    feature.tippecanoe.minzoom = minzoom;
-    logger.info({ minzoom }, 'overidden styles');
-  }
-
-  // return feature
-  logger.info({}, 'HandleStreets:End');
+  logger.debug({}, 'HandleStreets:End');
   return feature;
 }
