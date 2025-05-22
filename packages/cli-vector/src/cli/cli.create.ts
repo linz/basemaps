635eeaa1c7ba6947d6f7becbf5b84961a69c6bf9
Tawera Manaena <TManaena@linz.govt.nz>
2025-05-23 11:09:54 +1200

    refactor(cli-vector): clarify logic for organising tmp files

--- packages/cli-vector/src/cli/cli.create.ts
+++ packages/cli-vector/src/cli/cli.create.ts
@@ -1,5 +1,3 @@
-import fs from 'node:fs';
-
 import { fsa, LogType, Url } from '@basemaps/shared';
 import { CliInfo } from '@basemaps/shared/build/cli/info.js';
 import { getLogger, logArguments } from '@basemaps/shared/build/cli/log.js';
@@ -12,6 +10,7 @@ import { Metrics } from '../schema-loader/schema.js';
 import { VectorStacItem } from '../stac.js';
 import { ogr2ogrNDJson } from '../transform/ogr2ogr.js';
 import { tileJoin, tippecanoe } from '../transform/tippecanoe.js';
+import { prepareTmpPaths } from '../util.js';
 
 const tmpPath = fsa.toUrl('tmp/create/');
 
@@ -98,92 +97,92 @@ export const CreateCommand = command({
 async function createMbtiles(path: URL, logger: LogType): Promise<Metrics> {
   logger.info({ path: path.href }, 'CreateMbtiles: Start');
 
-  // Read the stac file and get mbtiles creation options
+  /**
+   * Parse the Vector Stac Item file
+   */
+  logger.info({ path: path.href }, 'CreateMbtiles: Parse Vector Stac Item');
   const stac = await fsa.readJson<VectorStacItem>(path);
   if (stac == null) throw new Error(`Failed to read stac file from ${path.href}`);
 
+  // mbtiles creation options
   const options = stac.properties['linz_basemaps:options'];
   if (options == null) throw new Error(`Failed to read vector creation options from stac ${path.href}`);
 
   const layer = options.layer;
-  const ShortbreadLayer = options.name;
-  if (ShortbreadLayer == null) throw new Error(`Failed to read Shortbread layer name from stac ${path.href}`);
+  const shortbreadLayer = options.name;
+  if (shortbreadLayer == null) throw new Error(`Failed to read Shortbread layer name from stac ${path.href}`);
 
   const cache = layer.cache;
   if (cache == null) throw new Error(`Failed to read cache path from stac ${path.href}`);
-  logger.info({ ShortbreadLayer, layer: layer.name }, 'CreateMbtiles: Layer');
+  logger.info({ shortbreadLayer, layer: layer.name }, 'CreateMbtiles: Layer');
 
-  // create layer-specific tmp dir
-  const LayerDir = new URL(`${layer.id}/`, tmpPath);
-  if (!fs.existsSync(LayerDir)) {
-    fs.mkdirSync(LayerDir);
-  }
-
-  // download latest source file
-  logger.info({ source: layer.source }, 'CreateMbtiles: DownloadSource');
   const format = layer.source.split('.').pop();
   if (format == null) throw new Error(`Failed to parse source file format ${layer.source}`);
+
   const contentType = sourceFormats[format];
   if (contentType == null) throw new Error(`Unsupported source file format ${layer.source}`);
-  const sourceFile = new URL(`${layer.id}.${format}`, LayerDir);
 
-  const sourceFileExists = await fsa.exists(sourceFile);
-  if (!sourceFileExists) {
+  /**
+   * Prepare tmp paths for local files
+   */
+  const paths = prepareTmpPaths(tmpPath, path, layer.id, format, shortbreadLayer);
+
+  /**
+   * Download the source file
+   */
+  logger.info({ source: layer.source }, 'CreateMbtiles: DownloadSource');
+  if (!(await fsa.exists(paths.source))) {
     const stream = fsa.readStream(new URL(layer.source));
-    await fsa.write(sourceFile, stream.pipe(createGunzip()), {
+    await fsa.write(paths.source, stream.pipe(createGunzip()), {
       contentType,
     });
   }
 
-  // Transform the source file to ndjson for generalization
-  logger.info({ source: sourceFile.href }, 'CreateMbtiles: ToNdjson');
-  const ndjsonFile = new URL(`${layer.id}.ndjson`, LayerDir);
-
-  const ndjsonFileExists = await fsa.exists(ndjsonFile);
-  if (!ndjsonFileExists) {
-    await ogr2ogrNDJson(sourceFile, ndjsonFile, logger);
+  /**
+   * Convert the source file into an ndjson
+   */
+  logger.info({ source: paths.source.href }, 'CreateMbtiles: ToNdjson');
+  if (!(await fsa.exists(paths.ndjson))) {
+    await ogr2ogrNDJson(paths.source, paths.ndjson, logger);
   }
 
-  // Create Shortbread layer-specific tmp subdir
-  const ShortbreadDir = new URL(`${ShortbreadLayer}/`, LayerDir);
-  if (!fs.existsSync(ShortbreadDir)) {
-    fs.mkdirSync(ShortbreadDir);
-  }
+  /**
+   * Parse the ndjson file and apply the generalization options
+   */
+  logger.info({ source: paths.ndjson.href }, 'CreateMbtiles: doGeneralize');
+  if (!(await fsa.exists(paths.genNdjson))) {
+    const metrics = await generalize(paths.ndjson, paths.genNdjson, options, logger);
 
-  // Read the ndjson file and apply the generalization options
-  logger.info({ source: ndjsonFile.href }, 'CreateMbtiles: doGeneralize');
-  const generalizedFile = new URL(`${layer.id}-gen.ndjson`, ShortbreadDir);
-
-  const generalizedFileExists = await fsa.exists(generalizedFile);
-  if (!generalizedFileExists) {
-    const metrics = await generalize(ndjsonFile, generalizedFile, options, logger);
-
-    if (metrics.output === 0) throw new Error(`Failed to generalize ndjson file ${ndjsonFile.href}`);
+    if (metrics.output === 0) throw new Error(`Failed to generalize ndjson file ${paths.ndjson.href}`);
     layer.metrics = metrics;
   }
 
-  // Transform the generalized ndjson file to mbtiles
-  logger.info({ source: generalizedFile.href }, 'CreateMbtiles: ToMbtiles');
-  const mbtilesFile = new URL(`${layer.id}.mbtiles`, ShortbreadDir);
-
-  const mbtilesFileExists = await fsa.exists(mbtilesFile);
-  if (!mbtilesFileExists) {
-    await tippecanoe(generalizedFile, mbtilesFile, layer, logger);
+  /**
+   * Transform the generalized ndjson file to an mbtiles file
+   */
+  logger.info({ source: paths.genNdjson.href }, 'CreateMbtiles: ToMbtiles');
+  if (!(await fsa.exists(paths.mbtiles))) {
+    await tippecanoe(paths.genNdjson, paths.mbtiles, layer, logger);
   }
 
-  // Copy the mbtiles file to the same directory as the vector stac item
-  logger.info({ source: mbtilesFile.href }, 'CreateMbtiles: WriteMbtiles');
+  /**
+   * Copy the mbtiles file to the same directory as the Vector Stac Item file
+   */
+  logger.info({ source: paths.mbtiles.href }, 'CreateMbtiles: WriteMbtiles');
   const mbTilesFileCopy = new URL(path.href.replace(/\.json$/, '.mbtiles'));
 
-  let mbtilesFileCopyExists = await fsa.exists(mbTilesFileCopy);
-  if (!mbtilesFileCopyExists) {
-    await fsa.write(mbTilesFileCopy, fsa.readStream(mbtilesFile));
+  if (!(await fsa.exists(paths.mbtilesCopy))) {
+    await fsa.write(mbTilesFileCopy, fsa.readStream(paths.mbtiles));
+
+    // Ensure the mbtiles file was copied successfully
+    if (!(await fsa.exists(paths.mbtilesCopy))) {
+      throw new Error(`Failed to write the mbtiles file to ${mbTilesFileCopy.href}`);
+    }
   }
 
-  mbtilesFileCopyExists = await fsa.exists(mbTilesFileCopy);
-  if (!mbtilesFileCopyExists) throw new Error(`Failed to write the mbtiles file to ${mbTilesFileCopy.href}`);
-
-  // Update the vector stac item
+  /**
+   * Update the Vector Stac Item file
+   */
   logger.info({ stac: path.href }, 'CreateMbtiles: UpdateStac');
 
   // Update 'cache' flag to 'true' now that the mbtiles file exists
@@ -203,5 +202,8 @@ async function createMbtiles(path: URL, logger: LogType): Promise<Metrics> {
   // Overwrite the vector stac item
   await fsa.write(path, JSON.stringify(stac, null, 2));
 
-  return { mbTilesPath: mbtilesFile, input: layer.metrics?.input ?? -1, output: layer.metrics?.input ?? -1 };
+  /**
+   * Return metrics
+   */
+  return { mbTilesPath: paths.mbtiles, input: layer.metrics?.input ?? -1, output: layer.metrics?.input ?? -1 };
 }
--- packages/cli-vector/src/util.ts
+++ packages/cli-vector/src/util.ts
@@ -1,3 +1,5 @@
+import { existsSync, mkdirSync } from 'node:fs';
+
 import { Epsg } from '@basemaps/geo';
 import { createReadStream, promises as fs, ReadStream } from 'fs';
 
@@ -22,4 +24,52 @@ export function createReadStreamSafe(filename: string): Promise<ReadStream> {
       resolve(fileStream);
     });
   });
+}
+
+interface TmpPaths {
+  /** @example "tmp/create/layers/50248/50248.gpkg" */
+  source: URL;
+
+  /** @example "tmp/create/layers/50248/50248.ndjson" */
+  ndjson: URL;
+
+  /** @example "tmp/create/transform/aerialways/50248/50248-gen.ndjson" */
+  genNdjson: URL;
+
+  /** @example "tmp/create/transform/aerialways/50248/50248.mbtiles" */
+  mbtiles: URL;
+
+  /**
+   * @example "local-cache/layerId/{layerId}_{version}_{hash}.mbtiles"
+   * @example "s3://linz-lds-cache/layerId/{layerId}_{version}_{hash}.mbtiles"
+   */
+  mbtilesCopy: URL;
+}
+
+export function prepareTmpPaths(
+  tmpPath: URL,
+  path: URL,
+  layerId: string,
+  format: string,
+  shortbreadLayer: string,
+): TmpPaths {
+  /** @example "tmp/create/layers/50248/" */
+  const LayersDir = new URL(`layers/${layerId}/`, tmpPath);
+  if (!existsSync(LayersDir)) {
+    mkdirSync(LayersDir, { recursive: true });
+  }
+
+  /** @example "tmp/create/transform/aerialways/50248/" */
+  const TransformDir = new URL(`transform/${shortbreadLayer}/${layerId}/`, tmpPath);
+  if (!existsSync(TransformDir)) {
+    mkdirSync(TransformDir, { recursive: true });
+  }
+
+  return {
+    source: new URL(`${layerId}.${format}`, LayersDir),
+    ndjson: new URL(`${layerId}.ndjson`, LayersDir),
+    genNdjson: new URL(`${layerId}-gen.ndjson`, TransformDir),
+    mbtiles: new URL(`${layerId}.mbtiles`, TransformDir),
+    mbtilesCopy: new URL(path.href.replace(/\.json$/, '.mbtiles')),
+  };
 }
