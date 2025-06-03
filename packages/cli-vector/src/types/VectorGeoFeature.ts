9757c472897c192b3ac6a97b94609e45e14d991c
Tawera Manaena <TManaena@linz.govt.nz>
2025-06-03 14:37:12 +1200

    fix(cli-vector): restore VectorGeoFeature type

--- packages/cli-vector/src/types/VectorGeoFeature.ts
+++ packages/cli-vector/src/types/VectorGeoFeature.ts
@@ -1,4 +1,6 @@
-export interface VectorGeoFeature extends GeoJSON.Feature {
+import { Feature } from 'geojson';
+
+export interface VectorGeoFeature extends Feature {
   properties: Record<string, boolean | null | number | string | undefined>;
   tippecanoe: {
     layer: string;
