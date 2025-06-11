ab2c063f874024c2ea0eb405b4cfe90920535c96
Tawera Manaena <TManaena@linz.govt.nz>
2025-06-11 14:14:13 +1200

    feat(cli-vector): add the cli-vector reports command to the parent cli command

--- packages/cli-vector/src/index.ts
+++ packages/cli-vector/src/index.ts
@@ -4,6 +4,7 @@ import { CreateCommand } from './cli/cli.create.js';
 import { DocsCommand } from './cli/cli.docs.js';
 import { ExtractCommand } from './cli/cli.extract.js';
 import { JoinCommand } from './cli/cli.join.js';
+import { ReportsCommand } from './cli/cli.reports.js';
 
 export const VectorCli = subcommands({
   name: 'vector',
@@ -11,6 +12,7 @@ export const VectorCli = subcommands({
     extract: ExtractCommand,
     create: CreateCommand,
     join: JoinCommand,
+    reports: ReportsCommand,
     docs: DocsCommand,
   },
 });
