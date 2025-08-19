5e9493baf539fe6374dbcf1017401b553f734ece
Tawera Manaena <TManaena@linz.govt.nz>
2025-08-20 10:02:21 +1200

    test(geo): refine projjson unit tests

--- packages/geo/src/proj/json/__tests__/proj.json.test.ts
+++ packages/geo/src/proj/json/__tests__/proj.json.test.ts
@@ -4,13 +4,24 @@ import { describe, it } from 'node:test';
 import { Epsg } from '../../../epsg.js';
 import { Projection } from '../../projection.js';
 import { ProjectionLoader } from '../../projection.loader.js';
+import { ProjJsons } from '../proj.json.js';
 
 describe('ProjJson', () => {
-  it('should parse the AKTM2000 projjson into a runtime projection', async () => {
-    const code = 3788;
-    assert.strictEqual(Epsg.tryGet(code), undefined);
-    const epsg = await ProjectionLoader.load(code);
-    const projection = Projection.tryGet(epsg);
-    assert.notStrictEqual(projection, null);
+  it('should parse each unsupported EPSG code and ProjJSON pair into a runtime Epsg and Projection', async () => {
+    for (const key of Object.keys(ProjJsons)) {
+      const code = Number(key);
+
+      // skip any already supported EPSG codes
+      if (Epsg.tryGet(code) != null) continue;
+
+      // ensure that neither an Epsg or Projection already exists
+      assert.strictEqual(Epsg.tryGet(code), undefined);
+      assert.strictEqual(Projection.tryGet(code), null);
+
+      // ensure that both an Epsg and Projection now exist
+      const epsg = await ProjectionLoader.load(code);
+      assert.notStrictEqual(Epsg.tryGet(code), undefined);
+      assert.notStrictEqual(Projection.tryGet(epsg), null);
+    }
   });
 });
