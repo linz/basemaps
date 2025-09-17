284fbcdaa3ed1b1528c86231b99ce245b27cb7f7
Blayne Chard <bchard@linz.govt.nz>
2025-09-17 15:12:05 +1200

    build: node 24 (#3512)
    ### Motivation
    
    the new ZSTD decompression APIs are only added in node24, in preparation
    for node24 coming to aws lambda upgrade to node24
    
    ### Modifications
    
    Swapped to the new node --test globber as the globbing logic has changed
    and broken the current state
    Upgraded lambdas to node 22 (Current latest version, we are waiting for
    node24 lambda to release)
    Upgraded containers to node 24
    Upgrade pipelines to node 24
    
    
    ### Verification
    
    unit tests, local imagery service.

--- packages/_infra/src/serve/lambda.tiler.ts
+++ packages/_infra/src/serve/lambda.tiler.ts
@@ -59,7 +59,7 @@ export class LambdaTiler extends Construct {
      */
 
     this.lambdaNoVpc = new lambda.Function(this, 'TilerNoVpc', {
-      runtime: lambda.Runtime.NODEJS_20_X,
+      runtime: lambda.Runtime.NODEJS_22_X,
       memorySize: 3072,
       timeout: Duration.seconds(60),
       handler: 'index.handler',
