80068b394629d3a2b461d4cbd70460f59b1b88d9
Tawera Manaena <TManaena@linz.govt.nz>
2025-12-02 10:32:53 +1300

    feat(cli-config): add diff command entry to config cli

--- packages/cli-config/src/index.ts
+++ packages/cli-config/src/index.ts
@@ -4,13 +4,15 @@ import { BundleAssetsCommand } from './cli/action.bundle.assets.js';
 import { BundleCommand } from './cli/action.bundle.js';
 import { CreateConfigCommand } from './cli/action.create.config.js';
 import { ImportCommand } from './cli/action.import.js';
+import { DiffCommand } from './cli/action.diff.js';
 
 export const ConfigCli = subcommands({
   name: 'config',
   cmds: {
     bundle: BundleCommand,
     'bundle-assets': BundleAssetsCommand,
-    import: ImportCommand,
     'create-config': CreateConfigCommand,
+    diff: DiffCommand,
+    import: ImportCommand,
   },
 });
