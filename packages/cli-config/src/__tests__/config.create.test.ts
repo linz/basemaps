3e24388e005114812c7c68418710455fcb28c730
Blayne Chard <bchard@linz.govt.nz>
2026-02-04 16:23:05 +1300

    build: more tests

--- packages/cli-config/src/__tests__/config.create.test.ts
+++ packages/cli-config/src/__tests__/config.create.test.ts
@@ -25,9 +25,6 @@ describe('action.config.create', async () => {
   };
 
   before(() => {
-    // Imagery is linked from s3, overwrite s3 links to just use memory
-    fsa.register('s3://', fsMem);
-
     fsa.register('source://', fsMem);
     fsa.register('target://', fsMem);
     fsa.register('file:///tmp/', fsMem);
@@ -53,6 +50,52 @@ describe('action.config.create', async () => {
 
     const configUrlPreview = new URL(await fsMem.read(new URL('file:///tmp/cogify/config-url-preview')));
     assert.equal(configUrlPreview.searchParams.get('pipeline'), 'color-ramp');
+    assert.equal(configUrlPreview.searchParams.get('config'), ensureBase58(configPath));
+    assert.equal(configUrlPreview.host, 'basemaps.linz.govt.nz');
+
+    assert.ok(configUrlPreview.pathname.startsWith('/v1/preview/westland-2024-0.075m/'));
+  });
+
+  it('should create a config with rgbi', async () => {
+    await fsMem.write(new URL('source://imagery/westland_2024_0.075m/cog1.tif'), fsa.readStream(TestTiff.Rgbi16));
+    await CreateConfigCommand.handler(baseArgs);
+
+    const configPath = String(await fsMem.read(new URL('file:///tmp/cogify/config-path')));
+    assert.ok(configPath.startsWith('target://imagery/basemaps-config-'));
+
+    const configUrl = new URL(await fsMem.read(new URL('file:///tmp/cogify/config-url')));
+    assert.equal(configUrl.searchParams.get('pipeline'), 'rgb');
+    assert.equal(configUrl.searchParams.get('config'), ensureBase58(configPath));
+    assert.equal(configUrl.searchParams.get('tileMatrix'), null); // web mercator quad is the default
+    assert.equal(configUrl.searchParams.get('debug'), 'true');
+    assert.equal(configUrl.searchParams.get('style'), 'westland-2024-0.075m');
+
+    assert.equal(configUrl.host, 'basemaps.linz.govt.nz');
+
+    const configUrlPreview = new URL(await fsMem.read(new URL('file:///tmp/cogify/config-url-preview')));
+    assert.equal(configUrlPreview.searchParams.get('pipeline'), 'rgb');
+    assert.equal(configUrlPreview.searchParams.get('config'), ensureBase58(configPath));
+    assert.equal(configUrlPreview.host, 'basemaps.linz.govt.nz');
+
+    assert.ok(configUrlPreview.pathname.startsWith('/v1/preview/westland-2024-0.075m/'));
+  });
+
+  it('should create a include tile matrix with NZTM2000Quad', async () => {
+    await fsMem.write(new URL('source://imagery/westland_2024_0.075m/cog1.tif'), fsa.readStream(TestTiff.Nztm2000));
+    await CreateConfigCommand.handler(baseArgs);
+
+    const configPath = String(await fsMem.read(new URL('file:///tmp/cogify/config-path')));
+    assert.ok(configPath.startsWith('target://imagery/basemaps-config-'));
+
+    const configUrl = new URL(await fsMem.read(new URL('file:///tmp/cogify/config-url')));
+    assert.equal(configUrl.searchParams.get('config'), ensureBase58(configPath));
+    assert.equal(configUrl.searchParams.get('tileMatrix'), 'NZTM2000Quad');
+    assert.equal(configUrl.searchParams.get('debug'), 'true');
+    assert.equal(configUrl.searchParams.get('style'), 'westland-2024-0.075m');
+
+    assert.equal(configUrl.host, 'basemaps.linz.govt.nz');
+
+    const configUrlPreview = new URL(await fsMem.read(new URL('file:///tmp/cogify/config-url-preview')));
     assert.equal(configUrlPreview.searchParams.get('config'), ensureBase58(configPath));
     assert.equal(configUrlPreview.host, 'basemaps.linz.govt.nz');
 
